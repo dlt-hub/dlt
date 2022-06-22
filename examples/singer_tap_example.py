af67924fb099b1cd45722db0cd8f214661177c04
Marcin Rudolf <rudolfix@rudolfix.org>
2022-06-22 21:45:04 +0200

    adds singer tap source and examples

--- examples/singer_tap_example.py
+++ examples/singer_tap_example.py
@@ -1,18 +1,36 @@
+import os
+from tempfile import mkdtemp
 
-from typing import Iterator
-from dlt.pipeline import Pipeline
-from dlt.pipeline.typing import GCPPipelineCredentials
+from dlt.pipeline import Pipeline, GCPPipelineCredentials
+from dlt.common.runners.venv import Venv
 
-from examples.sources.singer_tap import SingerMessage, get_source
-from examples.sources.jsonl import get_source as read_jsonl
+from examples.sources.singer_tap import get_source
 
-# load hubspot schema stub - it converts all field names with `timestamp` into timestamp type
-schema = Pipeline.load_schema_from_file("examples/schemas/hubspot_schema.yml")
-p = Pipeline("singer_tap_sheets")
-p.create_pipeline(GCPPipelineCredentials.from_services_file("_secrets/project1234_service.json", "load_1"), schema=schema)
-# p.extract(get_source(read_jsonl("examples/data/singer_taps/tap_google_sheet.jsonl")))
-raw_messages: Iterator[SingerMessage] = read_jsonl("examples/data/singer_taps/tap_hubspot.jsonl")  # type: ignore
-p.extract(get_source(raw_messages, p.state))
-p.unpack()
-print(p.get_default_schema().as_yaml(remove_default_hints=True))
-# p.load()
+p = Pipeline("singer_csv")
+p.create_pipeline(GCPPipelineCredentials.from_services_file("_secrets/project1234_service.json", "load_1"))
+
+# create Venv with desired dependencies, in this case csv tap
+# venv creation costs time so it should be created only once and reused
+
+# here we use context manager to automatically delete venv after example was run
+# the dependency is meltano version of csv tap
+print("Spawning virtual environment to run singer and installing csv tap from git+https://github.com/MeltanoLabs/tap-csv.git")
+with Venv.create(mkdtemp(), ["git+https://github.com/MeltanoLabs/tap-csv.git"]) as venv:
+    # prep singer config for csv-tap
+    csv_tap_config = {
+        "files": [
+            {
+                "entity": "annotations_202205",
+                "path": os.path.abspath("examples/data/singer_taps/model_annotations.csv"),
+                "keys": [
+                    "message id"
+                ]
+            }
+        ]
+    }
+    print("running tap-csv")
+    i = get_source(venv, "tap-csv", csv_tap_config, "examples/data/singer_taps/csv_catalog.json", state=p.state)
+    p.extract(i)
+    p.unpack()
+    print(p.get_default_schema().as_yaml(remove_default_hints=True))
+    # p.load()
--- examples/sources/singer_tap.py
+++ examples/sources/singer_tap.py
@@ -1,10 +1,16 @@
+import os
+import tempfile
+from typing import Iterator, TypedDict, cast, Union
 
-
-from typing import Iterator, TypedDict, cast
+from dlt.common import json
+from dlt.common.runners.venv import Venv
 from dlt.common.sources import with_table_name
-from dlt.common.typing import DictStrAny
+from dlt.common.typing import DictStrAny, StrAny, StrOrBytesPath
+
+from examples.sources.stdout import get_source as get_singer_pipe
 
 # from dlt.pipeline.exceptions import MissingDependencyException
+FilePathOrDict = Union[StrAny, StrOrBytesPath]
 
 
 class SingerMessage(TypedDict):
@@ -28,7 +34,7 @@ class SingerState(SingerMessage):
 # pip install ../singer/singer-python
 # https://github.com/datamill-co/singer-runner/tree/master/singer_runner
 # https://techgaun.github.io/active-forks/index.html#singer-io/singer-python
-def get_source(singer_messages: Iterator[SingerMessage], state: DictStrAny = None) -> Iterator[DictStrAny]:
+def get_source_from_stream(singer_messages: Iterator[SingerMessage], state: DictStrAny = None) -> Iterator[DictStrAny]:
     last_state = {}
     for msg in singer_messages:
         if msg["type"] == "RECORD":
@@ -40,3 +46,34 @@ def get_source(singer_messages: Iterator[SingerMessage], state: DictStrAny = Non
             last_state = msg["value"]
     if state:
         state["singer"] = last_state
+
+
+def get_source(venv: Venv, tap_name: str, config_file: FilePathOrDict, catalog_file: FilePathOrDict, state: DictStrAny = None) -> Iterator[DictStrAny]:
+    # write config dictionary to temp file in virtual environment if passed as dict
+    config_file_path: StrOrBytesPath = None
+    if type(config_file) is dict:
+        fd, tmp_name = tempfile.mkstemp(dir=venv.context.env_dir)
+        with os.fdopen(fd, "w") as f:
+            json.dump(config_file, f)
+            config_file_path = tmp_name
+    else:
+        config_file_path = config_file  # type: ignore
+
+    # process catalog like config
+    catalog_file_path: StrOrBytesPath = None
+    if type(catalog_file) is dict:
+        fd, tmp_name = tempfile.mkstemp(dir=venv.context.env_dir)
+        with os.fdopen(fd, "w") as f:
+            json.dump(catalog_file, f)
+            catalog_file_path = tmp_name
+    else:
+        catalog_file_path = catalog_file  # type: ignore
+
+    pipe_iterator = get_singer_pipe(venv,
+                                    tap_name,
+                                    "--config",
+                                    os.path.abspath(config_file_path),
+                                    "--catalog",
+                                    os.path.abspath(catalog_file_path)
+                                    )
+    yield from get_source_from_stream(pipe_iterator, state)  # type: ignore
