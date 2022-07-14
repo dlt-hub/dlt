ccfad023793dd010a4f801034fb7966d60a4951f
Marcin Rudolf <rudolfix@rudolfix.org>
2022-07-14 20:59:13 +0200

    overwrites env to test pipelines

--- tests/loaders/redshift/test_pipelines.py
+++ tests/loaders/redshift/test_pipelines.py
@@ -1,8 +1,9 @@
 import os
-
 import pytest
-from dlt.common.schema.schema import Schema
+from os import environ
 
+from dlt.common.schema.schema import Schema
+from dlt.common.utils import uniq_id
 from dlt.pipeline import Pipeline, PostgresPipelineCredentials
 from dlt.pipeline.exceptions import InvalidPipelineContextException
 
@@ -12,9 +13,17 @@ from tests.utils import autouse_root_storage, TEST_STORAGE
 FAKE_CREDENTIALS = PostgresPipelineCredentials("redshift", None, None, None, None)
 
 
+@pytest.fixture(scope="module", autouse=True)
+def preserve_environ() -> None:
+    saved_environ = environ.copy()
+    yield
+    environ.clear()
+    environ.update(saved_environ)
+
+
 def test_empty_default_schema_name() -> None:
     p = Pipeline("test_empty_default_schema_name")
-    FAKE_CREDENTIALS.PG_SCHEMA_PREFIX = "test_empty_default_schema_name"
+    FAKE_CREDENTIALS.PG_SCHEMA_PREFIX = environ["PG_SCHEMA_PREFIX"] = "test_empty_default_schema_name" + uniq_id()
     p.create_pipeline(FAKE_CREDENTIALS, os.path.join(TEST_STORAGE, FAKE_CREDENTIALS.PG_SCHEMA_PREFIX), Schema(""))
     p.extract(iter(["a", "b", "c"]), table_name="test")
     p.unpack()
@@ -33,7 +42,7 @@ def test_empty_default_schema_name() -> None:
 
 def test_create_wipes_working_dir() -> None:
     p = Pipeline("test_create_wipes_working_dir")
-    FAKE_CREDENTIALS.PG_SCHEMA_PREFIX = "test_create_wipes_working_dir"
+    FAKE_CREDENTIALS.PG_SCHEMA_PREFIX = environ["PG_SCHEMA_PREFIX"] = "test_create_wipes_working_dir" + uniq_id()
     p.create_pipeline(FAKE_CREDENTIALS, os.path.join(TEST_STORAGE, FAKE_CREDENTIALS.PG_SCHEMA_PREFIX), Schema("table"))
     p.extract(iter(["a", "b", "c"]), table_name="test")
     p.unpack()
