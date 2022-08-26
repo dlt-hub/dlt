fba11c0941172c23352574434e72a708d2e92758
Marcin Rudolf <rudolfix@rudolfix.org>
2022-08-26 16:34:07 +0200

    adds import schema and execute in runner options to Pipeline v1

--- dlt/pipeline/__init__.py
+++ dlt/pipeline/__init__.py
@@ -1,3 +1,4 @@
+from dlt.common.schema import Schema  # noqa: F401
 from dlt.pipeline.pipeline import Pipeline  # noqa: F401
 from dlt.pipeline.typing import GCPPipelineCredentials, PostgresPipelineCredentials  # noqa: F401
 from dlt.pipeline.exceptions import CannotRestorePipelineException  # noqa: F401
