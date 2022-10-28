08c10dbff2456161f6bb377c3abaf73ce07c1468
Marcin Rudolf <rudolfix@rudolfix.org>
2022-10-28 11:32:44 +0200

    removes pipeline v1

--- dlt/pipeline/__init__.py
+++
@@ -1,4 +0,0 @@
-from dlt.common.schema import Schema  # noqa: F401
-from dlt.pipeline.pipeline import Pipeline  # noqa: F401
-from dlt.pipeline.typing import GCPPipelineCredentials, PostgresPipelineCredentials  # noqa: F401
-from dlt.pipeline.exceptions import CannotRestorePipelineException  # noqa: F401
