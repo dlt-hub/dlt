ab849b9c46925aa08658b18957ed181e9261f3a2
Marcin Rudolf <rudolfix@rudolfix.org>
2023-03-11 23:25:57 +0100

    adds script inspector tests

---
+++ tests/reflection/module_cases/__init__.py
@@ -0,0 +1,4 @@
+import xxx.absolutely
+
+from xxx.absolutely import a1, a3
+from dlt.common.utils import uniq_id
---
+++ tests/reflection/module_cases/raises.py
@@ -0,0 +1,4 @@
+from xxx.absolutely import a1, a3
+from dlt.common.utils import uniq_id
+
+raise NotImplementedError("empty module")
