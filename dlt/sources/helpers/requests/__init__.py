eaa3eb83fdd2fa6fff4ef906f990da7a8fc6b64a
Steinthor Palsson <steini90@gmail.com>
2023-03-15 11:43:02 -0400

    Remove global requests methods for now

--- dlt/sources/helpers/requests/__init__.py
+++ dlt/sources/helpers/requests/__init__.py
@@ -1,8 +1,6 @@
 from tenacity import RetryError
 from dlt.sources.helpers.requests.retry import requests_with_retry
 from dlt.sources.helpers.requests.session import Session
-from dlt.sources.helpers.requests.api import get, put, post, options, head, delete, requests, patch
-
 
 
 
