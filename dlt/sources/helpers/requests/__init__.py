f0f9abdf1a51f2aad5bc104bedcfd80986bc280f
Steinthor Palsson <steini90@gmail.com>
2023-03-21 14:24:54 -0400

    Export default requests.client methods from helper package

--- dlt/sources/helpers/requests/__init__.py
+++ dlt/sources/helpers/requests/__init__.py
@@ -16,3 +16,7 @@ from dlt.sources.helpers.requests.retry import Client
 from dlt.sources.helpers.requests.session import Session
 
 client = Client()
+
+get, post, put, patch, delete, options, head, request = (
+    client.get, client.post, client.put, client.patch, client.delete, client.options, client.head, client.request
+)
