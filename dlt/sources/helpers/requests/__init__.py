610bce2168bf342935e840268f56a5bc17d724ea
Steinthor Palsson <steini90@gmail.com>
2023-03-21 14:02:41 -0400

    Re-export requests `Request`, `Response` and exception types

--- dlt/sources/helpers/requests/__init__.py
+++ dlt/sources/helpers/requests/__init__.py
@@ -1,4 +1,17 @@
 from tenacity import RetryError
+from requests import (
+    Request, Response,
+    ConnectionError,
+    ConnectTimeout,
+    FileModeWarning,
+    HTTPError,
+    JSONDecodeError,
+    ReadTimeout,
+    RequestException,
+    Timeout,
+    TooManyRedirects,
+    URLRequired,
+)
 from dlt.sources.helpers.requests.retry import Client
 from dlt.sources.helpers.requests.session import Session
 
