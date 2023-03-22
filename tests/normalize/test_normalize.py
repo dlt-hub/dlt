31408081032d686104e7af9104a8161e0d5ba5a0
Marcin Rudolf <rudolfix@rudolfix.org>
2023-03-22 18:27:30 +0100

    fixes the schema update format in normalize to dictionary of tables with changed columns

--- tests/normalize/test_normalize.py
+++ tests/normalize/test_normalize.py
@@ -433,9 +433,3 @@ def assert_timestamp_data_type(load_storage: LoadStorage, data_type: TDataType) 
     event_schema = load_storage.load_package_schema(loads[0])
     # in raw normalize timestamp column must not be coerced to timestamp
     assert event_schema.get_table_columns("event")["timestamp"]["data_type"] == data_type
-
-
-# def mock_destination_caps(n: Normalize, caps: DestinationCapabilitiesContext) -> None:
-#     # TODO: mock full capabilities here and inject them properly, this only works because jsonl does not need full caps
-#     n.config.destination_capabilities = caps
-#     n.load_storage.loader_file_format = caps.preferred_loader_file_format
