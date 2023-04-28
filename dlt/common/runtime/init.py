84ea2163b10675b23c987aa01a94ac1af1db3bf4
Marcin Rudolf <rudolfix@rudolfix.org>
2023-04-28 23:49:00 +0200

    delays signals only when needed

--- dlt/common/runtime/init.py
+++ dlt/common/runtime/init.py
@@ -1,17 +1,13 @@
-import threading
-
 from dlt.common.configuration.specs import RunConfiguration
 
-# signals and telemetry should be initialized only once
+# telemetry should be initialized only once
 _INITIALIZED = False
 _RUN_CONFIGURATION: RunConfiguration = None
 
 
 def initialize_runtime(config: RunConfiguration) -> None:
-    from dlt.common import logger
     from dlt.common.runtime.logger import init_logging
     from dlt.common.runtime.telemetry import start_telemetry
-    from dlt.common.runtime.signals import register_signals
 
     global _INITIALIZED, _RUN_CONFIGURATION
 
@@ -21,12 +17,6 @@ def initialize_runtime(config: RunConfiguration) -> None:
     # initialize only once
     if not _INITIALIZED:
         start_telemetry(config)
-
-        if threading.current_thread() is threading.main_thread():
-            register_signals()
-        else:
-            logger.info("Running in daemon thread, signals not enabled")
-
         _INITIALIZED = True
 
     # store last config
--- dlt/common/runtime/signals.py
+++ dlt/common/runtime/signals.py
@@ -1,3 +1,4 @@
+import threading
 import signal
 from contextlib import contextmanager
 from threading import Event
@@ -6,9 +7,6 @@ from typing import Any, TYPE_CHECKING, Iterator
 from dlt.common.exceptions import SignalReceivedException
 
 _received_signal: int = 0
-_raise_immediately: bool = False
-_original_sigint_handler = signal.getsignal(signal.SIGINT)
-_original_sigterm_handler = signal.getsignal(signal.SIGTERM)
 exit_event = Event()
 
 
@@ -32,23 +30,10 @@ def signal_receiver(sig: int, frame: Any) -> None:
 
     logger.info("Sleeping threads signalled")
 
-    if _raise_immediately and _received_signal == signal.SIGINT:
-        raise_if_signalled()
-
 
 def raise_if_signalled() -> None:
     if _received_signal:
         raise SignalReceivedException(_received_signal)
-
-
-def register_signals() -> None:
-    signal.signal(signal.SIGINT, signal_receiver)
-    signal.signal(signal.SIGTERM, signal_receiver)
-
-
-def restore_signals() -> None:
-    signal.signal(signal.SIGINT, _original_sigint_handler)
-    signal.signal(signal.SIGTERM, _original_sigterm_handler)
 
 
 def sleep(sleep_seconds: float) -> None:
@@ -62,11 +47,22 @@ def sleep(sleep_seconds: float) -> None:
 
 
 @contextmanager
-def raise_immediately() -> Iterator[None]:
-    global _raise_immediately
+def delayed_signals() -> Iterator[None]:
+    """Will delay signalling until `raise_if_signalled` is used or signalled `sleep`"""
 
-    try:
-        _raise_immediately = True
+    if threading.current_thread() is threading.main_thread():
+        original_sigint_handler = signal.getsignal(signal.SIGINT)
+        original_sigterm_handler = signal.getsignal(signal.SIGTERM)
+        try:
+            signal.signal(signal.SIGINT, signal_receiver)
+            signal.signal(signal.SIGTERM, signal_receiver)
+            yield
+        finally:
+            global _received_signal
+
+            _received_signal = 0
+            signal.signal(signal.SIGINT, original_sigint_handler)
+            signal.signal(signal.SIGTERM, original_sigterm_handler)
+    else:
+        print("Running in daemon thread, signals not enabled")
         yield
-    finally:
-        _raise_immediately = False
