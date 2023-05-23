3929ea5b5778ed5a5f38bf947794cab8ac494384
Steinthor Palsson <steini90@gmail.com>
2023-05-23 13:38:25 -0400

    Let gcsfs resolve default credentials

--- dlt/destinations/filesystem/filesystem_client.py
+++ dlt/destinations/filesystem/filesystem_client.py
@@ -5,7 +5,9 @@ from fsspec import AbstractFileSystem
 
 from dlt.common.exceptions import MissingDependencyException
 from dlt.common.typing import DictStrAny
-from dlt.destinations.filesystem.configuration import FilesystemClientConfiguration, GcpCredentials, GcpServiceAccountCredentials, AwsCredentials
+from dlt.common.configuration.specs import CredentialsWithDefault
+from dlt.destinations.filesystem.configuration import FilesystemClientConfiguration, GcpCredentials, GcpServiceAccountCredentials,  AwsCredentials
+
 from dlt import version
 
 
@@ -17,10 +19,11 @@ def client_from_config(config: FilesystemClientConfiguration) -> Tuple[AbstractF
         fs_kwargs.update(credentials.to_s3fs_credentials())
     elif proto in ['gcs', 'gs']:
         assert isinstance(config.credentials, GcpCredentials)
-        if isinstance(config.credentials, GcpServiceAccountCredentials):
-            fs_kwargs['token'] = dict(config.credentials)
+        # Default credentials are handled by gcsfs
+        if isinstance(config.credentials, CredentialsWithDefault) and config.credentials.has_default_credentials():
+            fs_kwargs['token'] = None
         else:
-            fs_kwargs['token'] = config.credentials.to_native_credentials()
+            fs_kwargs['token'] = dict(config.credentials)
         fs_kwargs['project'] = config.credentials.project_id
     try:
         return url_to_fs(config.bucket_url, **fs_kwargs)  # type: ignore[no-any-return]
