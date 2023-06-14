2910ebc18c5b6fb0d5eeebbc42815695158a85aa
Adrian <Adrian>
2023-06-14 14:12:58 +0200

    run in serverless text

--- docs/website/sidebars.js
+++ docs/website/sidebars.js
@@ -62,6 +62,7 @@ const sidebars = {
           },
           items: [
             'dlt-ecosystem/deployments/where-can-dlt-run',
+            'dlt-ecosystem/deployments/running-in-cloud-functions',
           ]
         },
         {
