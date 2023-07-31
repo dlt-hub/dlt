78bb2fd9f70a8de49463dc3d0f8e180663bf6835
Anton Burnashev <anton.burnashev@gmail.com>
2023-07-31 19:10:08 +0200

    Remove trailing whitespaces

--- dlt/extract/schema.py
+++ dlt/extract/schema.py
@@ -68,9 +68,9 @@ class DltResourceSchema:
         table_schema = self._merge_keys(resolved_template)
         table_schema["resource"] = self._name
         validate_dict(
-            spec=TPartialTableSchema, 
-            doc=table_schema, 
-            path=f"new_table/{self._name}", 
+            spec=TPartialTableSchema,
+            doc=table_schema,
+            path=f"new_table/{self._name}",
             filter_f=lambda k: not k.startswith("x-")
         )
         return table_schema
