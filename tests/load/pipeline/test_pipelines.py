72f32447d07851e0853700525e7e498027e3b869
Steinthor Palsson <steini90@gmail.com>
2023-08-02 22:39:09 -0400

    Fix test

--- tests/load/pipeline/test_pipelines.py
+++ tests/load/pipeline/test_pipelines.py
@@ -18,7 +18,7 @@ from dlt.pipeline.exceptions import CannotRestorePipelineException, PipelineConf
 from dlt.common.schema.exceptions import CannotCoerceColumnException
 from dlt.common.exceptions import DestinationHasFailedJobs
 
-from tests.utils import ALL_DESTINATIONS, TEST_STORAGE_ROOT, ALL_DESTINATIONS_SUBSET
+from tests.utils import ALL_DESTINATIONS, TEST_STORAGE_ROOT, ALL_DESTINATIONS_SUBSET, preserve_environ
 from tests.pipeline.utils import assert_load_info
 from tests.load.utils import TABLE_ROW_ALL_DATA_TYPES, TABLE_UPDATE_COLUMNS_SCHEMA, assert_all_data_types_row, delete_dataset
 from tests.load.pipeline.utils import drop_active_pipeline_data, assert_query_data, assert_table, load_table_counts, select_data
@@ -362,6 +362,9 @@ def test_pipeline_explicit_destination_credentials(destination_name: str) -> Non
     c = p._get_destination_client(Schema("s"), p._get_destination_client_initial_config())
     assert c.config.credentials.host == "localhost"
 
+    # Remove connection string in CI to start with clean environ
+    # TODO: may want to clear the env completely and ignore/mock config files somehow to avoid side effects
+    os.environ.pop("DESTINATION__POSTGRES__CREDENTIALS", None)
     # explicit credentials resolved ignoring the config providers
     os.environ["DESTINATION__POSTGRES__CREDENTIALS__HOST"] = "HOST"
     p = dlt.pipeline(destination="postgres", credentials="postgresql://loader:loader@localhost:5432/dlt_data")
@@ -370,6 +373,7 @@ def test_pipeline_explicit_destination_credentials(destination_name: str) -> Non
 
     # explicit partial credentials will use config providers
     os.environ["DESTINATION__POSTGRES__CREDENTIALS__USERNAME"] = "UN"
+    os.environ["DESTINATION__POSTGRES__CREDENTIALS__PASSWORD"] = "PW"
     p = dlt.pipeline(destination="postgres", credentials="postgresql://localhost:5432/dlt_data")
     c = p._get_destination_client(Schema("s"), p._get_destination_client_initial_config())
     assert c.config.credentials.username == "UN"
