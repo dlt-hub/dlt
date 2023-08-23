853a76bff5ddf54a8b2ae303c509ed32374177ee
Dave <shrps@posteo.net>
2023-08-23 15:58:12 +0200

    set and clean test storage on snippet execution

--- docs/snippets/utils.py
+++ docs/snippets/utils.py
@@ -1,8 +1,12 @@
 from typing import Dict, Any
+from dlt.common.utils import set_working_dir
+from tests.utils import TEST_STORAGE_ROOT, test_storage
 
 BASEPATH = "docs/snippets"
 
 def run_snippet(filename: str) -> Dict[str, Any]:
-    variables: Dict[str, Any] = {}
-    exec(open(f"{BASEPATH}/{filename}.py", encoding="utf-8").read(), variables)
-    return variables
+    code = open(f"{BASEPATH}/{filename}.py", encoding="utf-8").read()
+    with set_working_dir(TEST_STORAGE_ROOT):
+        variables: Dict[str, Any] = {}
+        exec(code, variables)
+        return variables
