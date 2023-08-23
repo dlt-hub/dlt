5535a0bf1c69725c38db5441d44bb57497446221
Dave <shrps@posteo.net>
2023-08-23 17:34:29 +0200

    pr comments

---
+++ docs/snippets/conftest.py
@@ -0,0 +1,3 @@
+from tests.utils import patch_home_dir, autouse_test_storage, preserve_environ
+
+
--- docs/snippets/utils.py
+++ docs/snippets/utils.py
@@ -1,12 +1,14 @@
 from typing import Dict, Any
+
 from dlt.common.utils import set_working_dir
+
 from tests.utils import TEST_STORAGE_ROOT, test_storage
 
 BASEPATH = "docs/snippets"
 
 def run_snippet(filename: str) -> Dict[str, Any]:
-    code = open(f"{BASEPATH}/{filename}.py", encoding="utf-8").read()
-    with set_working_dir(TEST_STORAGE_ROOT):
+    with set_working_dir(BASEPATH):
+        code = open(f"{filename}.py", encoding="utf-8").read()
         variables: Dict[str, Any] = {}
         exec(code, variables)
         return variables
