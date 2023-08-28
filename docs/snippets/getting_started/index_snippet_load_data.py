ce221119ffb52d13edd4dd37fdc7599cf4f173b3
alenaastrakhantseva <astra92293@gmail.com>
2023-08-28 16:12:25 +0200

    fix code blocks tabulation

--- docs/snippets/getting_started/index_snippet_load_data.py
+++ docs/snippets/getting_started/index_snippet_load_data.py
@@ -3,69 +3,69 @@ import tempfile
 import json
 
 # Create a temporary file in the system's temporary directory
-with tempfile.NamedTemporaryFile(mode='w') as temp_file:
-    # Create a dictionary with some sample JSON data
-    data = {'id': 1, 'name': 'Alice', 'children': {'id': 1, 'name': 'Eve'}}
+temp_file = tempfile.NamedTemporaryFile(mode='w+')
+# Create a dictionary with some sample JSON data
+data = {'id': 1, 'name': 'Alice', 'children': {'id': 1, 'name': 'Eve'}}
+# Write the JSON data to the temporary file
+json.dump(data, temp_file)
+# Get the path of the temporary file
+json_path = temp_file.name
+# Move the file pointer back to the beginning of the file
+temp_file.seek(0)
 
-    # Write the JSON data to the temporary file
-    json.dump(data, temp_file)
+# @@@SNIPSTART getting_started_index_snippet_json
+import json
+import dlt
 
-    # Get the path of the temporary file
-    json_path = temp_file.name
+with open(json_path, 'r') as file:
+    data = json.load(file)
 
-    # Move the file pointer back to the beginning of the file
-    temp_file.seek(0)
+pipeline = dlt.pipeline(
+    pipeline_name='from_json',
+    destination='duckdb',
+    dataset_name='mydata',
+)
+# dlt works with lists of dicts, so wrap data to the list
+load_info = pipeline.run([data], table_name="json_data")
+print(load_info)
+# @@@SNIPEND
 
-    # @@@SNIPSTART getting_started_index_snippet_json
-    import json
-    import dlt
-
-    with open(json_path, 'r') as file:
-        data = json.load(file)
-
-    pipeline = dlt.pipeline(
-        pipeline_name='from_json',
-        destination='duckdb',
-        dataset_name='mydata',
-    )
-    # dlt works with lists of dicts, so wrap data to the list
-    load_info = pipeline.run([data], table_name="json_data")
-    print(load_info)
-    # @@@SNIPEND
+# Close the temporary file manually
+temp_file.close()
 
 
 # Create a temporary file in the system's temporary directory
-with tempfile.NamedTemporaryFile(mode='w') as temp_file:
-    # create test csv file
-    import pandas as pd
+temp_file = tempfile.NamedTemporaryFile(mode='w+')
+# create test csv file
+import pandas as pd
+df = pd.DataFrame([
+    {'id': 1, 'name': 'Alice'},
+    {'id': 2, 'name': 'Bob'}
+])
+# Get the path of the temporary file and save
+csv_path = temp_file.name
+df.to_csv(csv_path, index=False)
+# Move the file pointer back to the beginning of the file
+temp_file.seek(0)
 
-    df = pd.DataFrame([
-        {'id': 1, 'name': 'Alice'},
-        {'id': 2, 'name': 'Bob'}
-    ])
-    # Get the path of the temporary file and save
-    csv_path = temp_file.name
-    df.to_csv(csv_path, index=False)
+# @@@SNIPSTART getting_started_index_snippet_csv
+import dlt
+import pandas as pd
 
-    # Move the file pointer back to the beginning of the file
-    temp_file.seek(0)
+df = pd.read_csv(csv_path)
+data = df.to_dict(orient='records')
 
-    # @@@SNIPSTART getting_started_index_snippet_csv
-    import dlt
-    import pandas as pd
+pipeline = dlt.pipeline(
+    pipeline_name='from_csv',
+    destination='duckdb',
+    dataset_name='mydata',
+)
+load_info = pipeline.run(data, table_name="csv_data")
+print(load_info)
+# @@@SNIPEND
 
-    df = pd.read_csv(csv_path)
-    data = df.to_dict(orient='records')
-
-    pipeline = dlt.pipeline(
-        pipeline_name='from_csv',
-        destination='duckdb',
-        dataset_name='mydata',
-    )
-    load_info = pipeline.run(data, table_name="csv_data")
-    print(load_info)
-    # @@@SNIPEND
-
+# Close the temporary file manually
+temp_file.close()
 
 
 # @@@SNIPSTART getting_started_index_snippet_api
