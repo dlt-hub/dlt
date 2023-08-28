e84bcfa83f12efee8541701410046f2a935d9c5e
Dave <shrps@posteo.net>
2023-08-28 12:44:36 +0200

    start adding tested snippets for getting started index page

---
+++ docs/snippets/getting_started/index_snippets_test.py
@@ -0,0 +1,9 @@
+
+
+
+from tests.pipeline.utils import assert_load_info
+from docs.snippets.utils import run_snippet
+
+def test_snippet_1() -> None:
+    variables = run_snippet("getting_started/index_snippet_1")
+    assert_load_info(variables["load_info"])
