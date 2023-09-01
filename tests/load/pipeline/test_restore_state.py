ea7eed0da082ad396b7a6637c55734ee7a795a6f
Alexander Butler <41213451+z3z1ma@users.noreply.github.com>
2023-09-01 08:27:30 -0700

    Feat: add time to pua serde (#604)
    * feat: add time to pua serde
    
    * bumps to 0.3.16a0
    
    * keeps times as string + tests
    
    * keeps times as string + tests
    
    ---------
    
    Co-authored-by: Marcin Rudolf <rudolfix@rudolfix.org>

--- tests/load/pipeline/test_restore_state.py
+++ tests/load/pipeline/test_restore_state.py
@@ -15,7 +15,7 @@ from dlt.pipeline.pipeline import Pipeline
 from dlt.pipeline.state_sync import STATE_TABLE_COLUMNS, STATE_TABLE_NAME, load_state_from_destination, state_resource
 
 from tests.utils import TEST_STORAGE_ROOT
-from tests.cases import JSON_TYPED_DICT
+from tests.cases import JSON_TYPED_DICT, JSON_TYPED_DICT_DECODED
 from tests.common.utils import IMPORTED_VERSION_HASH_ETH_V6, yml_case_path as common_yml_case_path
 from tests.common.configuration.utils import environment
 from tests.load.pipeline.utils import assert_query_data, drop_active_pipeline_data
@@ -81,11 +81,11 @@ def test_restore_state_utils(destination_config: DestinationTestConfiguration) -
         # extract state again
         with p.managed_state(extract_state=True) as managed_state:
             # this will be saved
-            managed_state["sources"] = {"source": dict(JSON_TYPED_DICT)}
+            managed_state["sources"] = {"source": dict(JSON_TYPED_DICT_DECODED)}
         p.normalize(loader_file_format=destination_config.file_format)
         p.load()
         stored_state = load_state_from_destination(p.pipeline_name, job_client.sql_client)
-        assert stored_state["sources"] == {"source": JSON_TYPED_DICT}
+        assert stored_state["sources"] == {"source": JSON_TYPED_DICT_DECODED}
         local_state = p._get_state()
         local_state.pop("_local")
         assert stored_state == local_state
@@ -241,7 +241,7 @@ def test_restore_state_pipeline(destination_config: DestinationTestConfiguration
     def source_four():
         @dlt.resource
         def some_data():
-            dlt.current.source_state()["state5"] = dict(JSON_TYPED_DICT)
+            dlt.current.source_state()["state5"] = dict(JSON_TYPED_DICT_DECODED)
             yield "four"
 
         return some_data()
@@ -280,7 +280,7 @@ def test_restore_state_pipeline(destination_config: DestinationTestConfiguration
     assert p.default_schema_name == "default"
     assert set(p.schema_names) == set(["default", "two", "three", "four"])
     assert p.state["sources"] == {
-        "default": {'state1': 'state1', 'state2': 'state2'}, "two": {'state3': 'state3'}, "three": {'state4': 'state4'}, "four": {"state5": JSON_TYPED_DICT}
+        "default": {'state1': 'state1', 'state2': 'state2'}, "two": {'state3': 'state3'}, "three": {'state4': 'state4'}, "four": {"state5": JSON_TYPED_DICT_DECODED}
     }
     for schema in p.schemas.values():
         assert "some_data" in schema.tables
