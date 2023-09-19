7a0edb8765d7d2a06613fdc26d89aca50168f1bf
Marcin Rudolf <rudolfix@rudolfix.org>
2023-09-19 21:52:54 +0200

    Revert "avoid import/export first word in docstrings"
    This reverts commit f313c6bd7ca5db2c95c50a8a4c6dbcebd68d7638.

--- dlt/common/storages/configuration.py
+++ dlt/common/storages/configuration.py
@@ -14,8 +14,8 @@ SchemaFileExtensions = get_args(TSchemaFileFormat)
 @configspec
 class SchemaStorageConfiguration(BaseConfiguration):
     schema_volume_path: str = None  # path to volume with default schemas
-    import_schema_path: Optional[str] = None  # the import schema from external location
-    export_schema_path: Optional[str] = None  # the export schema to external location
+    import_schema_path: Optional[str] = None  # import schema from external location
+    export_schema_path: Optional[str] = None  # export schema to external location
     external_schema_format: TSchemaFileFormat = "yaml"  # format in which to expect external schema
     external_schema_format_remove_defaults: bool = True  # remove default values when exporting schema
 
