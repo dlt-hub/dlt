62ce595148ef2db8fa673c1864e242bea9629590
DhrruvRM <113296128+DhrruvRM@users.noreply.github.com>
2023-09-21 10:47:49 +0530

    Update escape.py for synapse

--- dlt/common/data_writers/escape.py
+++ dlt/common/data_writers/escape.py
@@ -88,15 +88,13 @@ def escape_mssql_literal(v: Any) -> Any:
         return str(int(v))
     return str(v)
 
-
-
 def escape_synapse_literal(v: Any) -> Any:
     if isinstance(v, str):
-         return _escape_extended(v, prefix="N'", escape_dict=MS_SQL_ESCAPE_DICT, escape_re=MS_SQL_ESCAPE_RE)
+         return _escape_extended(v, prefix="N'", escape_dict=SYNAPSE_ESCAPE_DICT, escape_re=SYNAPSE_ESCAPE_RE)
     if isinstance(v, (datetime, date, time)):
         return f"'{v.isoformat()}'"
     if isinstance(v, (list, dict)):
-        return _escape_extended(json.dumps(v), prefix="N'", escape_dict=MS_SQL_ESCAPE_DICT, escape_re=MS_SQL_ESCAPE_RE)
+        return _escape_extended(json.dumps(v), prefix="N'", escape_dict=SYNAPSE_ESCAPE_DICT, escape_re=SYNAPSE_ESCAPE_RE)
     if isinstance(v, bytes):
         # Updated to hex: azure synapse doesn't have XML and base64Binary
         hex_string = v.hex()
