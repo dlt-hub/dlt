4021987db2a96f899f10e5a2c2976779dd774c9c
Steinthor Palsson <steini90@gmail.com>
2023-09-22 19:36:23 -0400

    Precision in parquet writer

--- dlt/common/libs/pyarrow.py
+++ dlt/common/libs/pyarrow.py
@@ -1,8 +1,9 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, Optional
 from dlt import version
 from dlt.common.exceptions import MissingDependencyException
 
 from dlt.common.destination.capabilities import DestinationCapabilitiesContext
+from dlt.common.schema.typing import TColumnType
 
 try:
     import pyarrow
@@ -11,7 +12,8 @@ except ModuleNotFoundError:
     raise MissingDependencyException("DLT parquet Helpers", [f"{version.DLT_PKG_NAME}[parquet]"], "DLT Helpers for for parquet.")
 
 
-def get_py_arrow_datatype(column_type: str, caps: DestinationCapabilitiesContext, tz: str) -> Any:
+def get_py_arrow_datatype(column: TColumnType, caps: DestinationCapabilitiesContext, tz: str) -> Any:
+    column_type = column["data_type"]
     if column_type == "text":
         return pyarrow.string()
     elif column_type == "double":
@@ -19,22 +21,24 @@ def get_py_arrow_datatype(column_type: str, caps: DestinationCapabilitiesContext
     elif column_type == "bool":
         return pyarrow.bool_()
     elif column_type == "timestamp":
-        return get_py_arrow_timestamp(caps.timestamp_precision, tz)
+        return get_py_arrow_timestamp(column.get("precision") or caps.timestamp_precision, tz)
     elif column_type == "bigint":
-        return pyarrow.int64()
+        return get_pyarrow_int(column.get("precision"))
     elif column_type == "binary":
-        return pyarrow.binary()
+        return pyarrow.binary(column.get("precision") or -1)
     elif column_type == "complex":
         # return pyarrow.struct([pyarrow.field('json', pyarrow.string())])
         return pyarrow.string()
     elif column_type == "decimal":
-        return get_py_arrow_numeric(caps.decimal_precision)
+        precision, scale = column.get("precision"), column.get("scale")
+        precision_tuple = (precision, scale) if precision is not None and scale is not None else caps.decimal_precision
+        return get_py_arrow_numeric(precision_tuple)
     elif column_type == "wei":
         return get_py_arrow_numeric(caps.wei_precision)
     elif column_type == "date":
         return pyarrow.date32()
     elif column_type == "time":
-        return get_py_arrow_time(caps.timestamp_precision)
+        return get_py_arrow_time(column.get("precision") or caps.timestamp_precision)
     else:
         raise ValueError(column_type)
 
@@ -66,3 +70,15 @@ def get_py_arrow_numeric(precision: Tuple[int, int]) -> Any:
         return pyarrow.decimal256(*precision)
     # for higher precision use max precision and trim scale to leave the most significant part
     return pyarrow.decimal256(76, max(0, 76 - (precision[0] - precision[1])))
+
+
+def get_pyarrow_int(precision: Optional[int]) -> Any:
+    if precision is None:
+        return pyarrow.int64()
+    if precision <= 8:
+        return pyarrow.int8()
+    elif precision <= 16:
+        return pyarrow.int16()
+    elif precision <= 32:
+        return pyarrow.int32()
+    return pyarrow.int64()
