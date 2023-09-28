9eed6dbae30e9310a6f90746e9a671ed6ff6225a
NeilGorman104 <neil@ryan-miranda.com>
2023-09-28 14:41:02 -0400

    update begin transaction autocommit to false

--- dlt/destinations/synapse/sql_client.py
+++ dlt/destinations/synapse/sql_client.py
@@ -55,7 +55,7 @@ class PyOdbcSynapseClient(SqlClientBase[pyodbc.Connection], DBTransaction):
     @contextmanager
     def begin_transaction(self) -> Iterator[DBTransaction]:
         try:
-            self._conn.autocommit = True
+            self._conn.autocommit = False
             yield self
             self.commit_transaction()
         except Exception:
