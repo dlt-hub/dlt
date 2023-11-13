1e9992d9406616fb7fa742c545fa18fdbd40ad5d
Evan Phillips <phillem15@gmail.com>
2023-11-12 18:32:41 -0600

    Update file formats for Databricks staging

--- dlt/destinations/databricks/__init__.py
+++ dlt/destinations/databricks/__init__.py
@@ -20,8 +20,8 @@ def capabilities() -> DestinationCapabilitiesContext:
     caps = DestinationCapabilitiesContext()
     caps.preferred_loader_file_format = "jsonl"
     caps.supported_loader_file_formats = ["jsonl", "parquet"]
-    caps.preferred_staging_file_format = "jsonl"
-    caps.supported_staging_file_formats = ["jsonl", "parquet"]
+    caps.preferred_staging_file_format = "parquet"
+    caps.supported_staging_file_formats = ["parquet", "jsonl"]
     caps.escape_identifier = escape_databricks_identifier
     caps.decimal_precision = (DEFAULT_NUMERIC_PRECISION, DEFAULT_NUMERIC_SCALE)
     caps.wei_precision = (DEFAULT_NUMERIC_PRECISION, 0)
