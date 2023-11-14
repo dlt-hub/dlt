3bde2f49ea873382944483ff71abdf55d953d81b
Steinthor Palsson <steini90@gmail.com>
2023-11-13 20:21:58 -0500

    bigquery, athena factories

---
+++ dlt/destinations/impl/athena/factory.py
@@ -0,0 +1,35 @@
+import typing as t
+
+from dlt.common.destination import Destination, DestinationCapabilitiesContext
+from dlt.destinations.impl.athena.configuration import AthenaClientConfiguration
+from dlt.common.configuration.specs import AwsCredentials
+from dlt.destinations.impl.athena import capabilities
+
+if t.TYPE_CHECKING:
+    from dlt.destinations.impl.athena.athena import AthenaClient
+
+
+class athena(Destination):
+
+    spec = AthenaClientConfiguration
+
+    def capabilities(self) -> DestinationCapabilitiesContext:
+        return capabilities()
+
+    @property
+    def client_class(self) -> t.Type["AthenaClient"]:
+        from dlt.destinations.impl.athena.athena import AthenaClient
+
+        return AthenaClient
+
+    def __init__(
+        self,
+        query_result_bucket: t.Optional[str] = None,
+        credentials: t.Optional[AwsCredentials] = None,
+        athena_work_group: t.Optional[str] = None,
+        aws_data_catalog: t.Optional[str] = "awsdatacatalog",
+        supports_truncate_command: bool = False,
+        force_iceberg: bool = False,
+        **kwargs: t.Any,
+    ) -> None:
+        super().__init__(**kwargs)
---
+++ dlt/destinations/impl/bigquery/factory.py
@@ -0,0 +1,69 @@
+# import typing as t
+
+# from dlt.destinations.impl.snowflake.configuration import SnowflakeCredentials, SnowflakeClientConfiguration
+# from dlt.destinations.impl.snowflake import capabilities
+# from dlt.common.destination import Destination, DestinationCapabilitiesContext
+# from dlt.common.destination import DestinationCapabilitiesContext
+
+# if t.TYPE_CHECKING:
+#     from dlt.destinations.impl.snowflake.snowflake import SnowflakeClient
+
+
+# class snowflake(Destination):
+
+#     spec = SnowflakeClientConfiguration
+
+#     def capabilities(self) -> DestinationCapabilitiesContext:
+#         return capabilities()
+
+#     @property
+#     def client_class(self) -> t.Type["SnowflakeClient"]:
+#         from dlt.destinations.impl.snowflake.snowflake import SnowflakeClient
+
+#         return SnowflakeClient
+
+#     def __init__(
+#         self,
+#         credentials: SnowflakeCredentials = None,
+#         stage_name: t.Optional[str] = None,
+#         keep_staged_files: bool = True,
+#         **kwargs: t.Any,
+#     ) -> None:
+#         super().__init__(credentials=credentials, stage_name=stage_name, keep_staged_files=keep_staged_files, **kwargs)
+
+
+import typing as t
+
+from dlt.destinations.impl.bigquery.configuration import BigQueryClientConfiguration
+from dlt.common.configuration.specs import GcpServiceAccountCredentials
+from dlt.destinations.impl.bigquery import capabilities
+from dlt.common.destination import Destination, DestinationCapabilitiesContext
+
+if t.TYPE_CHECKING:
+    from dlt.destinations.impl.bigquery.bigquery import BigQueryClient
+
+
+class bigquery(Destination):
+
+    spec = BigQueryClientConfiguration
+
+    def capabilities(self) -> DestinationCapabilitiesContext:
+        return capabilities()
+
+    @property
+    def client_class(self) -> t.Type["BigQueryClient"]:
+        from dlt.destinations.impl.bigquery.bigquery import BigQueryClient
+
+        return BigQueryClient
+
+    def __init__(
+        self,
+        credentials: t.Optional[GcpServiceAccountCredentials] = None,
+        location: t.Optional[str] = None,
+        **kwargs: t.Any,            
+    ) -> None:
+        super().__init__(
+            credentials=credentials,
+            location=location,
+            **kwargs
+        )
