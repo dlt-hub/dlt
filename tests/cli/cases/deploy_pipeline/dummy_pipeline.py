a261ca92ea0e544884d93861b507fc421b22188f
Marcin Rudolf <rudolfix@rudolfix.org>
2023-11-18 15:15:20 +0100

    adds debug flag to cli to show exceptions

---
+++ tests/cli/cases/deploy_pipeline/dummy_pipeline.py
@@ -0,0 +1,20 @@
+import dlt
+
+
+@dlt.resource
+def example_resource(api_url=dlt.config.value, api_key=dlt.secrets.value, last_id=0):
+    yield [api_url, api_key, str(last_id), "param4", "param5"]
+
+
+@dlt.source
+def example_source(api_url=dlt.config.value, api_key=dlt.secrets.value, last_id = 0):
+    # return all the resources to be loaded
+    return example_resource(api_url, api_key, last_id)
+
+
+if __name__ == '__main__':
+    p = dlt.pipeline(pipeline_name="dummy_pipeline", destination="dummy")
+    load_info = p.run(
+        example_source(last_id=819273998)
+    )
+    print(load_info)
