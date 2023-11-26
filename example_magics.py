1807ffbe3e299e687eb1c1c222a04225b41917df
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2023-11-26 21:10:25 +0100

    Added initial version of logic for the notebook magics that work when DLT is loaded

---
+++ example_magics.py
@@ -0,0 +1,24 @@
+import dlt
+# from dlt.cli.init_command import init_command
+#
+# from dlt.helpers.magics import register_magics
+from dlt.cli import echo as fmt
+from IPython import get_ipython
+
+from IPython.terminal.interactiveshell import InteractiveShell
+# register_magics()
+
+shell = get_ipython()
+
+fmt.echo("got here234234")
+
+
+# source_name='chess', destination_name='duckdb', use_generic_template=True, repo_location='https://github.com/dlt-hub/verified-sources')
+
+
+# Example of running a magic command
+oo = shell.run_line_magic('init',"--source_name='chess' --destination_name='duckdb' --use_generic_template=True --repo_location='https://github.com/dlt-hub/verified-sources'" )
+
+print(oo)
+
+#init_command(source_name='chess', destination_name='duckdb', use_generic_template=True, repo_location='https://github.com/dlt-hub/verified-sources')
