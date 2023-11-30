2c69cc29deea48a9b37ceb9a8ae100735df97a27
AstrakhantsevaAA <astra92293@gmail.com>
2023-11-30 12:01:42 +0100

    return unused imports back

--- docs/website/docs/conftest.py
+++ docs/website/docs/conftest.py
@@ -1,13 +1,16 @@
 import os
 import pytest
+from typing import List
 from unittest.mock import patch
 
 from dlt.common.utils import set_working_dir
 from dlt.common.configuration.container import Container
 
 # patch which providers to enable
-from dlt.common.configuration.providers import EnvironProvider, SecretsTomlProvider, ConfigTomlProvider
-from dlt.common.configuration.specs.config_providers_context import ConfigProvidersContext
+from dlt.common.configuration.providers import StringTomlProvider, EnvironProvider, SecretsTomlProvider, ConfigTomlProvider
+from dlt.common.configuration.specs.config_providers_context import ConfigProvidersContext, ConfigProvidersConfiguration
+
+from tests.utils import patch_home_dir, autouse_test_storage, preserve_environ, duckdb_pipeline_location, wipe_pipeline
 
 
 @pytest.fixture(autouse=True)
