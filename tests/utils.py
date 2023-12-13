d6d84852f0131f0147bf7f42594b466919ca4945
IlyaFaer <gurovilya9@gmail.com>
2023-12-13 08:48:34 +0400

    test vars

--- tests/utils.py
+++ tests/utils.py
@@ -249,6 +249,7 @@ def is_running_in_github_fork() -> bool:
     is_github_actions = os.environ.get("GITHUB_ACTIONS") == "true"
     head_ref = os.environ.get("GITHUB_HEAD_REF", "")
     repo = os.environ.get("GITHUB_REPOSITORY", "")
+    raise ValueError(is_github_actions, head_ref, repo)
     return is_github_actions and ":" in head_ref and not head_ref.startswith(repo.split("/")[0])
 
 
