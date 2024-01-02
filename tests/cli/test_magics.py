1d6d6df29336a4e36fc5022cb010e46704dcb3ee
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-01-02 18:22:17 +0100

    Next step for tests

--- tests/cli/test_magics.py
+++ tests/cli/test_magics.py
@@ -1,17 +1,23 @@
 
 import pytest
+from IPython import get_ipython
 import dlt
 
-@pytest.fixture
-def magic_shell():
-    # Create a mock IPython environment
-    shell = get_ipython()
-    return shell
+# @pytest.fixture
+# def magic_shell():
+#     # Create a mock IPython environment
+#     shell = get_ipython()
+#     return shell
 
-def test_init_command(magic_shell):
+
+def test_init_command():
     # Run the init magic command
-    shell= magic_shell()
+    shell= get_ipython()
     result = shell.run_line_magic('init', "--source_name=chess --destination_name=duckdb --use_generic_template --repo_location=https://github.com/dlt-hub/verified-sources")
 
     # Check if the init command returns the expected result
     assert result == 0
+
+
+test_init_command()
+
