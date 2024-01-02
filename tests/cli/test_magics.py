65778b01b72a3066fbd892313a5b9ee3af190c64
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2024-01-02 23:05:01 +0100

    Fix notebook install

--- tests/cli/test_magics.py
+++ tests/cli/test_magics.py
@@ -10,13 +10,23 @@ import dlt
 #     return shell
 
 
+
+
+
 def test_init_command():
     # Run the init magic command
-    shell= get_ipython()
-    result = shell.run_line_magic('init', "--source_name=chess --destination_name=duckdb --use_generic_template --repo_location=https://github.com/dlt-hub/verified-sources")
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
+    # import dlt
+    from dlt.cli.magics import register_notebook_magics
 
+    shell = TerminalInteractiveShell()
+    shell.run_cell("import dlt")
+    shell.run_cell("from dlt.cli.magics import register_notebook_magics")
+    shell.run_cell("register_notebook_magics()")
+    result = shell.run_line_magic('init', "--source_name=chess --destination_name=duckdb --use_generic_template --repo_location=")
+    print("result is", result)
     # Check if the init command returns the expected result
-    assert result == 0
+    assert result == -1
 
 
 test_init_command()
