e6bce28508101624252e0959b87e31ee1f405745
dat-a-man <amanguptanalytics@gmail.com>
2024-01-22 19:08:06 +0530

    Added data enrichment part one documentation

--- docs/website/sidebars.js
+++ docs/website/sidebars.js
@@ -211,6 +211,13 @@ const sidebars = {
         },
         {
           type: 'category',
+          label: 'Data enrichments',
+          items: [
+            'general-usage/data-enrichments/user_agent_device_data_enrichment',
+          ]
+        },
+        {
+          type: 'category',
           label: 'Running in production',
           link: {
             type: 'generated-index',
