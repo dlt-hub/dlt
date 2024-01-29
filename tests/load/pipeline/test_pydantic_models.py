9acab6257171e55f695dd4ac026a46144e4355f2
Sultan Iman <sultan@dlthub.com>
2024-01-29 16:55:49 +0100

    Remove test

--- tests/load/pipeline/test_pydantic_models.py
+++ tests/load/pipeline/test_pydantic_models.py
@@ -1,4 +1,3 @@
-import json
 import dlt
 import typing as t
 
@@ -40,36 +39,3 @@ def test_flattened_model() -> None:
 
     # this is defined in the pydantic schema but does not end up in the resulting schema
     assert "child__optional_child_attribute" in p.default_schema.tables["items"]["columns"].keys()
-
-
-def test_list_model() -> None:
-    class Child(BaseModel):
-        child_attribute: str
-        optional_child_attribute: t.Optional[str] = None
-
-    class Parent(BaseModel):
-        children: t.List[Child]
-        optional_parent_attribute: t.Optional[str] = None
-        dlt_config: t.ClassVar[DltConfig] = {"skip_complex_types": True}
-
-    example_data = {
-        "optidonal_parent_attribute": None,
-        "children": [{"child_attribute": "any string", "optional_child_attribute": None}],
-    }
-
-    p = dlt.pipeline("items", full_refresh=True, destination="duckdb")
-    p.run([example_data], table_name="items", columns=Parent)
-
-    # in the parent this works
-    assert "optional_parent_attribute" in p.default_schema.tables["items"]["columns"].keys()
-
-    # we now now have a child table
-    assert "items__children" in p.default_schema.tables.keys()
-
-    # but it does not have the right columns
-    assert (
-        "child_attribute" in p.default_schema.tables["items__children"]["columns"].keys()
-    )  # works
-    assert (
-        "optional_child_attribute" in p.default_schema.tables["items__children"]["columns"].keys()
-    )  # fails
