dd8e766546b723a27650a9c86930ec2059b46a30
Sultan Iman <sultan@dlthub.com>
2024-02-06 16:32:35 +0100

    Remove duplicate test

--- tests/load/pipeline/test_pydantic_models.py
+++ tests/load/pipeline/test_pydantic_models.py
@@ -50,6 +50,8 @@ def test_flattens_model_when_skip_complex_types_is_set() -> None:
             }
 
     keys = p.default_schema.tables["items"]["columns"].keys()
+    columns = p.default_schema.tables["items"]["columns"]
+
     assert keys == {
         "child__child_attribute",
         "child__optional_child_attribute",
@@ -58,7 +60,6 @@ def test_flattens_model_when_skip_complex_types_is_set() -> None:
         "_dlt_id",
     }
 
-    columns = p.default_schema.tables["items"]["columns"]
     assert columns["child__child_attribute"] == {
         "name": "child__child_attribute",
         "data_type": "text",
@@ -75,73 +76,4 @@ def test_flattens_model_when_skip_complex_types_is_set() -> None:
         "name": "optional_parent_attribute",
         "data_type": "text",
         "nullable": True,
-    }
-
-
-def test_flattens_model_when_skip_complex_types_is_not_set():
-    class Parent(BaseModel):
-        child: Child
-        optional_parent_attribute: t.Optional[str] = None
-        data_dictionary: t.Dict[str, t.Any] = None
-        dlt_config: t.ClassVar[DltConfig] = {"skip_complex_types": False}
-
-    example_data = {
-        "optional_parent_attribute": None,
-        "data_dictionary": {
-            "child_attribute": "any string",
-        },
-        "child": {
-            "child_attribute": "any string",
-            "optional_child_attribute": None,
-        },
-    }
-
-    @dlt.resource
-    def res():
-        yield [example_data]
-
-    @dlt.source(max_table_nesting=1)
-    def src():
-        yield res()
-
-    p = dlt.pipeline("example", full_refresh=True, destination="duckdb")
-    p.run(src(), table_name="items", columns=Parent)
-
-    with p.sql_client() as client:
-        with client.execute_query("SELECT * FROM items") as cursor:
-            loaded_values = {
-                col[0]: val
-                for val, col in zip(cursor.fetchall()[0], cursor.description)
-                if col[0] not in ("_dlt_id", "_dlt_load_id")
-            }
-
-            assert loaded_values == {
-                "child__child_attribute": "any string",
-                "child__optional_child_attribute": None,
-                "optional_parent_attribute": None,
-                "data_dictionary": '{"child_attribute":"any string"}',
-            }
-
-    keys = p.default_schema.tables["items"]["columns"].keys()
-    assert keys == {
-        "child__child_attribute",
-        "child__optional_child_attribute",
-        "optional_parent_attribute",
-        "data_dictionary",
-        "_dlt_load_id",
-        "_dlt_id",
-    }
-
-    columns = p.default_schema.tables["items"]["columns"]
-
-    assert columns["optional_parent_attribute"] == {
-        "name": "optional_parent_attribute",
-        "data_type": "text",
-        "nullable": True,
-    }
-
-    assert columns["data_dictionary"] == {
-        "name": "data_dictionary",
-        "data_type": "complex",
-        "nullable": False,
     }
