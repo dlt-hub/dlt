61ce130a567381f803fe3ff5394ccd5e3d696716
Dave <shrps@posteo.net>
2024-02-23 16:55:58 +0100

    update prompts

--- fix_grammar.py
+++ fix_grammar.py
@@ -19,7 +19,12 @@ if __name__ == "__main__":
             response = client.chat.completions.create(
                 model="gpt-4",
                 messages=[
-                    {"role": "system", "content": "You are a grammar checker. Every message you get will be a document that is to be grammarchecked and returned as such. You will not change the markdown syntax. You will only fix the grammar. You will not change the code snippets except for code snippets therein."},
+                    {"role": "system", "content": """
+You are a grammar checker. Every message you get will be a document that is to be grammarchecked and returned as such.
+You will not change the markdown syntax. You will only fix the grammar. You will not change the code snippets except for code snippets therein.
+You will not modify the header section which is enclosed by two occurences of "---". 
+Do not change the spelling or casing of these words: dlt, sdf, dbt
+"""},
                     {"role": "user", "content": doc},
                 ],
                 temperature=0,
