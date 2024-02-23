4cd3474e0df9f0ef07a099fddf00092bf874cd6e
Dave <shrps@posteo.net>
2024-02-23 16:32:11 +0100

    script

---
+++ fix_grammar.py
@@ -0,0 +1,38 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    for root, dirs, files in os.walk("./docs/website/docs", topdown=False):
+        for name in files:
+            if not name.endswith(".md"):
+                continue
+
+            filename = os.path.join(root, name)
+            with open(filename, "r") as f:
+                doc = f.read()
+            
+            print("fixing grammar for", filename)
+            client = OpenAI()
+
+            response = client.chat.completions.create(
+                model="gpt-4",
+                messages=[
+                    {"role": "system", "content": "You are a grammar checker. Every message you get will be a document that is to be grammarchecked and returned as such. You will not change the markdown syntax. You will only fix the grammar. You will not change the code snippets except for code snippets therein."},
+                    {"role": "user", "content": doc},
+                ],
+                temperature=0,
+            )
+
+            fixed_doc = response.choices[0].message.content
+
+            fixed_doc = fixed_doc + """
+<!---
+grammarcheck: true
+-->
+"""
+
+            with open(filename, "w") as f:
+                f.writelines(fixed_doc)
+
