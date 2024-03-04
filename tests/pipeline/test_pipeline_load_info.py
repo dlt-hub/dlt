1ad601a8f1d89f7a9b65d71b40eb10c63fdef392
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-03-04 14:35:49 +0100

    Add test to check if version_hash is consistent for load info schema

--- tests/pipeline/test_pipeline_load_info.py
+++ tests/pipeline/test_pipeline_load_info.py
@@ -9,14 +9,15 @@ def get_metrics_table(tables: List[Dict]) -> List[str]:
     ]
 
 
-def test_pipeline_load_info_metrics_is_always_a_single_table() -> None:
-    data = [
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-    ]
+data = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+]
 
+
+def test_pipeline_load_info_metrics_is_always_a_single_table_e2e() -> None:
     pipeline = dlt.pipeline(
-        pipeline_name="quick_start",
+        pipeline_name="quick_start_e2e",
         destination="duckdb",
         dataset_name="mydata",
     )
@@ -32,3 +33,22 @@ def test_pipeline_load_info_metrics_is_always_a_single_table() -> None:
 
     assert len(metrics_tables_first_run) == len(metrics_tables_second_run)
     assert metrics_tables_first_run == metrics_tables_second_run
+
+
+def test_pipeline_load_info_metrics_schema_is_not_chaning() -> None:
+    pipeline = dlt.pipeline(
+        pipeline_name="quick_start",
+        destination="duckdb",
+        dataset_name="mydata",
+    )
+
+    load_info = pipeline.run(data, table_name="users")
+
+    pipeline.run([load_info], table_name="_load_info")
+    first_version_hash = pipeline.default_schema.version_hash
+
+    load_info = pipeline.run(data, table_name="users")
+    pipeline.run([load_info], table_name="_load_info")
+    second_version_hash = pipeline.default_schema.version_hash
+
+    assert first_version_hash == second_version_hash
