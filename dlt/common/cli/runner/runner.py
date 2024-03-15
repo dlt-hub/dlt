756ed577868941064b9815af059600a85c368d12
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-03-15 13:20:51 +0100

    Show warning if current directory is different from the one of pipeline script

--- dlt/common/cli/runner/runner.py
+++ dlt/common/cli/runner/runner.py
@@ -1,3 +1,4 @@
+import os
 import typing as t
 
 from dlt.cli import echo as fmt
@@ -5,14 +6,14 @@ from dlt.common.cli.runner.inquirer import Inquirer
 from dlt.common.pipeline import LoadInfo
 from dlt.common.cli.runner.errors import FriendlyExit, RunnerError
 from dlt.common.cli.runner.pipeline_script import PipelineScript
-from dlt.common.cli.runner.types import PipelineMembers, RunnerInventory
+from dlt.common.cli.runner.types import PipelineMembers, RunnerParams
 
 
 class PipelineRunner:
-    def __init__(self, inventory: RunnerInventory) -> None:
-        self.inventory = inventory
-        self.script = PipelineScript(inventory)
-        self.yolo = self.inventory.pipeline_name is None and self.inventory.source_name is None
+    def __init__(self, params: RunnerParams, current_dir: str) -> None:
+        self.params = params
+        self.current_dir = current_dir
+        self.script = PipelineScript(params)
 
     def __enter__(self) -> t.Self:
         self.check_if_runnable(self.script.pipeline_members)
@@ -34,7 +35,14 @@ class PipelineRunner:
 
     def check_if_runnable(self, pipeline_members: PipelineMembers) -> None:
         if not pipeline_members["pipelines"]:
-            raise RunnerError(f"No pipelines found in {self.inventory.script_path}")
+            raise RunnerError(f"No pipelines found in {self.params.script_path}")
 
         if not pipeline_members["sources"]:
-            raise RunnerError(f"Could not find any source or resource {self.inventory.script_path}")
+            raise RunnerError(f"Could not find any source or resource {self.params.script_path}")
+
+    def check_workdir(self):
+        if self.current_dir != self.script.workdir:
+            fmt.warning(
+                "Current working directory is different from the pipeline script"
+                f" {self.script.workdir}"
+            )
