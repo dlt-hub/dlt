1a197467d437011c4e4b855ecd0c75b5933f81da
Dave <shrps@posteo.net>
2024-03-18 14:56:10 +0100

    small script changes

--- check-lockfile.sh
+++ check-lockfile.sh
@@ -2,24 +2,24 @@
 
 # searching for the string "hash = " in poetry.lock file will
 # help us to verify if the lockfile is in the right order
-FILENAME="poetry.lock"
-SEARCH_STRING="hash = "
+LOCKFILENAME="poetry.lock"
+HASH_STRING="hash = "
 
 # Check if the file exists
-if [ ! -f "$FILENAME" ]; then
-    echo "Error: File $FILENAME does not exist."
+if [ ! -f "$LOCKFILENAME" ]; then
+    echo "Error: File $LOCKFILENAME does not exist."
     exit 1
 fi
 
 # Count the occurrences of the string
-COUNT=$(grep -o "$SEARCH_STRING" "$FILENAME" | wc -l)
+COUNT=$(grep -o "$HASH_STRING" "$LOCKFILENAME" | wc -l)
 
-# Check if the string appears less than 10 times
+# Check if the string appears less than 100 times, if so, then error
 if [ $COUNT -lt 100 ]; then
-    echo "Error: The string '$SEARCH_STRING' does not appear in $FILENAME, please make sure you are using an up to date poetry version."
+    echo "Error: The string '$HASH_STRING' does not appear in $LOCKFILENAME, please make sure you are using an up to date poetry version."
     exit 1
 else
-    echo "The string '$SEARCH_STRING' appears $COUNT times in $FILENAME."
+    echo "The string '$HASH_STRING' appears $COUNT times in $LOCKFILENAME. All good!"
 fi
 
 # Exit normally
