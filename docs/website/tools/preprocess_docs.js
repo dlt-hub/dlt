10175244c8790bfd690977a799a5a62caac47943
Dave <shrps@posteo.net>
2024-03-20 14:29:32 +0100

    update script to insert snippets

--- docs/website/tools/preprocess_docs.js
+++ docs/website/tools/preprocess_docs.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const watch = require('node-watch');
-const fetch = require('sync-fetch')
+const fetch = require('sync-fetch');
+const { get } = require('http');
+const dedent = require('dedent');
 
 // constants
 const MD_SOURCE_DIR = "docs/";
@@ -10,18 +12,28 @@ const MD_TARGET_DIR = "docs_processed/";
 const MOVE_FILES_EXTENSION = [".md", ".mdx", ".py", ".png", ".jpg", ".jpeg"];
 const DOCS_EXTENSIONS = [".md", ".mdx"];
 
-// markers
-const TUBA_MARKER = "@@@DLT_TUBA";
-const DLT_MARKER = "@@@DLT";
+const SNIPPETS_FILE_SUFFIX = "-snippets.py"
 
-// fetch tuba config. TODO: make this fail gracefully
+// markers
+const DLT_MARKER = "@@@DLT";
+const TUBA_MARKER = `${DLT_MARKER}_TUBA`;
+const SNIPPET_MARKER = `${DLT_MARKER}_SNIPPET_START`;
+const SNIPPET_START_MARKER = `${DLT_MARKER}_SNIPPET_START`;
+const SNIPPET_END_MARKER = `${DLT_MARKER}_SNIPPET_END`;
+
+/**
+ * Fetch tuba config
+ */
 const tubaConfig = fetch('https://dlthub.com/docs/pipelines/links.json', {
   headers: {
     Accept: 'application/vnd.citationstyles.csl+json'
   }
 }).json();
 
-// yield all files in dir
+
+/**
+ * Yield all files in docs dir
+ */
 function *walkSync(dir) {
     const files = fs.readdirSync(dir, { withFileTypes: true });
     for (const file of files) {
@@ -33,20 +45,116 @@ function *walkSync(dir) {
     }
   }
 
-// takes a line with a dlt marker and returns only the trimmed content
-function extractMarkerContent(line, marker) {
-  line = line.replace("-->", "");
-  line = line.replace("<!--", "");
-  line = line.replace(marker, "");
-  line = line.trim();
-  return line
+
+/**
+ * Extract the snippet or tuba tag name from a line
+ */
+const extractMarkerContent = (tag, line) => {
+  if (line && line.includes(tag)) {
+      // strip out md start and end comments
+      line = line.replace("-->", "");
+      line = line.replace("<!--", "");
+      const words = line.split(" ");
+      const tagIndex = words.findIndex(w => w==tag);
+      return words[tagIndex+1].trim();
+  }
+  return undefined;
 }
 
+/**
+ * Runs through an array or lines from a file and builds a map of found snippets
+ */
+function buildSnippetMap(lines, fileName) {
+  const snippetMap = {};
+  for (let lineIndex in lines) {
+      let line = lines[lineIndex];
+      let snippetName;
+      line.trimEnd();
+      if (snippetName = extractMarkerContent(SNIPPET_START_MARKER, line)) {
+          snippetMap[snippetName] = {
+              start: parseInt(lineIndex),
+          };
+      }
+      if (snippetName = extractMarkerContent(SNIPPET_END_MARKER, line)) {
+          if (snippetName in snippetMap) {
+              snippetMap[snippetName]["end"] = parseInt(lineIndex);
+          } else {
+              throw new Error(`Found end tag for snippet "${snippetName}" but start tag not found! File ${fileName}.`);
+          }
+      }
+  }
+  return snippetMap;
+}
+
+/** 
+ * Get snippet from file
+ */
+function getSnippetFromFile(snippetsFileName, snippetName) {
+  const lines = fs.readFileSync(snippetsFileName, 'utf8').split(/\r?\n/);
+  const snippetMap = buildSnippetMap(lines, snippetsFileName);
+
+  if (!(snippetName in snippetMap)) {
+      return undefined;
+  }
+
+  let result = lines.slice((snippetMap[snippetName]["start"]+1), snippetMap[snippetName]["end"]);
+  // dedent works on strings, not on string arrays, so this is very ineffective unfortunately...
+  result = dedent(result.join("\n")).split(/\r?\n/);
+  return result;
+}
+
+/**
+ * Get snippet from file
+ */
+function getSnippet(fileName, snippetName) {
+
+  // regular snippet
+  const ext = path.extname(fileName);
+  const snippetParts = snippetName.split("::"); 
+
+  // regular snippet
+  let snippetsFileName = fileName.slice(0, -ext.length) + SNIPPETS_FILE_SUFFIX;
+  if (snippetParts.length > 1) {
+      snippetsFileName = path. dirname(fileName) + "/" + snippetParts[0];
+      snippetName = snippetParts[1];
+  }
+  const snippet = getSnippetFromFile(snippetsFileName, snippetName);
+  if (!snippet) {
+      throw new Error(`Could not find requested snippet "${snippetName}" requested in file ${fileName} in file ${snippetsFileName}.`);
+  }
+
+  const codeType = path.extname(snippetsFileName).replace(".", "");
+  snippet.unshift(`\`\`\`${codeType}`);
+  snippet.push("```");
+
+  return snippet;
+}
+
+/**
+ * Insert snippets into the markdown file
+ */
+function insertSnippets(fileName, lines) {
+  const result = []
+  for (let line of lines) {
+    if (line.includes(SNIPPET_MARKER)) {
+      const snippetName = extractMarkerContent(SNIPPET_MARKER, line);
+      snippet = getSnippet(fileName, snippetName);
+      result.push(...snippet);
+    }
+    result.push(line);
+  }
+  return result;
+}
+
+
+/**
+ * Insert tuba links into the markdown file
+ */
 function insertTubaLinks(lines) {
   const result = []
   for (let line of lines) {
     if (line.includes(TUBA_MARKER)) {
-      const tubaTag = extractMarkerContent(line, TUBA_MARKER);
+      const tubaTag = extractMarkerContent(SNIPPET_MARKER, line);
       const links = tubaConfig.filter((link) => link.tags.includes(tubaTag));
       if (links.length > 0) {
         result.push("## Additional Setup guides")
@@ -62,14 +170,18 @@ function insertTubaLinks(lines) {
   return result;
 }
 
-// remove all lines with DLT markers
-// TODO: maybe warn here if there are markers that were not
-// processed
+/** 
+ * Remove all lines that contain a DLT_MARKER
+ * TODO: we should probably warn here if we find a DLT_MARKER
+ * that was not processed before
+ */
 function removeRemainingMarkers(lines) {
   return lines.filter((line) => !line.includes(DLT_MARKER));
 }
 
-
+/**
+ * Preprocess all docs in the docs folder
+ */
 function preprocess_docs() {
     console.log("Processing docs...");
     let processedFiles = 0;
@@ -92,6 +204,7 @@ function preprocess_docs() {
         let lines = fs.readFileSync(fileName, 'utf8').split(/\r?\n/);
 
         // insert stuff
+        lines = insertSnippets(fileName, lines);
         lines = insertTubaLinks(lines);
         lines = removeRemainingMarkers(lines);
 
@@ -103,6 +216,9 @@ function preprocess_docs() {
 
 preprocess_docs();
 
+/**
+ * Watch for changes and preprocess the docs if --watch cli command flag is present
+ */
 if (process.argv.includes("--watch")) {
   console.log(`Watching...`)
   let lastUpdate = Date.now();
