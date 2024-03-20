2c7ec786ca23870971b7ab7c8eee26521374b424
Dave <shrps@posteo.net>
2024-03-20 13:31:27 +0100

    extend preprocess script to insert tuba links

--- docs/website/tools/preprocess_docs.js
+++ docs/website/tools/preprocess_docs.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const watch = require('node-watch');
+const fetch = require('sync-fetch')
 
 // constants
 const MD_SOURCE_DIR = "docs/";
@@ -7,6 +9,17 @@ const MD_TARGET_DIR = "docs_processed/";
 
 const MOVE_FILES_EXTENSION = [".md", ".mdx", ".py", ".png", ".jpg", ".jpeg"];
 const DOCS_EXTENSIONS = [".md", ".mdx"];
+
+// markers
+const TUBA_MARKER = "@@@DLT_TUBA";
+const DLT_MARKER = "@@@DLT";
+
+// fetch tuba config. TODO: make this fail gracefully
+const tubaConfig = fetch('https://dlthub.com/docs/pipelines/links.json', {
+  headers: {
+    Accept: 'application/vnd.citationstyles.csl+json'
+  }
+}).json();
 
 // yield all files in dir
 function *walkSync(dir) {
@@ -19,6 +32,39 @@ function *walkSync(dir) {
       }
     }
   }
+
+// takes a line with a dlt marker and returns only the trimmed content
+function extractMarkerContent(line, marker) {
+  line = line.replace("-->", "");
+  line = line.replace("<!--", "");
+  line = line.replace(marker, "");
+  line = line.trim();
+  return line
+}
+
+function insertTubaLinks(lines) {
+  const result = []
+  for (let line of lines) {
+    if (line.includes(TUBA_MARKER)) {
+      const tubaTag = extractMarkerContent(line, TUBA_MARKER);
+      const links = tubaConfig.filter((link) => link.tags.includes(tubaTag));
+      if (links.length > 0) {
+        result.push("## Additional Setup guides")
+        for (const link of links) {
+          result.push(`- [${link.title}](${link.public_url})`)
+        }
+      } else {
+        logger.warn(`No tuba links found for tag ${tubaTag}`)
+      }
+    }
+    result.push(line);
+  }
+  return result;
+}
+
+function removeRemainingMarkers(lines) {
+  return lines.filter((line) => !line.includes(DLT_MARKER));
+}
 
 
 function preprocess_docs() {
@@ -40,15 +86,30 @@ function preprocess_docs() {
         }
   
         // copy docs to correct folder
-        const lines = fs.readFileSync(fileName, 'utf8').split(/\r?\n/);
-        const updatedLines = lines;
+        let lines = fs.readFileSync(fileName, 'utf8').split(/\r?\n/);
 
-        // insert something to see wether it works
-        lines.splice(8, 0, "Generated")
+        // insert stuff
+        lines = insertTubaLinks(lines);
+        lines = removeRemainingMarkers(lines);
 
-        fs.writeFileSync(targetFileName, updatedLines.join("\n"));
+        fs.writeFileSync(targetFileName, lines.join("\n"));
+        processedFiles += 1;
     }
     console.log(`Processed ${processedFiles} files.`);
 }
 
 preprocess_docs();
+
+if (process.argv.includes("--watch")) {
+  console.log(`Watching...`)
+  let lastUpdate = Date.now();
+  watch("../", { recursive: true, filter: /\.(py|toml|md)$/  }, function(evt, name) {
+      // break update loop
+      if (Date.now() - lastUpdate < 500) {
+          return;
+      }
+      console.log('%s changed...', name);
+      preprocess_docs();
+      lastUpdate = Date.now();
+  });
+}
