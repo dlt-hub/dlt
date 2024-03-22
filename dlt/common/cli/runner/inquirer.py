38aef786a068356aed490de4c189fb277c2a6d00
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-03-22 12:10:00 +0100

    Adjust error messages

--- dlt/common/cli/runner/inquirer.py
+++ dlt/common/cli/runner/inquirer.py
@@ -115,15 +115,17 @@ class Inquirer:
     def preflight_checks(self) -> None:
         if pipeline_name := self.params.pipeline_name:
             if pipeline_name not in self.pipelines and pipeline_name not in self.aliases:
-                fmt.warning(f"Pipeline {pipeline_name} has not been found in pipeline script")
+                fmt.error(f"Pipeline {pipeline_name} has not been found in pipeline script")
+                fmt.error("You can choose one of: " + ", ".join(self.pipelines.keys()))
                 raise PreflightError()
 
         if source_name := self.params.source_name:
             if source_name not in self.sources and source_name not in self.aliases:
-                fmt.warning(
+                fmt.error(
                     f"Source or resouce with name: {source_name} has not been found in pipeline"
                     " script"
                 )
+                fmt.error("You can choose one of: " + ", ".join(self.sources.keys()))
                 raise PreflightError()
 
         if self.params.current_dir != self.params.pipeline_workdir:
