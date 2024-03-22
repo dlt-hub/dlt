51a757b09ff3583aa99961a591a00e6f96d76691
Dave <shrps@posteo.net>
2024-03-22 14:14:15 +0100

    bring examples into desired shape

--- docs/examples/connector_x_arrow/connector_x_arrow.py
+++ docs/examples/connector_x_arrow/connector_x_arrow.py
@@ -1,52 +1,53 @@
-def connector_x_snippet() -> None:
-    # @@@DLT_SNIPPET_START example
-    # @@@DLT_SNIPPET_START markdown_source
-    import connectorx as cx
+"""
+---
+title: Load mysql table with ConnectorX & Arrow
+description: Load data from sql queries fast with connector x and arrow tables
+keywords: [connector x, pyarrow, zero copy]
+---
+"""
 
-    import dlt
-    from dlt.sources.credentials import ConnectionStringCredentials
+import connectorx as cx
 
-    def read_sql_x(
-        conn_str: ConnectionStringCredentials = dlt.secrets.value,
-        query: str = dlt.config.value,
-    ):
-        yield cx.read_sql(
-            conn_str.to_native_representation(),
-            query,
-            return_type="arrow2",
-            protocol="binary",
-        )
+import dlt
+from dlt.sources.credentials import ConnectionStringCredentials
 
-    def genome_resource():
-        # create genome resource with merge on `upid` primary key
-        genome = dlt.resource(
-            name="genome",
-            write_disposition="merge",
-            primary_key="upid",
-            standalone=True,
-        )(read_sql_x)(
-            "mysql://rfamro@mysql-rfam-public.ebi.ac.uk:4497/Rfam",  # type: ignore[arg-type]
-            "SELECT * FROM genome ORDER BY created LIMIT 1000",
-        )
-        # add incremental on created at
-        genome.apply_hints(incremental=dlt.sources.incremental("created"))
-        return genome
 
-    # @@@DLT_SNIPPET_END markdown_source
+def read_sql_x(
+    conn_str: ConnectionStringCredentials = dlt.secrets.value,
+    query: str = dlt.config.value,
+):
+    yield cx.read_sql(
+        conn_str.to_native_representation(),
+        query,
+        return_type="arrow2",
+        protocol="binary",
+    )
 
-    # @@@DLT_SNIPPET_START markdown_pipeline
-    __name__ = "__main__"  # @@@DLT_REMOVE
-    if __name__ == "__main__":
-        pipeline = dlt.pipeline(destination="duckdb")
-        genome = genome_resource()
 
-        print(pipeline.run(genome))
-        print(pipeline.last_trace.last_normalize_info)
-        # NOTE: run pipeline again to see that no more records got loaded thanks to incremental loading
-    # @@@DLT_SNIPPET_END markdown_pipeline
+def genome_resource():
+    # create genome resource with merge on `upid` primary key
+    genome = dlt.resource(
+        name="genome",
+        write_disposition="merge",
+        primary_key="upid",
+        standalone=True,
+    )(read_sql_x)(
+        "mysql://rfamro@mysql-rfam-public.ebi.ac.uk:4497/Rfam",  # type: ignore[arg-type]
+        "SELECT * FROM genome ORDER BY created LIMIT 1000",
+    )
+    # add incremental on created at
+    genome.apply_hints(incremental=dlt.sources.incremental("created"))
+    return genome
 
-    # check that stuff was loaded # @@@DLT_REMOVE
-    row_counts = pipeline.last_trace.last_normalize_info.row_counts  # @@@DLT_REMOVE
-    assert row_counts["genome"] == 1000  # @@@DLT_REMOVE
 
-    # @@@DLT_SNIPPET_END example
+if __name__ == "__main__":
+    pipeline = dlt.pipeline(destination="duckdb")
+    genome = genome_resource()
+
+    print(pipeline.run(genome))
+    print(pipeline.last_trace.last_normalize_info)
+    # NOTE: run pipeline again to see that no more records got loaded thanks to incremental loading
+
+    # check that stuff was loaded
+    row_counts = pipeline.last_trace.last_normalize_info.row_counts
+    assert row_counts["genome"] == 1000
--- docs/examples/transformers/transformers.py
+++ docs/examples/transformers/transformers.py
@@ -1,71 +1,69 @@
-def transformers_snippet() -> None:
-    # @@@DLT_SNIPPET_START example
-    import dlt
-    from dlt.sources.helpers import requests
+"""
+---
+title: Pokemon details in parallel using transformers
+description: Learn how to use dlt transformers and how to speed up your loads with parallelism
+keywords: [transformers, parallelism, example]
+---
+"""
 
-    @dlt.source(max_table_nesting=2)
-    def source(pokemon_api_url: str):
-        """"""
+import dlt
+from dlt.sources.helpers import requests
 
-        # note that we deselect `pokemon_list` - we do not want it to be loaded
-        @dlt.resource(write_disposition="replace", selected=False)
-        def pokemon_list():
-            """Retrieve a first page of Pokemons and yield it. We do not retrieve all the pages in this example"""
-            yield requests.get(pokemon_api_url).json()["results"]
 
-        # transformer that retrieves a list of objects in parallel
-        @dlt.transformer
-        def pokemon(pokemons):
-            """Yields details for a list of `pokemons`"""
+@dlt.source(max_table_nesting=2)
+def source(pokemon_api_url: str):
 
-            # @dlt.defer marks a function to be executed in parallel
-            # in a thread pool
-            @dlt.defer
-            def _get_pokemon(_pokemon):
-                return requests.get(_pokemon["url"]).json()
+    # note that we deselect `pokemon_list` - we do not want it to be loaded
+    @dlt.resource(write_disposition="replace", selected=False)
+    def pokemon_list():
+        """Retrieve a first page of Pokemons and yield it. We do not retrieve all the pages in this example"""
+        yield requests.get(pokemon_api_url).json()["results"]
 
-            # call and yield the function result normally, the @dlt.defer takes care of parallelism
-            for _pokemon in pokemons:
-                yield _get_pokemon(_pokemon)
+    # transformer that retrieves a list of objects in parallel
+    @dlt.transformer
+    def pokemon(pokemons):
+        """Yields details for a list of `pokemons`"""
 
-        # a special case where just one item is retrieved in transformer
-        # a whole transformer may be marked for parallel execution
-        @dlt.transformer(parallelized=True)
-        def species(pokemon_details):
-            """Yields species details for a pokemon"""
-            species_data = requests.get(pokemon_details["species"]["url"]).json()
-            # link back to pokemon so we have a relation in loaded data
-            species_data["pokemon_id"] = pokemon_details["id"]
-            # You can return the result instead of yield since the transformer only generates one result
-            return species_data
+        # @dlt.defer marks a function to be executed in parallel
+        # in a thread pool
+        @dlt.defer
+        def _get_pokemon(_pokemon):
+            return requests.get(_pokemon["url"]).json()
 
-        # create two simple pipelines with | operator
-        # 1. send list of pokemons into `pokemon` transformer to get pokemon details
-        # 2. send pokemon details into `species` transformer to get species details
-        # NOTE: dlt is smart enough to get data from pokemon_list and pokemon details once
+        # call and yield the function result normally, the @dlt.defer takes care of parallelism
+        for _pokemon in pokemons:
+            yield _get_pokemon(_pokemon)
 
-        return (pokemon_list | pokemon, pokemon_list | pokemon | species)
+    # a special case where just one item is retrieved in transformer
+    # a whole transformer may be marked for parallel execution
+    @dlt.transformer(parallelized=True)
+    def species(pokemon_details):
+        """Yields species details for a pokemon"""
+        species_data = requests.get(pokemon_details["species"]["url"]).json()
+        # link back to pokemon so we have a relation in loaded data
+        species_data["pokemon_id"] = pokemon_details["id"]
+        # You can return the result instead of yield since the transformer only generates one result
+        return species_data
 
-    if __name__ == "__main__":
-        # build duck db pipeline
-        pipeline = dlt.pipeline(
-            pipeline_name="pokemon", destination="duckdb", dataset_name="pokemon_data"
-        )
+    # create two simple pipelines with | operator
+    # 1. send list of pokemons into `pokemon` transformer to get pokemon details
+    # 2. send pokemon details into `species` transformer to get species details
+    # NOTE: dlt is smart enough to get data from pokemon_list and pokemon details once
 
-        # the pokemon_list resource does not need to be loaded
-        load_info = pipeline.run(source("https://pokeapi.co/api/v2/pokemon"))
-        print(load_info)
-    # @@@DLT_SNIPPET_END example
+    return (pokemon_list | pokemon, pokemon_list | pokemon | species)
 
-    # Run without __main__
+
+if __name__ == "__main__":
+    # build duck db pipeline
     pipeline = dlt.pipeline(
         pipeline_name="pokemon", destination="duckdb", dataset_name="pokemon_data"
     )
 
     # the pokemon_list resource does not need to be loaded
     load_info = pipeline.run(source("https://pokeapi.co/api/v2/pokemon"))
+    print(load_info)
 
-    # test assertions
+    # verify that all went well
     row_counts = pipeline.last_trace.last_normalize_info.row_counts
     assert row_counts["pokemon"] == 20
     assert row_counts["species"] == 20
