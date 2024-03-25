b1ee49ab10c4426f1aa4a9d12b6d8bdb77a42a73
Anton Burnashev <anton.burnashev@gmail.com>
2024-03-25 18:23:04 +0300

    Move `paginate` function test to new file `test_requests_paginate.py`

--- tests/sources/helpers/rest_client/test_client.py
+++ tests/sources/helpers/rest_client/test_client.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from typing import Any, cast
 from dlt.common.typing import TSecretStrValue
-from dlt.sources.helpers.requests import Response, Request, paginate
+from dlt.sources.helpers.requests import Response, Request
 from dlt.sources.helpers.rest_client import RESTClient
 from dlt.sources.helpers.rest_client.client import Hooks
 from dlt.sources.helpers.rest_client.paginators import JSONResponsePaginator
@@ -167,13 +167,3 @@ class TestRESTClient:
         )
 
         assert_pagination(list(pages_iter))
-
-    def test_paginate_function(self, rest_client: RESTClient):
-        pages_iter = paginate(
-            "https://api.example.com/posts",
-            paginator=JSONResponsePaginator(next_url_path="next_page"),
-        )
-
-        pages = list(pages_iter)
-
-        assert_pagination(pages)
