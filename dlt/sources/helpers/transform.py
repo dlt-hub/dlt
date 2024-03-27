2ed1233372e378ea1608320f7f35b4876b13810e
IlyaFaer <gurovilya9@gmail.com>
2024-03-27 10:09:49 +0400

    feat(transform): implement columns pivot map function

--- dlt/sources/helpers/transform.py
+++ dlt/sources/helpers/transform.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from dlt.common.typing import TDataItem
 from dlt.extract.items import ItemTransformFunctionNoMeta
 
@@ -24,3 +26,31 @@ def skip_first(max_items: int) -> ItemTransformFunctionNoMeta[bool]:
         return count > max_items
 
     return _filter
+
+
+def pivot(columns: Union[str, List[str]], prefix: str) -> ItemTransformFunctionNoMeta[TDataItem]:
+    """Pivot a list of columns into a dictionary.
+
+    Args:
+        columns (Union[str, List[str]]): list of column names
+        prefix (str): prefix to add to the column names
+
+    Returns:
+        ItemTransformFunctionNoMeta[TDataItem]:
+            A function to pivot columns into a dict.
+    """
+    if isinstance(columns, str):
+        columns = [columns]
+
+    def _transformer(item: TDataItem) -> TDataItem:
+        """Pivot columns into a dictionary.
+
+        Args:
+            item (TDataItem): a data item.
+
+        Returns:
+            TDataItem: a data item with pivoted columns.
+        """
+        return {prefix + col: item[ind] for ind, col in enumerate(columns)}
+
+    return _transformer
