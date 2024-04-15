7bb951749f8b5743d4778b848280ec96a4c335a3
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-04-15 16:07:17 +0200

    Try concurrent snippet linting

--- docs/tools/check_embedded_snippets.py
+++ docs/tools/check_embedded_snippets.py
@@ -1,11 +1,19 @@
 """
 Walks through all markdown files, finds all code snippets, and checks wether they are parseable.
 """
+import os
+import ast
+import json
+import subprocess
+import argparse
+
+from dataclasses import dataclass
+from concurrent.futures import ThreadPoolExecutor
+from textwrap import dedent
 from typing import List, Dict, Optional
 
-import os, ast, json, yaml, tomlkit, subprocess, argparse  # noqa: I251
-from dataclasses import dataclass
-from textwrap import dedent
+import tomlkit
+import yaml
 
 import dlt.cli.echo as fmt
 
@@ -293,10 +301,12 @@ if __name__ == "__main__":
 
     # these stages are python only
     python_snippets = [s for s in filtered_snippets if s.language == "py"]
-    if args.command in ["lint", "full"]:
-        lint_snippets(python_snippets, args.verbose)
-    if ENABLE_MYPY and args.command in ["typecheck", "full"]:
-        typecheck_snippets(python_snippets, args.verbose)
+    with ThreadPoolExecutor() as runner:
+        if args.command in ["lint", "full"]:
+            runner.submit(lint_snippets, python_snippets, args.verbose)
+
+        if ENABLE_MYPY and args.command in ["typecheck", "full"]:
+            runner.submit(typecheck_snippets(python_snippets, args.verbose))
 
     # unlink lint_me file
     if os.path.exists(LINT_FILE):
