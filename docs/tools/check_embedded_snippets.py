5089343704b8d55a33ebf019b45c4033df716b34
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-04-15 16:07:47 +0200

    Try concurrent snippet linting

--- docs/tools/check_embedded_snippets.py
+++ docs/tools/check_embedded_snippets.py
@@ -302,11 +302,12 @@ if __name__ == "__main__":
     # these stages are python only
     python_snippets = [s for s in filtered_snippets if s.language == "py"]
     with ThreadPoolExecutor() as runner:
+        futures = []
         if args.command in ["lint", "full"]:
-            runner.submit(lint_snippets, python_snippets, args.verbose)
+            futures.append(runner.submit(lint_snippets, python_snippets, args.verbose))
 
         if ENABLE_MYPY and args.command in ["typecheck", "full"]:
-            runner.submit(typecheck_snippets(python_snippets, args.verbose))
+            futures.append(runner.submit(typecheck_snippets(python_snippets, args.verbose)))
 
     # unlink lint_me file
     if os.path.exists(LINT_FILE):
