15ac9bf81490c4e52967e2fab5c7e70b13d4d84e
Dave <shrps@posteo.net>
2024-04-15 12:36:16 +0200

    update testing pipe

--- fs_testing_pipe.py
+++ fs_testing_pipe.py
@@ -1,7 +1,11 @@
 import dlt
 import os
+import shutil
+import random
 
 if __name__ == "__main__":
+
+    shutil.rmtree("./my_files", ignore_errors=True)
     os.environ["DESTINATION__FILESYSTEM__BUCKET_URL"] = "file://my_files"
     os.environ["DATA_WRITER__DISABLE_COMPRESSION"] = "TRUE"
 
@@ -15,6 +19,21 @@ if __name__ == "__main__":
             {"id": 4},
             {"id": 5}
             ]
+        
+    pipeline_name = f"dave_{random.randint(0, 10000000)}"
 
-    pipe = dlt.pipeline(pipeline_name="dave", destination="filesystem")
+    pipe = dlt.pipeline(pipeline_name=pipeline_name, destination="filesystem")
     pipe.run(my_resouce(), table_name="my_table") #, loader_file_format="parquet")
+
+    # resource with incremental for testing restoring of pipeline state 
+    @dlt.resource(name="my_table")
+    def updated_resouce(id=dlt.sources.incremental("id")):
+        yield from [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3},
+            {"id": 4},
+            {"id": 5},
+            {"id": 6}
+            ]
+    pipe.run(updated_resouce(), table_name="my_table") #, loader_file_format="parquet")
