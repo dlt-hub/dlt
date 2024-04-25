2f36b7cc15024b5b34ce331202504a5abe66f730
Jorrit Sandbrink <sandbj01@heiway.net>
2024-04-25 23:32:51 +0400

    make missing keys warning conditional on merge strategy

--- dlt/common/destination/reference.py
+++ dlt/common/destination/reference.py
@@ -353,13 +353,16 @@ class JobClientBase(ABC):
                         f'"{table["x-merge-strategy"]}" is not a valid merge strategy. '  # type: ignore[typeddict-item]
                         f"""Allowed values: {', '.join(['"' + s + '"' for s in MERGE_STRATEGIES])}."""
                     )
-                if not has_column_with_prop(table, "primary_key") and not has_column_with_prop(
-                    table, "merge_key"
+                if (
+                    table.get("x-merge-strategy") == "delete-insert"
+                    and not has_column_with_prop(table, "primary_key")
+                    and not has_column_with_prop(table, "merge_key")
                 ):
                     logger.warning(
-                        f"Table {table_name} has write_disposition set to merge, but no primary or"
-                        " merge keys defined. "
-                        + "dlt will fall back to append for this table."
+                        f"Table {table_name} has `write_disposition` set to `merge`"
+                        " and `merge_strategy` set to `delete-insert`, but no primary or"
+                        " merge keys defined."
+                        " dlt will fall back to `append` for this table."
                     )
             if has_column_with_prop(table, "hard_delete"):
                 if len(get_columns_names_with_prop(table, "hard_delete")) > 1:
