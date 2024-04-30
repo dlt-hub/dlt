e3def352aba7e72e185afabc7f3e05b1192d2b6c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-04-30 13:28:52 +0200

    Adjust error message

--- dlt/destinations/impl/duckdb/exceptions.py
+++ dlt/destinations/impl/duckdb/exceptions.py
@@ -5,6 +5,7 @@ class InvalidInMemoryDuckDbUsage(DestinationTerminalException):
     def __init__(self) -> None:
         super().__init__(
             'You have specicied credentials=":memory:" this is incorrect.\n'
-            "Please pass in memory instance of duckdb connection\n"
-            'credentials=dlt.destinations.duckdb(":memory:")`'
+            'Please create in memory instance of duckdb `conn = duckdb.connect(":memory:")`\n'
+            "then pass it as via parameter via destination factory"
+            'dlt.pipeline(pipeline_name="...", destination=dlt.destinations.duckdb(conn)'
         )
