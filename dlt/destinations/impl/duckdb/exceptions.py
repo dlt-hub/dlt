98d02aee545420da42e60698c4250628f43d0173
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-04-30 13:34:21 +0200

    Remove backticks

--- dlt/destinations/impl/duckdb/exceptions.py
+++ dlt/destinations/impl/duckdb/exceptions.py
@@ -5,7 +5,7 @@ class InvalidInMemoryDuckDbUsage(DestinationTerminalException):
     def __init__(self) -> None:
         super().__init__(
             'You have specicied credentials=":memory:" this is incorrect.\n'
-            'Please create in memory instance of duckdb `conn = duckdb.connect(":memory:")`\n'
+            'Please create in memory instance of duckdb conn = duckdb.connect(":memory:")\n'
             "then pass it as via parameter via destination factory"
             'dlt.pipeline(pipeline_name="...", destination=dlt.destinations.duckdb(conn)'
         )
