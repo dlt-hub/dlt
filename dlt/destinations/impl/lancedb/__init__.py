cfc40388d73867bcb0fa55cc537b18868d53b4c8
Marcel Coetzee <marcel@mooncoon.com>
2024-05-17 16:42:37 +0200

    Add initial capabilities for LanceDB destination
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

---
+++ dlt/destinations/impl/lancedb/__init__.py
@@ -0,0 +1,17 @@
+from dlt.common.destination import DestinationCapabilitiesContext
+
+
+def capabilities() -> DestinationCapabilitiesContext:
+    caps = DestinationCapabilitiesContext()
+    caps.preferred_loader_file_format = "jsonl"
+    caps.supported_loader_file_formats = ["jsonl", "parquet"]
+
+    caps.max_identifier_length = 200
+    caps.max_column_identifier_length = 1024
+    caps.max_query_length = 8 * 1024 * 1024
+    caps.is_max_query_length_in_bytes = False
+    caps.max_text_data_type_length = 8 * 1024 * 1024
+    caps.is_max_text_data_type_length_in_bytes = False
+    caps.supports_ddl_transactions = False
+
+    return caps
