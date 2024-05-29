fad4ff0f836be468c8f62b38254a458b945a16a0
jorritsandbrink <jorritsandbrink@proton.me>
2024-05-29 16:27:33 +0400

    revert import changes

--- dlt/common/storages/data_item_storage.py
+++ dlt/common/storages/data_item_storage.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-from typing import Dict, Any, List, Optional
+from typing import Dict, Any, List, Sequence
 from abc import ABC, abstractmethod
 
 from dlt.common import logger
 from dlt.common.schema import TTableSchemaColumns
 from dlt.common.typing import StrAny, TDataItems
-from dlt.common.storages.load_package import PackageStorage
 from dlt.common.data_writers import (
     BufferedDataWriter,
     DataWriter,
