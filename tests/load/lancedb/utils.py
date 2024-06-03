32fd5c82f78230ab613de74c24e0b3ea0b3584e9
Marcel Coetzee <marcel@mooncoon.com>
2024-06-04 00:39:15 +0200

    Refactor load tests in lancedb/utils.py and add test for LanceDB model inference
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- tests/load/lancedb/utils.py
+++ tests/load/lancedb/utils.py
@@ -1,65 +1,45 @@
-import dlt
-from typing import Any, List
+from typing import Union, List
 
-import dlt
-from dlt.common.pipeline import PipelineContext
-from dlt.common.configuration.container import Container
+import numpy as np
+from lancedb.embeddings import TextEmbeddingFunction
+from lancedb.pydantic import LanceModel, Vector
 
-from dlt.destinations.impl.qdrant.qdrant_client import QdrantClient
-
-
-def assert_unordered_list_equal(list1: List[Any], list2: List[Any]) -> None:
-    assert len(list1) == len(list2), "Lists have different length"
-    for item in list1:
-        assert item in list2, f"Item {item} not found in list2"
+from dlt.destinations.impl.lancedb.utils import infer_lancedb_model_from_data
 
 
-def assert_collection(
-    pipeline: dlt.Pipeline,
-    collection_name: str,
-    expected_items_count: int = None,
-    items: List[Any] = None,
-) -> None:
-    client: QdrantClient = pipeline.destination_client()  # type: ignore[assignment]
+class MockEmbeddingFunc(TextEmbeddingFunction):
+    def generate_embeddings(
+        self, texts: Union[List[str], np.ndarray]
+    ) -> List[np.array]:
+        return [np.array(None)]
 
-    # Check if collection exists
-    exists = client._collection_exists(collection_name)
-    assert exists
+    def ndims(self) -> int:
+        return 2
 
-    qualified_collection_name = client._make_qualified_collection_name(collection_name)
-    point_records, offset = client.db_client.scroll(
-        qualified_collection_name, with_payload=True, limit=50
+
+def test_infer_lancedb_model_from_data() -> None:
+    data = [
+        {"id__": "1", "item": "tyre", "price": 12.0, "customer": "jack"},
+        {"id__": "2", "item": "wheel", "price": 100, "customer": "jill"},
+    ]
+    id_field_name = "id__"
+    vector_field_name = "vector__"
+    embedding_fields = ["item", "price"]
+    embedding_model_func = MockEmbeddingFunc()
+
+    inferred_model = infer_lancedb_model_from_data(
+        data, id_field_name, vector_field_name, embedding_fields, embedding_model_func
     )
 
-    if expected_items_count is not None:
-        assert expected_items_count == len(point_records)
+    expected_fields = {
+        "id__": (str, ...),
+        "vector__": (Vector(2), ...),
+        "item": (str, embedding_model_func.SourceField()),
+        "price": (float, embedding_model_func.SourceField()),
+        "customer": (str, ...),
+    }
 
-    if items is None:
-        return
-
-    drop_keys = ["_dlt_id", "_dlt_load_id"]
-    objects_without_dlt_keys = [
-        {k: v for k, v in point.payload.items() if k not in drop_keys} for point in point_records
-    ]
-
-    assert_unordered_list_equal(objects_without_dlt_keys, items)
-
-
-def drop_active_pipeline_data() -> None:
-    print("Dropping active pipeline data for test")
-
-    def has_collections(client):
-        schema = client.db_client.get_collections().collections
-        return len(schema) > 0
-
-    if Container()[PipelineContext].is_active():
-        # take existing pipeline
-        p = dlt.pipeline()
-        client: QdrantClient = p.destination_client()  # type: ignore[assignment]
-
-        if has_collections(client):
-            client.drop_storage()
-
-        p._wipe_working_folder()
-        # deactivate context
-        Container()[PipelineContext].deactivate()
+    assert issubclass(inferred_model, LanceModel)
+    for field_name, (field_type, field_default) in expected_fields.items():
+        assert field_name in inferred_model.model_fields
+        inferred_default = inferred_model.model_fields[field_name].default
