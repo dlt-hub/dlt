2e04cffb46ad6d57e07f6655f56add325e89b7d7
jorritsandbrink <jorritsandbrink@proton.me>
2024-06-03 13:46:09 +0400

    move function to top level to prevent multiprocessing pickle error

--- dlt/destinations/impl/filesystem/__init__.py
+++ dlt/destinations/impl/filesystem/__init__.py
@@ -4,18 +4,19 @@ from dlt.common.schema.typing import TSchemaTables
 from dlt.common.destination import DestinationCapabilitiesContext, TLoaderFileFormat
 
 
-def capabilities() -> DestinationCapabilitiesContext:
-    def loader_file_format_adapter(
-        preferred_loader_file_format: TLoaderFileFormat,
-        supported_loader_file_formats: Sequence[TLoaderFileFormat],
-        /,
-        *,
-        schema_tables: TSchemaTables,
-    ) -> Tuple[TLoaderFileFormat, Sequence[TLoaderFileFormat]]:
-        if any([table.get("table_format") == "delta" for table in schema_tables.values()]):
-            return ("parquet", ["parquet"])
-        return (preferred_loader_file_format, supported_loader_file_formats)
+def loader_file_format_adapter(
+    preferred_loader_file_format: TLoaderFileFormat,
+    supported_loader_file_formats: Sequence[TLoaderFileFormat],
+    /,
+    *,
+    schema_tables: TSchemaTables,
+) -> Tuple[TLoaderFileFormat, Sequence[TLoaderFileFormat]]:
+    if any([table.get("table_format") == "delta" for table in schema_tables.values()]):
+        return ("parquet", ["parquet"])
+    return (preferred_loader_file_format, supported_loader_file_formats)
 
+
+def capabilities() -> DestinationCapabilitiesContext:
     return DestinationCapabilitiesContext.generic_capabilities(
         preferred_loader_file_format="jsonl",
         loader_file_format_adapter=loader_file_format_adapter,
