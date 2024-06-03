1d9b968eba308dbc0026ebc3f7a7331bae275c64
jorritsandbrink <jorritsandbrink@proton.me>
2024-06-03 15:42:10 +0400

    reorganize object_store rust crate credentials tests

--- tests/load/filesystem/test_object_store_rs_credentials.py
+++ tests/load/filesystem/test_object_store_rs_credentials.py
@@ -1,3 +1,5 @@
+"""Tests translation of `dlt` credentials into `object_store` Rust crate credentials."""
+
 from typing import Any, Dict, cast
 
 import pytest
@@ -8,7 +10,7 @@ import dlt
 from dlt.common.typing import TSecretStrValue
 from dlt.common.configuration import resolve_configuration
 from dlt.common.configuration.specs import (
-    CredentialsConfiguration,
+    AnyAzureCredentials,
     AzureServicePrincipalCredentialsWithoutDefaults,
     AzureCredentialsWithoutDefaults,
     AwsCredentials,
@@ -17,63 +19,79 @@ from dlt.common.configuration.specs import (
     GcpOAuthCredentialsWithoutDefaults,
 )
 
+from tests.load.utils import AZ_BUCKET, AWS_BUCKET, GCS_BUCKET, ALL_FILESYSTEM_DRIVERS
 
-def test_object_store_rs_credentials() -> None:
-    """Tests translation of `dlt` credentials into `object_store` Rust crate credentials."""
+if all(driver not in ALL_FILESYSTEM_DRIVERS for driver in ("az", "s3", "gs")):
+    pytest.skip(
+        "Requires at least one of `az`, `s3`, `gs` in `ALL_FILESYSTEM_DRIVERS`.",
+        allow_module_level=True,
+    )
 
-    def can_connect(bucket_url: str, object_store_rs_credentials: Dict[str, str]) -> bool:
-        """Returns True if client can connect to object store, False otherwise.
 
-        Uses `deltatable` library as Python interface to `object_store` Rust crate.
-        """
-        try:
-            DeltaTable(
-                bucket_url,
-                storage_options=object_store_rs_credentials,
-            )
-        except TableNotFoundError:
-            # this error implies the connection was succesful
-            # there is no Delta table at `bucket_url`
-            return True
-        return False
+FS_CREDS: Dict[str, Any] = dlt.secrets.get("destination.filesystem.credentials")
+assert (
+    FS_CREDS is not None
+), "`destination.filesystem.credentials` must be configured for these tests."
 
-    fs_creds: Dict[str, Any] = dlt.secrets.get("destination.filesystem.credentials")
-    creds: CredentialsConfiguration
 
-    ### Azure
-    bucket_url = dlt.config.get("tests.bucket_url_az", str)
+def can_connect(bucket_url: str, object_store_rs_credentials: Dict[str, str]) -> bool:
+    """Returns True if client can connect to object store, False otherwise.
+
+    Uses `deltatable` library as Python interface to `object_store` Rust crate.
+    """
+    try:
+        DeltaTable(
+            bucket_url,
+            storage_options=object_store_rs_credentials,
+        )
+    except TableNotFoundError:
+        # this error implies the connection was succesful
+        # there is no Delta table at `bucket_url`
+        return True
+    return False
+
+
+@pytest.mark.skipif(
+    "az" not in ALL_FILESYSTEM_DRIVERS, reason="`az` not in `ALL_FILESYSTEM_DRIVERS`"
+)
+def test_azure_object_store_rs_credentials() -> None:
+    creds: AnyAzureCredentials
 
     creds = AzureServicePrincipalCredentialsWithoutDefaults(
         **dlt.secrets.get("destination.fsazureprincipal.credentials")
     )
-    assert can_connect(bucket_url, creds.to_object_store_rs_credentials())
+    assert can_connect(AZ_BUCKET, creds.to_object_store_rs_credentials())
 
     # without SAS token
     creds = AzureCredentialsWithoutDefaults(
-        azure_storage_account_name=fs_creds["azure_storage_account_name"],
-        azure_storage_account_key=fs_creds["azure_storage_account_key"],
+        azure_storage_account_name=FS_CREDS["azure_storage_account_name"],
+        azure_storage_account_key=FS_CREDS["azure_storage_account_key"],
     )
     assert creds.azure_storage_sas_token is None
-    assert can_connect(bucket_url, creds.to_object_store_rs_credentials())
+    assert can_connect(AZ_BUCKET, creds.to_object_store_rs_credentials())
 
     # with SAS token
     creds = resolve_configuration(creds)
     assert creds.azure_storage_sas_token is not None
-    assert can_connect(bucket_url, creds.to_object_store_rs_credentials())
+    assert can_connect(AZ_BUCKET, creds.to_object_store_rs_credentials())
 
-    ### AWS
-    bucket_url = dlt.config.get("tests.bucket_url_s3", str)
+
+@pytest.mark.skipif(
+    "s3" not in ALL_FILESYSTEM_DRIVERS, reason="`s3` not in `ALL_FILESYSTEM_DRIVERS`"
+)
+def test_aws_object_store_rs_credentials() -> None:
+    creds: AwsCredentialsWithoutDefaults
 
     # AwsCredentials: no user-provided session token
     creds = AwsCredentials(
-        aws_access_key_id=fs_creds["aws_access_key_id"],
-        aws_secret_access_key=fs_creds["aws_secret_access_key"],
-        region_name=fs_creds["region_name"],
+        aws_access_key_id=FS_CREDS["aws_access_key_id"],
+        aws_secret_access_key=FS_CREDS["aws_secret_access_key"],
+        region_name=FS_CREDS["region_name"],
     )
     assert creds.aws_session_token is None
     object_store_rs_creds = creds.to_object_store_rs_credentials()
     assert object_store_rs_creds["aws_session_token"] is not None  # auto-generated token
-    assert can_connect(bucket_url, object_store_rs_creds)
+    assert can_connect(AWS_BUCKET, object_store_rs_creds)
 
     # AwsCredentials: user-provided session token
     # use previous credentials to create session token for new credentials
@@ -82,46 +100,48 @@ def test_object_store_rs_credentials() -> None:
         aws_access_key_id=sess_creds["aws_access_key_id"],
         aws_secret_access_key=cast(TSecretStrValue, sess_creds["aws_secret_access_key"]),
         aws_session_token=cast(TSecretStrValue, sess_creds["aws_session_token"]),
-        region_name=fs_creds["region_name"],
+        region_name=FS_CREDS["region_name"],
     )
     assert creds.aws_session_token is not None
     object_store_rs_creds = creds.to_object_store_rs_credentials()
     assert object_store_rs_creds["aws_session_token"] is not None
-    assert can_connect(bucket_url, object_store_rs_creds)
+    assert can_connect(AWS_BUCKET, object_store_rs_creds)
 
     # AwsCredentialsWithoutDefaults: no user-provided session token
     creds = AwsCredentialsWithoutDefaults(
-        aws_access_key_id=fs_creds["aws_access_key_id"],
-        aws_secret_access_key=fs_creds["aws_secret_access_key"],
-        region_name=fs_creds["region_name"],
+        aws_access_key_id=FS_CREDS["aws_access_key_id"],
+        aws_secret_access_key=FS_CREDS["aws_secret_access_key"],
+        region_name=FS_CREDS["region_name"],
     )
     assert creds.aws_session_token is None
     object_store_rs_creds = creds.to_object_store_rs_credentials()
     assert "aws_session_token" not in object_store_rs_creds  # no auto-generated token
-    assert can_connect(bucket_url, object_store_rs_creds)
+    assert can_connect(AWS_BUCKET, object_store_rs_creds)
 
     # AwsCredentialsWithoutDefaults: user-provided session token
     creds = AwsCredentialsWithoutDefaults(
         aws_access_key_id=sess_creds["aws_access_key_id"],
         aws_secret_access_key=cast(TSecretStrValue, sess_creds["aws_secret_access_key"]),
         aws_session_token=cast(TSecretStrValue, sess_creds["aws_session_token"]),
-        region_name=fs_creds["region_name"],
+        region_name=FS_CREDS["region_name"],
     )
     assert creds.aws_session_token is not None
     object_store_rs_creds = creds.to_object_store_rs_credentials()
     assert object_store_rs_creds["aws_session_token"] is not None
-    assert can_connect(bucket_url, object_store_rs_creds)
+    assert can_connect(AWS_BUCKET, object_store_rs_creds)
 
-    ### GCP
-    bucket_url = dlt.config.get("tests.bucket_url_gs", str)
 
+@pytest.mark.skipif(
+    "gs" not in ALL_FILESYSTEM_DRIVERS, reason="`gs` not in `ALL_FILESYSTEM_DRIVERS`"
+)
+def test_gcp_object_store_rs_credentials() -> None:
     creds = GcpServiceAccountCredentialsWithoutDefaults(
-        project_id=fs_creds["project_id"],
-        private_key=fs_creds["private_key"],
-        private_key_id=fs_creds["private_key_id"],
-        client_email=fs_creds["client_email"],
+        project_id=FS_CREDS["project_id"],
+        private_key=FS_CREDS["private_key"],
+        private_key_id=FS_CREDS["private_key_id"],
+        client_email=FS_CREDS["client_email"],
     )
-    assert can_connect(bucket_url, creds.to_object_store_rs_credentials())
+    assert can_connect(GCS_BUCKET, creds.to_object_store_rs_credentials())
 
     # GcpOAuthCredentialsWithoutDefaults is currently not supported
     with pytest.raises(NotImplementedError):
