a8546494e57abf27b1550abdd9739ba0b59ce36c
Marcel Coetzee <marcel@mooncoon.com>
2024-06-15 16:17:38 +0200

    Refactor lancedb_client.py for better code readability
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- dlt/destinations/impl/lancedb/lancedb_client.py
+++ dlt/destinations/impl/lancedb/lancedb_client.py
@@ -21,7 +21,6 @@ from lancedb.embeddings import EmbeddingFunctionRegistry, TextEmbeddingFunction 
 from lancedb.query import LanceQueryBuilder  # type: ignore
 from lancedb.table import Table  # type: ignore[import-untyped]
 from numpy import ndarray
-from orjson import JSONDecodeError
 from pyarrow import Array, ChunkedArray, ArrowInvalid
 
 from dlt.common import json, pendulum, logger
@@ -74,7 +73,9 @@ from dlt.destinations.type_mapping import TypeMapper
 
 
 TIMESTAMP_PRECISION_TO_UNIT: Dict[int, str] = {0: "s", 3: "ms", 6: "us", 9: "ns"}
-UNIT_TO_TIMESTAMP_PRECISION: Dict[str, int] = {v: k for k, v in TIMESTAMP_PRECISION_TO_UNIT.items()}
+UNIT_TO_TIMESTAMP_PRECISION: Dict[str, int] = {
+    v: k for k, v in TIMESTAMP_PRECISION_TO_UNIT.items()
+}
 
 
 class LanceDBTypeMapper(TypeMapper):
@@ -180,7 +181,9 @@ def upload_batch(
             tbl.add(records, mode="overwrite")
         elif write_disposition == "merge":
             if not id_field_name:
-                raise ValueError("To perform a merge update, 'id_field_name' must be specified.")
+                raise ValueError(
+                    "To perform a merge update, 'id_field_name' must be specified."
+                )
             tbl.merge_insert(
                 id_field_name
             ).when_matched_update_all().when_not_matched_insert_all().execute(records)
@@ -252,7 +255,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
         )
 
     @lancedb_error
-    def create_table(self, table_name: str, schema: TArrowSchema, mode: str = "create") -> Table:
+    def create_table(
+        self, table_name: str, schema: TArrowSchema, mode: str = "create"
+    ) -> Table:
         """Create a LanceDB Table from the provided LanceModel or PyArrow schema.
 
         Args:
@@ -355,7 +360,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
         applied_update: TSchemaTables = {}
 
         try:
-            schema_info = self.get_stored_schema_by_hash(self.schema.stored_version_hash)
+            schema_info = self.get_stored_schema_by_hash(
+                self.schema.stored_version_hash
+            )
         except DestinationUndefinedEntity:
             schema_info = None
 
@@ -404,19 +411,25 @@ class LanceDBClient(JobClientBase, WithStateSync):
 
         # Check if any of the new fields already exist in the table.
         existing_fields = set(arrow_table.schema.names)
-        new_fields = [field for field in field_schemas if field.name not in existing_fields]
+        new_fields = [
+            field for field in field_schemas if field.name not in existing_fields
+        ]
 
         if not new_fields:
             # All fields already present, skip.
             return None
 
-        null_arrays = [pa.nulls(len(arrow_table), type=field.type) for field in new_fields]
+        null_arrays = [
+            pa.nulls(len(arrow_table), type=field.type) for field in new_fields
+        ]
 
         for field, null_array in zip(new_fields, null_arrays):
             arrow_table = arrow_table.append_column(field, null_array)
 
         try:
-            return self.db_client.create_table(table_name, arrow_table, mode="overwrite")
+            return self.db_client.create_table(
+                table_name, arrow_table, mode="overwrite"
+            )
         except OSError:
             # Error occurred while creating the table, skip.
             return None
@@ -424,11 +437,15 @@ class LanceDBClient(JobClientBase, WithStateSync):
     def _execute_schema_update(self, only_tables: Iterable[str]) -> None:
         for table_name in only_tables or self.schema.tables:
             exists, existing_columns = self.get_storage_table(table_name)
-            new_columns = self.schema.get_new_table_columns(table_name, existing_columns)
+            new_columns = self.schema.get_new_table_columns(
+                table_name, existing_columns
+            )
             embedding_fields: List[str] = get_columns_names_with_prop(
                 self.schema.get_table(table_name), VECTORIZE_HINT
             )
-            logger.info(f"Found {len(new_columns)} updates for {table_name} in {self.schema.name}")
+            logger.info(
+                f"Found {len(new_columns)} updates for {table_name} in {self.schema.name}"
+            )
             if len(new_columns) > 0:
                 if exists:
                     field_schemas: List[TArrowField] = [
@@ -447,7 +464,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
                         vector_field_name = self.vector_field_name
                         id_field_name = self.id_field_name
                         embedding_model_func = self.model_func
-                        embedding_model_dimensions = self.config.embedding_model_dimensions
+                        embedding_model_dimensions = (
+                            self.config.embedding_model_dimensions
+                        )
                     else:
                         embedding_fields = None
                         vector_field_name = None
@@ -482,7 +501,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
                 "schema": json.dumps(self.schema.to_dict()),
             }
         ]
-        fq_version_table_name = self.make_qualified_table_name(self.schema.version_table_name)
+        fq_version_table_name = self.make_qualified_table_name(
+            self.schema.version_table_name
+        )
         write_disposition = self.schema.get_table(self.schema.version_table_name).get(
             "write_disposition"
         )
@@ -496,8 +517,12 @@ class LanceDBClient(JobClientBase, WithStateSync):
     @lancedb_error
     def get_stored_state(self, pipeline_name: str) -> Optional[StateInfo]:
         """Loads compressed state from destination storage by finding a load ID that was completed."""
-        fq_state_table_name = self.make_qualified_table_name(self.schema.state_table_name)
-        fq_loads_table_name = self.make_qualified_table_name(self.schema.loads_table_name)
+        fq_state_table_name = self.make_qualified_table_name(
+            self.schema.state_table_name
+        )
+        fq_loads_table_name = self.make_qualified_table_name(
+            self.schema.loads_table_name
+        )
 
         state_records = (
             self.db_client.open_table(fq_state_table_name)
@@ -518,12 +543,18 @@ class LanceDBClient(JobClientBase, WithStateSync):
                 .to_list()
             ):
                 state["dlt_load_id"] = state.pop("_dlt_load_id")
-                return StateInfo(**{k: v for k, v in state.items() if k in StateInfo._fields})
+                return StateInfo(
+                    **{k: v for k, v in state.items() if k in StateInfo._fields}
+                )
         return None
 
     @lancedb_error
-    def get_stored_schema_by_hash(self, schema_hash: str) -> Optional[StorageSchemaInfo]:
-        fq_version_table_name = self.make_qualified_table_name(self.schema.version_table_name)
+    def get_stored_schema_by_hash(
+        self, schema_hash: str
+    ) -> Optional[StorageSchemaInfo]:
+        fq_version_table_name = self.make_qualified_table_name(
+            self.schema.version_table_name
+        )
 
         try:
             response = (
@@ -540,7 +571,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
     @lancedb_error
     def get_stored_schema(self) -> Optional[StorageSchemaInfo]:
         """Retrieves newest schema from destination storage."""
-        fq_version_table_name = self.make_qualified_table_name(self.schema.version_table_name)
+        fq_version_table_name = self.make_qualified_table_name(
+            self.schema.version_table_name
+        )
 
         try:
             response = (
@@ -576,7 +609,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
                 "schema_version_hash": None,  # Payload schema must match the target schema.
             }
         ]
-        fq_loads_table_name = self.make_qualified_table_name(self.schema.loads_table_name)
+        fq_loads_table_name = self.make_qualified_table_name(
+            self.schema.loads_table_name
+        )
         write_disposition = self.schema.get_table(self.schema.loads_table_name).get(
             "write_disposition"
         )
@@ -590,7 +625,9 @@ class LanceDBClient(JobClientBase, WithStateSync):
     def restore_file_load(self, file_path: str) -> LoadJob:
         return EmptyLoadJob.from_file_path(file_path, "completed")
 
-    def start_file_load(self, table: TTableSchema, file_path: str, load_id: str) -> LoadJob:
+    def start_file_load(
+        self, table: TTableSchema, file_path: str, load_id: str
+    ) -> LoadJob:
         return LoadLanceDBJob(
             self.schema,
             table,
@@ -629,7 +666,9 @@ class LoadLanceDBJob(LoadJob):
         self.table_name: str = table_schema["name"]
         self.fq_table_name: str = fq_table_name
         self.unique_identifiers: Sequence[str] = list_unique_identifiers(table_schema)
-        self.embedding_fields: List[str] = get_columns_names_with_prop(table_schema, VECTORIZE_HINT)
+        self.embedding_fields: List[str] = get_columns_names_with_prop(
+            table_schema, VECTORIZE_HINT
+        )
         self.embedding_model_func: TextEmbeddingFunction = model_func
         self.embedding_model_dimensions: int = client_config.embedding_model_dimensions
         self.id_field_name: str = client_config.id_field_name
@@ -637,24 +676,8 @@ class LoadLanceDBJob(LoadJob):
             TWriteDisposition, self.table_schema.get("write_disposition", "append")
         )
 
-        records: List[DictStrAny]
         with FileStorage.open_zipsafe_ro(local_path) as f:
-            try:
-                records = json.load(f)
-            except JSONDecodeError:
-                # If parsing as a single object fails, try a line at a time.
-                records = []
-                f.seek(0)
-                for line in f:
-                    try:
-                        json_object = json.loads(line)
-                        records.append(json_object)
-                    except JSONDecodeError:
-                        raise
-
-        # Batch load only accepts a list of dicts.
-        if not isinstance(records, list) and isinstance(records, dict):
-            records = [records]
+            records: List[DictStrAny] = [json.loads(line) for line in f]
 
         if self.table_schema not in self.schema.dlt_tables():
             for record in records:
