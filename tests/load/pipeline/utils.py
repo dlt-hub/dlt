43d6d5fa7ea073409ac306766202408393aaafaa
Marcin Rudolf <rudolfix@rudolfix.org>
2024-06-17 20:15:56 +0200

    fixes drop_pipeline fixture

--- tests/load/pipeline/utils.py
+++ tests/load/pipeline/utils.py
@@ -32,36 +32,38 @@ def drop_pipeline(request) -> Iterator[None]:
 def drop_active_pipeline_data() -> None:
     """Drops all the datasets for currently active pipeline, wipes the working folder and then deactivated it."""
     if Container()[PipelineContext].is_active():
-        # take existing pipeline
-        p = dlt.pipeline()
+        try:
+            # take existing pipeline
+            p = dlt.pipeline()
 
-        def _drop_dataset(schema_name: str) -> None:
-            with p.destination_client(schema_name) as client:
-                try:
-                    client.drop_storage()
-                    print("dropped")
-                except Exception as exc:
-                    print(exc)
-                if isinstance(client, WithStagingDataset):
-                    with client.with_staging_dataset():
-                        try:
-                            client.drop_storage()
-                            print("staging dropped")
-                        except Exception as exc:
-                            print(exc)
+            def _drop_dataset(schema_name: str) -> None:
+                with p.destination_client(schema_name) as client:
+                    try:
+                        client.drop_storage()
+                        print("dropped")
+                    except Exception as exc:
+                        print(exc)
+                    if isinstance(client, WithStagingDataset):
+                        with client.with_staging_dataset():
+                            try:
+                                client.drop_storage()
+                                print("staging dropped")
+                            except Exception as exc:
+                                print(exc)
 
-        # drop_func = _drop_dataset_fs if _is_filesystem(p) else _drop_dataset_sql
-        # take all schemas and if destination was set
-        if p.destination:
-            if p.config.use_single_dataset:
-                # drop just the dataset for default schema
-                if p.default_schema_name:
-                    _drop_dataset(p.default_schema_name)
-            else:
-                # for each schema, drop the dataset
-                for schema_name in p.schema_names:
-                    _drop_dataset(schema_name)
+            # drop_func = _drop_dataset_fs if _is_filesystem(p) else _drop_dataset_sql
+            # take all schemas and if destination was set
+            if p.destination:
+                if p.config.use_single_dataset:
+                    # drop just the dataset for default schema
+                    if p.default_schema_name:
+                        _drop_dataset(p.default_schema_name)
+                else:
+                    # for each schema, drop the dataset
+                    for schema_name in p.schema_names:
+                        _drop_dataset(schema_name)
 
-        # p._wipe_working_folder()
-        # deactivate context
-        Container()[PipelineContext].deactivate()
+            # p._wipe_working_folder()
+        finally:
+            # always deactivate context, working directory will be wiped when the next test starts
+            Container()[PipelineContext].deactivate()
