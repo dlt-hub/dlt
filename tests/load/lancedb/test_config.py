7f08f85ad9c64cab61e7419be768032531f9080e
Marcel Coetzee <marcel@mooncoon.com>
2024-06-21 15:23:15 +0200

    Minor test config
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- tests/load/lancedb/test_config.py
+++ tests/load/lancedb/test_config.py
@@ -1,14 +1,15 @@
-from typing import Iterator, Any
+from typing import Iterator
 
 import pytest
 
+from tests.common.configuration.utils import environment
 from dlt.common.configuration import resolve_configuration
 from dlt.common.utils import digest128
 from dlt.destinations.impl.lancedb.configuration import LanceDBClientConfiguration
 from tests.load.pipeline.utils import (
     drop_active_pipeline_data,
 )
-from tests.common.configuration.utils import environment
+
 
 
 # Mark all tests as essential, do not remove.
@@ -21,9 +22,7 @@ def drop_lancedb_data() -> Iterator[None]:
     drop_active_pipeline_data()
 
 
-def test_lancedb_configuration(
-    environment: Any,
-) -> None:
+def test_lancedb_configuration(environment) -> None:
     # Ensure that api key and endpoint defaults are applied without exception.
 
     config = resolve_configuration(
@@ -31,11 +30,11 @@ def test_lancedb_configuration(
         sections=("destination", "lancedb"),
     )
     assert config.embedding_model_provider == "cohere"
+    assert config.embedding_model == "embed-english-v3.0"
+    assert config.embedding_model_dimensions is None
+    assert config.sentinel_table_name == "dltSentinelTable"
     assert config.id_field_name == "id__"
     assert config.vector_field_name == "vector__"
-    assert config.sentinel_table_name == "dltSentinelTable"
-    assert config.embedding_model_dimensions is None
-    assert config.embedding_model == "embed-english-v3.0"
     assert config.dataset_separator == "___"
     assert config.credentials.uri == ".lancedb"
     assert config.credentials.api_key is None
