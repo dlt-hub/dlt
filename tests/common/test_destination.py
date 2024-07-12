c765fc0fc3914627eee197e21d003449541a2407
IlyaFaer <gurovilya9@gmail.com>
2024-07-12 11:48:27 +0400

    move tests to verified-sources

--- tests/common/test_destination.py
+++ tests/common/test_destination.py
@@ -400,34 +400,3 @@ def test_normalize_dataset_name_none_default_schema() -> None:
         .normalize_dataset_name(Schema("default"))
         == "ban_ana_dataset"
     )
-
-
-@pytest.mark.parametrize("destination_name", IMPLEMENTED_DESTINATIONS)
-def test_destination_info(destination_name):
-    p = dlt.pipeline(
-        pipeline_name="dest_info",
-        destination=destination_name,
-        dataset_name="dest_info_data",
-    )
-    assert p.destination.destination_info == {
-        "destination_name": p.destination.destination_name,
-        "destination_type": p.destination.destination_type,
-        "environment": None,
-        "fingerprint": p.destination.configuration(None).fingerprint(),
-        "repr": p.destination.destination_description,
-    }
-
-
-def test_destination_info_environment():
-    duck = duckdb(
-        credentials=os.path.join(TEST_STORAGE_ROOT, "quack.duckdb"),
-        destination_name="duck1",
-        environment="production",
-    )
-    assert duck.destination_info == {
-        "destination_name": "duck1",
-        "destination_type": "dlt.destinations.duckdb",
-        "environment": "production",
-        "fingerprint": duck.configuration(None).fingerprint(),
-        "repr": "duck1(dlt.destinations.duckdb)",
-    }
