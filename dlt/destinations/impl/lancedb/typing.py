3f7a82f972561266d14e3e6c4e6880f1757ac5ff
Marcel Coetzee <marcel@mooncoon.com>
2024-07-19 12:18:14 +0200

    Add type definitions for chunk splitter function and related types
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- dlt/destinations/impl/lancedb/typing.py
+++
@@ -1,6 +0,0 @@
-from typing import Callable, Union, List, Dict, Any
-
-ChunkInputT = Union[str, Dict[str, Any], Any]
-ChunkOutputT = List[Any]
-
-TSplitter = Callable[[ChunkInputT, Any], ChunkOutputT]
