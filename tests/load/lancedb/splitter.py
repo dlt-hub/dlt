cd2960d2be1f724543b3a6bdd1e464e888f8d671
Marcel Coetzee <marcel@mooncoon.com>
2024-07-22 11:13:53 +0200

    Remove unrelated code
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- tests/load/lancedb/splitter.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.skip(reason="not implemented")
-def test_embedding_provider_only_called_once_per_chunk_hash() -> None:
-    """Verify that the embedding provider is called only once for each unique chunk hash to optimize API usage and reduce costs."""
-    raise NotImplementedError
