e340c313a3db87dff5b38c73411dd22b85aa54ce
Jorrit Sandbrink <47451109+jorritsandbrink@users.noreply.github.com>
2024-07-29 23:59:51 +0400

    Enable `upsert` merge strategy for more SQL destinations (#1628)
    * enable upsert merge strategy for athena, bigquery, databricks and mssql
    
    * add new upsert destinations to docs

--- dlt/destinations/impl/databricks/factory.py
+++ dlt/destinations/impl/databricks/factory.py
@@ -42,7 +42,7 @@ class databricks(Destination[DatabricksClientConfiguration, "DatabricksClient"])
         caps.alter_add_multi_column = True
         caps.supports_multiple_statements = False
         caps.supports_clone_table = True
-        caps.supported_merge_strategies = ["delete-insert", "scd2"]
+        caps.supported_merge_strategies = ["delete-insert", "upsert", "scd2"]
         return caps
 
     @property
