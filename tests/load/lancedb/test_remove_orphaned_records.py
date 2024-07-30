d338586b115ef6e5fbd1a4578fe6d374b42be714
Marcel Coetzee <marcel@mooncoon.com>
2024-07-30 22:41:48 +0200

    Update LanceDB orphaned records removal test for chunked documents
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- tests/load/lancedb/test_remove_orphaned_records.py
+++ tests/load/lancedb/test_remove_orphaned_records.py
@@ -11,8 +11,6 @@ from tests.load.utils import (
 )
 from tests.pipeline.utils import (
     assert_load_info,
-    load_table_counts,
-    load_tables_to_dicts,
 )
 
 
@@ -54,31 +52,23 @@ def test_lancedb_remove_orphaned_records(
     info = pipeline.run(identity_resource(run_1))
     assert_load_info(info)
 
-    counts = load_table_counts(pipeline, "parent", "parent__child")
-    assert counts["parent"] == 2
-    assert counts["parent__child"] == 4
-
     run_2 = [
         {"id": 1, "child": [{"bar": 1}]},  # Removed one child.
-        {"id": 2, "child": [{"bar": 4}, {"baz": 1}]},  # Changed child.
+        {"id": 2, "child": [{"bar": 4}]},  # Changed child.
     ]
     info = pipeline.run(identity_resource(run_2))
     assert_load_info(info)
 
-    # Check whether orphaned child records were removed.
-    counts = load_table_counts(pipeline, "parent", "parent__child")
-    assert counts["parent"] == 2
-    assert counts["parent__child"] == 2
+    with pipeline.destination_client() as client:
+        expected_child_data = [
+            1,
+            4,
+            10,
+            11,
+        ]
 
-    child_data = load_tables_to_dicts(pipeline, "parent__child")
-    expected_child_data = [
-        {"bar": 1},
-        {"bar": 4},
-        {"baz": 1},
-        {"bar": 10},
-        {"bar": 11},
-    ]
-    assert (
-        sorted(child_data["parent__child"], key=lambda x: x["bar"])
-        == expected_child_data
-    )
+        embeddings_table_name = client.make_qualified_table_name("parent__child")  # type: ignore[attr-defined]
+
+        tbl = client.db_client.open_table(embeddings_table_name)  # type: ignore[attr-defined]
+        df = tbl.to_pandas()
+        assert sorted(df["bar"].to_list()) == expected_child_data
