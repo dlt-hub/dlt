036801829879d34d54052755560debb2f6c3e1f8
Marcel Coetzee <marcel@mooncoon.com>
2024-08-02 20:03:09 +0200

    Set default file size to 128MB
    Signed-off-by: Marcel Coetzee <marcel@mooncoon.com>

--- dlt/destinations/impl/lancedb/factory.py
+++ dlt/destinations/impl/lancedb/factory.py
@@ -30,6 +30,8 @@ class lancedb(Destination[LanceDBClientConfiguration, "LanceDBClient"]):
         caps.decimal_precision = (38, 18)
         caps.timestamp_precision = 6
 
+        caps.recommended_file_size = 128_000_000
+
         return caps
 
     @property
