299cc2ba7be7c1d7d84817dabd83c158b75de328
dat-a-man <98139823+dat-a-man@users.noreply.github.com>
2024-08-10 06:50:29 +0000

    Add custom parent-child relationships example

---
+++ docs/examples/parent_child_relationship/parent_child_relationship.py
@@ -0,0 +1,65 @@
+"""
+---
+title: Load parent table keys into child table
+description: Learn how integrate custom parent keys into child records
+keywords: [parent child relationship, parent key]
+---
+
+This example demonstrates handling data with parent-child relationships using the `dlt` library. This process is about integrating specific fields (e.g. primary, foreign keys) from a parent record into each child record, ensuring that we can use self defined parent keys in the child table. 
+
+In this example, we'll explore how to:
+
+- Integrate a custom `parent_id` into each child record.
+- Ensure every child is correctly linked to its `parent_id` using a tailored function, `add_parent_id`.
+- Use the [`add_map` function](https://dlthub.com/docs/api_reference/extract/resource#add_map) to apply this custom logic to every record in our dataset.
+"""
+
+import dlt
+
+# Define a dlt resource with write disposition to 'merge'
+@dlt.resource(name='parent_with_children', write_disposition={"disposition": "merge"})
+def data_source() -> Dict[str, Any]:
+    # Example data 
+    data = [
+        {
+            'parent_id': 1,
+            'parent_name': 'Alice',
+            'children': [
+                {'child_id': 1, 'child_name': 'Child 1'},
+                {'child_id': 2, 'child_name': 'Child 2'}
+            ]
+        },
+        {
+            'parent_id': 2,
+            'parent_name': 'Bob',
+            'children': [
+                {'child_id': 3, 'child_name': 'Child 3'}
+            ]
+        }
+    ]
+
+    yield data
+
+# Function to add parent_id to each child record within a parent record
+def add_parent_id(record):
+    parent_id_key = "parent_id"
+    for child in record["children"]:
+        child[parent_id_key] = record[parent_id_key]
+    return record
+
+# Create and configure the dlt pipeline
+pipeline = dlt.pipeline(
+    pipeline_name='generic_pipeline',
+    destination='duckdb',
+    dataset_name='dataset',
+)
+
+# Run the pipeline
+load_info = pipeline.run(
+    data_source()
+    .add_map(add_parent_id),
+    primary_key="parent_id"
+)
+
+# print pipeline load info
+print(load_info)
