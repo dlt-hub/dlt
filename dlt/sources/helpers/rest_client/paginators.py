49b45fb4592e53e2d0d7eaf09c1c4279927b7853
Willi <willi.mueller@posteo.de>
2024-08-16 17:12:21 +0530

    sets default argument to None

--- dlt/sources/helpers/rest_client/paginators.py
+++ dlt/sources/helpers/rest_client/paginators.py
@@ -164,7 +164,7 @@ class RangePaginator(BasePaginator):
             ):
                 self._has_next_page = False
 
-    def _stop_after_this_page(self, data: Optional[List[Any]]) -> bool:
+    def _stop_after_this_page(self, data: Optional[List[Any]]=None) -> bool:
         return self.stop_after_empty_page and data == []
 
     def _handle_missing_total(self, response_json: Dict[str, Any]) -> None:
