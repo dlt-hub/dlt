386b74b2772fe0065711f575d53b840caa9ad1a3
Julian Alves <28436330+donotpush@users.noreply.github.com>
2024-08-20 18:44:55 +0200

    feat: add snowflake timezone and precision flag

--- dlt/destinations/impl/snowflake/snowflake.py
+++ dlt/destinations/impl/snowflake/snowflake.py
@@ -18,7 +18,7 @@ from dlt.common.storages.configuration import FilesystemConfiguration
 from dlt.common.storages.file_storage import FileStorage
 from dlt.common.schema import TColumnSchema, Schema, TTableSchemaColumns
 from dlt.common.schema.typing import TTableSchema, TColumnType, TTableFormat
-
+from dlt.common.exceptions import TerminalValueError
 
 from dlt.common.storages.load_package import ParsedLoadJobFileName
 from dlt.common.typing import TLoaderFileFormat
@@ -76,6 +76,36 @@ class SnowflakeTypeMapper(TypeMapper):
                 return dict(data_type="wei")
             return dict(data_type="decimal", precision=precision, scale=scale)
         return super().from_db_type(db_type, precision, scale)
+
+    def to_db_datetime_type(
+        self,
+        column: TColumnSchema = None,
+        table: TTableSchema = None,
+    ) -> str:
+        column_name = column.get("name")
+        table_name = table.get("name")
+        timezone = column.get("timezone")
+        precision = column.get("precision")
+
+        if timezone is None and precision is None:
+            return super().to_db_datetime_type(column, table)
+
+        timestamp = "TIMESTAMP_TZ"
+
+        if timezone is not None and not timezone:  # explicitaly handles timezone False
+            timestamp = "TIMESTAMP_NTZ"
+
+        # append precision if specified and valid
+        if precision is not None:
+            if 0 <= precision <= 9:
+                timestamp += f"({precision})"
+            else:
+                raise TerminalValueError(
+                    f"Snowflake does not support precision '{precision}' for '{column_name}' in"
+                    f" table '{table_name}'"
+                )
+
+        return timestamp
 
 
 class SnowflakeLoadJob(RunnableLoadJob, HasFollowupJobs):
