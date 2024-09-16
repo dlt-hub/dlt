7b6470fa5ded13b50ccfb09f550790515fcf2ee8
dave <shrps@posteo.net>
2024-09-16 19:19:23 +0200

    Fix caching issue on historic docs Fix some broken links (will need  cleanup) Allow broken links in docs as v 0.5.4. still has some

--- docs/website/tools/preprocess_docs.js
+++ docs/website/tools/preprocess_docs.js
@@ -372,6 +372,7 @@ if (process.argv.includes("--watch")) {
       if (Date.now() - lastUpdate < 500) {
           return;
       }
+      fs.rmSync(MD_TARGET_DIR, {force: true, recursive: true})
       console.log('%s changed...', name);
       syncExamples();
       preprocess_docs();
