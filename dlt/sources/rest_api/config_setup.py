3af1d12e33b5e0292905eb06d6a438aa4a4e77a5
Antony Rinaldi <antony.rinaldi@bailliegifford.com>
2024-09-20 13:42:34 +0000

    output response text rather trying to serialize to json

--- dlt/sources/rest_api/config_setup.py
+++ dlt/sources/rest_api/config_setup.py
@@ -506,7 +506,7 @@ def _create_response_action_hook(
         elif action_type == "ignore":
             logger.info(
                 f"Ignoring response with code {response.status_code} "
-                f"and content '{response.json()}'."
+                f"and content '{response.text}'."
             )
             raise IgnoreResponseException
 
