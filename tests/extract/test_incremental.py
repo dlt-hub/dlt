cb6da7eab21f7b696f88309c711b34e3390b1d07
Willi <willi.mueller@posteo.de>
2024-09-28 18:07:42 +0530

    tests late binding of incremental, formats code

--- tests/extract/test_incremental.py
+++ tests/extract/test_incremental.py
@@ -2613,7 +2613,6 @@ def test_warning_large_deduplication_state(item_type: TestDataItemFormat, primar
 def test_no_deduplication_on_write_disposition_merge(
     item_type: TestDataItemFormat,
 ) -> None:
-
     if item_type in ["pandas", "arrow-table", "arrow-batch"]:
         os.environ["NORMALIZE__PARQUET_NORMALIZER__ADD_DLT_LOAD_ID"] = "True"
 
@@ -2660,5 +2659,50 @@ def test_no_deduplication_on_write_disposition_merge(
     assert s["last_value"] == 2
 
 
-def test_deduplication_on_write_disposition_not_merge() -> None:
-    pytest.skip()
+@pytest.mark.parametrize("item_type", ALL_TEST_DATA_ITEM_FORMATS)
+def test_no_deduplication_on_write_disposition_merge_late_binding(
+    item_type: TestDataItemFormat,
+) -> None:
+    if item_type in ["pandas", "arrow-table", "arrow-batch"]:
+        os.environ["NORMALIZE__PARQUET_NORMALIZER__ADD_DLT_LOAD_ID"] = "True"
+
+    @dlt.resource(write_disposition="merge", merge_key="id", primary_key="id")
+    def some_data():
+        yield data_to_item_format(
+            item_type,
+            [
+                {"id": 1, "created_at": 1},
+                {"id": 2, "created_at": 2},
+            ],
+        )
+
+    some_data.apply_hints(incremental=dlt.sources.incremental("created_at"))
+    p = dlt.pipeline(pipeline_name=uniq_id())
+    load_info = p.run(some_data(), destination="duckdb")
+    assert len(load_info.loads_ids) == 1
+    load_id_1 = load_info.loads_ids[0]
+
+    load_info_2 = p.run(some_data(), destination="duckdb")
+    load_id_2 = load_info_2.loads_ids[0]
+    assert load_id_1 != load_id_2
+
+    with p.sql_client() as client:
+        assert_query_data(p, "select id from some_data order by id", [1, 2])
+
+        # no duplicates
+        assert_query_data(p, "select id from some_data order by id", [1, 2])
+
+        load_ids = client.execute_sql("select _dlt_load_id from some_data where id = 2")
+        assert len(load_ids) == 1, "Expected row with id = 2 to be loaded only once"
+        load_id_of_duplicate_row = load_ids[0][0]
+
+        # row with id=2 got updated – even though it has the same merge_key as the first row with id=2
+        assert load_id_1 != load_id_of_duplicate_row, "Expected row with id = 2 to get updated"
+
+        # row with id=1 is not updated
+        assert_query_data(p, "select _dlt_load_id from some_data where id = 1", [load_id_1])
+
+    s = p.state["sources"][p.default_schema_name]["resources"]["some_data"]["incremental"][
+        "created_at"
+    ]
+    assert s["last_value"] == 2
