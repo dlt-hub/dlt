995cb22de1d3acb964941b4ccbcb6e3df3939928
Anton Burnashev <anton.burnashev@gmail.com>
2024-10-21 14:07:56 +0200

    Renames `d` param  to `mapping`

--- dlt/common/utils.py
+++ dlt/common/utils.py
@@ -504,17 +504,18 @@ def without_none(d: Mapping[TKey, Optional[TValue]]) -> Mapping[TKey, TValue]:
     return {k: v for k, v in d.items() if v is not None}
 
 
-def exclude_keys(d: Mapping[str, Any], keys: Iterable[str]) -> Dict[str, Any]:
-    """Removes specified keys from a dictionary and returns a new dictionary.
+def exclude_keys(mapping: Mapping[str, Any], keys: Iterable[str]) -> Dict[str, Any]:
+    """Create a new dictionary from the input mapping, excluding specified keys.
 
     Args:
-        d (Mapping[str, Any]): The dictionary to remove keys from.
-        keys (Iterable[str]): The keys to remove.
+        mapping (Mapping[str, Any]): The input mapping from which keys will be excluded.
+        keys (Iterable[str]): The keys to exclude.
 
     Returns:
-        Dict[str, Any]: A new dictionary with the specified keys removed.
+        Dict[str, Any]: A new dictionary containing all key-value pairs from the original
+                        mapping except those with keys specified in `keys`.
     """
-    return {k: v for k, v in d.items() if k not in keys}
+    return {k: v for k, v in mapping.items() if k not in keys}
 
 
 def get_full_class_name(obj: Any) -> str:
