1e947a6e94ec2960b99d00aa69629be3f5033aad
Anton Burnashev <anton.burnashev@gmail.com>
2024-11-13 17:03:04 +0100

    Includes table format in resource definition

--- tests/load/pipeline/test_merge_disposition.py
+++ tests/load/pipeline/test_merge_disposition.py
@@ -1550,6 +1550,7 @@ def test_merge_arrow(
     @dlt.resource(
         write_disposition={"disposition": "merge", "strategy": merge_strategy},
         primary_key="id",
+        table_format=destination_config.table_format,
     )
     def arrow_items(rows, schema_columns, timezone="UTC"):
         yield row_tuples_to_arrow(
@@ -1588,23 +1589,12 @@ def test_merge_arrow(
     )
 
     assert_load_info(load_info)
-    tables = load_tables_to_dicts(pipeline, "arrow_items")
+    tables = load_tables_to_dicts(pipeline, "arrow_items", "arrow_items")
 
-    if destination_config.table_format == "delta":
-        assert_records_as_set(
-            tables["arrow_items"],
-            [
-                {"id": 1, "name": "foo"},
-                {"id": 2, "name": "updated bar"},
-                {"id": 1, "name": "foo"},
-                {"id": 2, "name": "bar"},
-            ],
-        )
-    else:
-        assert_records_as_set(
-            tables["arrow_items"],
-            [
-                {"id": 1, "name": "foo"},
-                {"id": 2, "name": "updated bar"},
-            ],
-        )
+    assert_records_as_set(
+        tables["arrow_items"],
+        [
+            {"id": 1, "name": "foo"},
+            {"id": 2, "name": "updated bar"},
+        ],
+    )
