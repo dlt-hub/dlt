9d560d9325c084cde611bc507df6d21c95387923
Julian Alves <28436330+donotpush@users.noreply.github.com>
2025-01-15 10:03:52 +0100

    databricks: enable local files

--- dlt/destinations/impl/databricks/factory.py
+++ dlt/destinations/impl/databricks/factory.py
@@ -107,8 +107,8 @@ class databricks(Destination[DatabricksClientConfiguration, "DatabricksClient"])
 
     def _raw_capabilities(self) -> DestinationCapabilitiesContext:
         caps = DestinationCapabilitiesContext()
-        caps.preferred_loader_file_format = None
-        caps.supported_loader_file_formats = []
+        caps.preferred_loader_file_format = "parquet"
+        caps.supported_loader_file_formats = ["jsonl", "parquet"]
         caps.preferred_staging_file_format = "parquet"
         caps.supported_staging_file_formats = ["jsonl", "parquet"]
         caps.supported_table_formats = ["delta"]
