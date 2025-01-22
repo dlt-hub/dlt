6f5bedfb0d7327233b7065e469204c5912eb8868
zilto <zilto@github.com>
2025-01-22 10:52:50 -0500

    fix linting issue

--- dlt/helpers/streamlit_app/blocks/menu.py
+++ dlt/helpers/streamlit_app/blocks/menu.py
@@ -10,7 +10,7 @@ def menu(pipeline: dlt.Pipeline) -> None:
     mode_selector()
     st.logo(
         "https://cdn.sanity.io/images/nsq559ov/production/7f85e56e715b847c5519848b7198db73f793448d-82x25.svg?q=75&fit=clip&auto=format",
-        size="large"
+        size="large",
     )
     st.page_link(f"{HERE}/pages/dashboard.py", label="Explore data", icon="🕹️")
     st.page_link(f"{HERE}/pages/load_info.py", label="Load info", icon="💾")
