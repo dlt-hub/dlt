19d0d90905da35d0f92f2f995493312c0d987de0
Stan Tsvek <stanislav.ts@darly.solutions>
2025-02-20 15:34:09 +0100

    Update dlt+ Admonition title to lowercase

--- docs/website/src/theme/DocBreadcrumbs/index.js
+++ docs/website/src/theme/DocBreadcrumbs/index.js
@@ -11,7 +11,7 @@ import Admonition from "@theme/Admonition";
 
 function PlusAdmonition() {
   return (
-    <Admonition type="note" title="dlt+">
+    <Admonition type="note" title={<span style={{ textTransform: "lowercase" }}>dlt+</span>}>
       <p>
         This page is for dlt+, which requires a license. <a href="https://info.dlthub.com/waiting-list">Join our early access program</a> for a trial license.
       </p>
