386981e4a378da66ae79552da9bbaef970e6ac66
Stan Tsvek <stas.tsvek@gmail.com>
2025-03-05 15:23:30 +0100

    feat: update PlusAdmonition title to use lowercase styling

--- docs/website/src/theme/PlusAdmonition/index.js
+++ docs/website/src/theme/PlusAdmonition/index.js
@@ -2,7 +2,7 @@ import Admonition from "@theme/Admonition";
 
 export function PlusAdmonition() {
   return (
-    <Admonition type="note" title="dlt+">
+    <Admonition type="note" title={<span style={{ textTransform: "lowercase" }}>dlt+</span>}>
       <p>
         This page is for dlt+, which requires a license. <a href="https://info.dlthub.com/waiting-list">Join our early access program</a> for a trial license.
       </p>
