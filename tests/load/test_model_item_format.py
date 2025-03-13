d92d693cacc4fd6f5ae7ad6a998ee471e253149a
dave <shrps@posteo.net>
2025-03-13 15:42:08 +0100

    add correct insert job

--- tests/load/test_model_item_format.py
+++ tests/load/test_model_item_format.py
@@ -7,11 +7,10 @@ import dlt
 from dlt.common.destination.dataset import SupportsReadableDataset
 
 from tests.pipeline.utils import load_table_counts
-
 from dlt.extract.hints import make_hints
 
 
-def test_sql_job() -> None:
+def test_simple_model_jobs() -> None:
     # populate a table with 10 items and retrieve dataset
     pipeline = dlt.pipeline(
         pipeline_name="example_pipeline", destination="duckdb", dataset_name="example_dataset"
@@ -19,32 +18,39 @@ def test_sql_job() -> None:
     pipeline.run([{"a": i} for i in range(10)], table_name="example_table")
     dataset = pipeline.dataset()
 
+    example_table_columns = dataset.schema.tables["example_table"]["columns"]
+
     # create a resource that generates sql statements to create 2 new tables
+    # we also need to supply all hints so the table can be created
     @dlt.resource()
     def copied_table() -> Any:
         query = dataset["example_table"].limit(5).query()
         yield dlt.mark.with_hints(
-            f"CREATE OR REPLACE TABLE copied_table AS {query}",
-            make_hints(file_format="sql"),
+            query, hints=make_hints(columns=example_table_columns), data_item_format="model"
         )
 
+    @dlt.resource()
+    def copied_table_2() -> Any:
         query = dataset["example_table"].limit(7).query()
         yield dlt.mark.with_hints(
-            f"CREATE OR REPLACE TABLE copied_table2 AS {query}",
-            make_hints(file_format="sql"),
+            query, hints=make_hints(columns=example_table_columns), data_item_format="model"
         )
 
     # run sql jobs
-    pipeline.run(copied_table())
+    pipeline.run([copied_table(), copied_table_2()])
 
     # the two tables where created
-    assert load_table_counts(pipeline, "example_table", "copied_table", "copied_table2") == {
-        "example_table": 10,
+    assert load_table_counts(pipeline, "copied_table", "copied_table_2", "example_table") == {
         "copied_table": 5,
-        "copied_table2": 7,
+        "copied_table_2": 7,
+        "example_table": 10,
     }
 
     # we have a table entry for the main table "copied_table"
     assert "copied_table" in pipeline.default_schema.tables
-    # but no columns, it's up to the user to provide a schema
-    assert len(pipeline.default_schema.tables["copied_table"]["columns"]) == 0
+    # and we only have the three columns from the original table
+    assert set(pipeline.default_schema.tables["copied_table"]["columns"].keys()) == {
+        "a",
+        "_dlt_id",
+        "_dlt_load_id",
+    }
