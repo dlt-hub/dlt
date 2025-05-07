65bc1b2f3e554d2ad96cbc02025f670c4a841498
dave <shrps@posteo.net>
2025-05-07 12:12:03 +0200

    add config and fallback if destination not reachable

--- dlt/transformations/configuration.py
+++ dlt/transformations/configuration.py
@@ -1,15 +1,18 @@
-from typing import Optional
+from typing import Optional, ClassVar, Tuple
 
 from dlt.common.configuration import configspec
 from dlt.common.configuration.specs import BaseConfiguration
+from dlt.common.configuration.specs import known_sections
 
 
 @configspec
 class TransformConfiguration(BaseConfiguration):
     """Configuration for a transformation"""
 
-    chunk_size: Optional[int] = None
+    buffer_max_items: Optional[int] = 5000
     """
     The chunk size to use for the transformation. If not provided, the chunk size
     will be inferred from the transformations.
     """
+
+    __known_sections__: ClassVar[Tuple[str, ...]] = (known_sections.EXTRACT,)
