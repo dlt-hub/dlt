9de4f24e2b1e0bdc8331a16e3ef3ab055f45692f
hsm207 <hsm207@users.noreply.github.com>
2025-05-08 15:50:15 +0000

    refactor:  reformat code

--- tests/load/clickhouse/test_type_mapper.py
+++ tests/load/clickhouse/test_type_mapper.py
@@ -1,6 +1,8 @@
 import pytest
 from dlt.destinations.impl.clickhouse.factory import ClickHouseTypeMapper
 from dlt.common.schema.typing import TColumnSchema
+
+
 @pytest.mark.parametrize(
     "column,expected_type",
     [
@@ -10,11 +12,12 @@ from dlt.common.schema.typing import TColumnSchema
         (TColumnSchema(precision=32), "Int32"),
         (TColumnSchema(precision=64), "Int64"),
         (TColumnSchema(), "Int64"),  # No precision key at all
-    ]
+    ],
 )
 def test_to_db_integer_type_valid(column, expected_type):
     mapper = ClickHouseTypeMapper(capabilities=None)
     assert mapper.to_db_integer_type(column) == expected_type
+
 
 def test_to_db_integer_type_invalid():
     mapper = ClickHouseTypeMapper(capabilities=None)
