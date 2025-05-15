167a871affc0690c1404e3a1435575c57076a8f5
David Scharf <shrps@posteo.net>
2025-05-15 22:31:18 +0200

    add more first tests

---
+++ tests/helpers/studio/test_all_cells.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from dlt.helpers.studio import app
+
+from marimo._ast.cell import Cell
+import marimo as mo
+from marimo._runtime.control_flow import MarimoStopError
+
+global_defaults = {
+    "dlt_query_params": {},
+    "dlt_pipeline_name": "my_pipeline",
+    "dlt_page_tabs": mo.ui.tabs({"tab": ""}),
+    "dlt_data_table_list": [],
+    "dlt_pipeline_link_list": [],
+    "dlt_pipelines_dir": "some_dir",
+    "dlt_pipeline_select": mo.ui.multiselect([]),
+    "dlt_pipeline_count": 0,
+    "dlt_schem_table_list": [],
+    "dlt_query_result": [],
+    "dlt_query_editor": "",
+    "dlt_run_query_button": mo.ui.button(),
+    "dlt_cache_query_results": mo.ui.switch(),
+    "dlt_execute_query_on_change": mo.ui.switch(),
+    "dlt_schema_show_other_hints": mo.ui.switch(),
+    "dlt_schema_show_custom_hints": mo.ui.switch(),
+    "dlt_schema_show_dlt_columns": mo.ui.switch(),
+    "dlt_schema_show_type_hints": mo.ui.switch(),
+}
+
+
+def test_run_all_cells():
+    """
+    Runs al cells with basic values, StopException is allowed
+    """
+    cells: List[Cell] = []
+    for item in dir(app):
+        c = getattr(app, item)
+        if isinstance(c, Cell):
+            cells.append(c)
+
+    assert len(cells) > 0, "No cells found"
+
+    for cell in cells:
+        if cell.name == "prepare_query_vars":
+            continue
+        try:
+            run_args = {k: v for k, v in global_defaults.items() if k in cell.refs}
+            cell.run(**run_args)
+        except MarimoStopError:
+            pass
