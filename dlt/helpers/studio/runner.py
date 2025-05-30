82348aafc8eb8bc21249ae30c4ad9bce833b2df7
David Scharf <shrps@posteo.net>
2025-05-30 13:54:29 +0200

    remove marimo extra and create dependency groups for marimo and streamlit

--- dlt/helpers/studio/runner.py
+++ dlt/helpers/studio/runner.py
@@ -5,11 +5,13 @@ from dlt.common.exceptions import MissingDependencyException
 # keep this, will raise if user tries to run studio without dependencies
 try:
     import marimo
+    import ibis
+    import pyarrow
 except ModuleNotFoundError:
     raise MissingDependencyException(
         "dlt Studio",
-        ["marimo"],
-        "dlt Studio requires additional dependencies such as marimo and pyarrow.",
+        ["marimo", "pyarrow", "ibis-framework"],
+        "dlt Studio requires additional dependencies, such as marimo, pyarrow and ibis-framework.",
     )
 
 
