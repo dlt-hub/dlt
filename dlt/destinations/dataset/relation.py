772069fc0422e29489187473f357f4dd341c8b83
David Scharf <shrps@posteo.net>
2025-06-03 11:24:34 +0200

    move query normalization into utils, cache result and do not modify original qualified query

--- dlt/destinations/dataset/relation.py
+++ dlt/destinations/dataset/relation.py
@@ -16,6 +16,7 @@ from dlt.destinations.sql_client import SqlClientBase, WithSqlClient
 from dlt.destinations.dataset.exceptions import (
     ReadableRelationHasQueryException,
 )
+from dlt.destinations.dataset.utils import normalize_query
 
 if TYPE_CHECKING:
     from dlt.destinations.dataset.dataset import ReadableDBAPIDataset
@@ -68,7 +69,9 @@ class BaseReadableDBAPIRelation(SupportsReadableRelation, WithSqlClient):
         #   also it does a heavy computation inside so it should be a method
         if not self._should_normalize_query:
             return self._query()
-        return self._normalize_query()
+        return self.normalized_query.sql(
+            dialect=self._dataset.sql_client.capabilities.sqlglot_dialect
+        )
 
     def _query(self) -> Any:
         """Returns a compliant with dlt schema in the relation.
@@ -77,68 +80,6 @@ class BaseReadableDBAPIRelation(SupportsReadableRelation, WithSqlClient):
         3. dlt schema compat aliases are not added to top level selects
         """
         raise NotImplementedError("No query in ReadableDBAPIRelation")
-
-    def _normalize_query(self) -> str:
-        caps = self._dataset.sql_client.capabilities
-        target_dialect = caps.sqlglot_dialect
-
-        # compute schema so other properties are available
-        _ = self.columns_schema
-        parsed_query = self._qualified_query
-
-        if parsed_query is not None:
-            # do we need to case-fold identifiers?
-            is_casefolding = caps.casefold_identifier is not str
-
-            # preserve "column" names in original selects which are done in dlt schema namespace
-            orig_selects: Dict[int, str] = None
-            if is_casefolding:
-                orig_selects = {}
-                for i, proj in enumerate(parsed_query.selects):
-                    orig_selects[i] = proj.name or proj.args["alias"].name
-
-            # case fold all identifiers and quote
-            for node in parsed_query.walk():
-                if isinstance(node, sge.Table):
-                    # expand named of known tables. this is currently clickhouse things where
-                    # we use dataset.table in queries but render those as dataset___table
-                    if self._dataset.sqlglot_schema.column_names(node):
-                        expanded_path = self.sql_client.make_qualified_table_name_path(
-                            node.name, quote=False, casefold=False
-                        )
-                        # set the table name
-                        if node.name != expanded_path[-1]:
-                            node.this.set("this", expanded_path[-1])
-                        # set the dataset/schema name
-                        if node.db != expanded_path[-2]:
-                            node.set("db", sqlglot.to_identifier(expanded_path[-2], quoted=False))
-                        # set the catalog name
-                        if len(expanded_path) == 3:
-                            if node.db != expanded_path[0]:
-                                node.set(
-                                    "catalog", sqlglot.to_identifier(expanded_path[0], quoted=False)
-                                )
-                # quote and case-fold identifiers, TODO: maybe we could be more intelligent, but then we need to unquote ibis
-                if isinstance(node, sge.Identifier):
-                    if is_casefolding:
-                        node.set("this", caps.casefold_identifier(node.this))
-                    node.set("quoted", True)
-
-            # add aliases to output selects to stay compatible with dlt schema after the query
-            if orig_selects:
-                for i, orig in orig_selects.items():
-                    case_folded_orig = caps.casefold_identifier(orig)
-                    if case_folded_orig != orig:
-                        # somehow we need to alias just top select in UNION (tested on Snowflake)
-                        sel_expr = parsed_query.selects[i]
-                        parsed_query.selects[i] = sge.alias_(sel_expr, orig, quoted=True)
-
-            query = parsed_query.sql(dialect=target_dialect)
-        else:
-            # TODO: log failed to obtain schema
-            pass
-
-        return query
 
     @contextmanager
     def cursor(self) -> Generator[SupportsReadableRelation, Any, Any]:
@@ -223,12 +164,11 @@ class BaseReadableDBAPIRelation(SupportsReadableRelation, WithSqlClient):
         ):
             return {}, None
 
-        # get dlt schema compliant query so lineage will work correctly on non case folded identifiers
-        query = self._query()
         dialect: str = self._dataset.sql_client.capabilities.sqlglot_dialect
 
         return lineage.compute_columns_schema(
-            query,
+            # use dlt schema compliant query so lineage will work correctly on non case folded identifiers
+            self._query(),
             self._dataset.sqlglot_schema,
             dialect,
             infer_sqlglot_schema=infer_sqlglot_schema,
@@ -245,6 +185,20 @@ class BaseReadableDBAPIRelation(SupportsReadableRelation, WithSqlClient):
     @columns_schema.setter
     def columns_schema(self, new_value: TTableSchemaColumns) -> None:
         raise NotImplementedError("Columns Schema may not be set")
+
+    @property
+    def qualified_query(self) -> sge.Query:
+        if self._qualified_query is None:
+            self._columns_schema, self._qualified_query = self._compute_columns_schema()
+        return self._qualified_query
+
+    @property
+    def normalized_query(self) -> sge.Query:
+        if self._normalized_query is None:
+            self._normalized_query = normalize_query(
+                self._dataset.sqlglot_schema, self.qualified_query, self._dataset.sql_client
+            )
+        return self._normalized_query
 
 
 class ReadableDBAPIRelation(BaseReadableDBAPIRelation):
