a9f73c9f5589d2667f19cd286a66616bdcb5ebc8
zilto <68975210+zilto@users.noreply.github.com>
2025-06-04 21:08:16 -0400

    working prototype

--- dlt/destinations/dataset/ibis_relation.py
+++ dlt/destinations/dataset/ibis_relation.py
@@ -2,6 +2,8 @@ from typing import TYPE_CHECKING, Any, Union, Sequence
 from functools import partial
 import sqlglot
 
+import dlt
+from dlt._dataset import ops
 from dlt.destinations.dataset.relation import BaseReadableDBAPIRelation
 
 
