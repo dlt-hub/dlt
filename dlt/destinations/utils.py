52aa277ddfa69b7e175d3d6466c2244c893270ec
zilto <68975210+zilto@users.noreply.github.com>
2025-06-06 09:50:52 -0400

    add raise keyword

--- dlt/destinations/utils.py
+++ dlt/destinations/utils.py
@@ -41,7 +41,7 @@ def get_resource_for_adapter(data: Any) -> DltResource:
         if len(data.selected_resources.keys()) == 1:
             return list(data.selected_resources.values())[0]
         else:
-            ValueError(
+            raise ValueError(
                 "You are trying to use an adapter on a `DltSource` with multiple resources. You can"
                 " only use adapters on: pure data, a `DltResouce` or a `DltSource` with a single"
                 " `DltResource`."
