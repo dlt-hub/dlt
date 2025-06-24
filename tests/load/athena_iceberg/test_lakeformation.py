b71a7feb536cd9d130256f42ddd521ae0629abfb
Marcin Rudolf <rudolfix@rudolfix.org>
2025-06-25 00:21:26 +0200

    fixes lakeformation tests to use ci credentials and to run tests selectively

--- tests/load/athena_iceberg/test_lakeformation.py
+++ tests/load/athena_iceberg/test_lakeformation.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Generator, Optional
+from typing import Iterator, Tuple, List, Generator, Optional
 
 from mypy_boto3_lakeformation import LakeFormationClient
 from mypy_boto3_lakeformation.type_defs import (
@@ -7,21 +7,40 @@ from mypy_boto3_lakeformation.type_defs import (
 )
 import pytest
 import boto3
+from botocore.session import Session
 
 import dlt
-from dlt.destinations import filesystem, athena
 from dlt.common.utils import uniq_id
+
+from dlt.sources.credentials import AwsCredentials
+
+from dlt.destinations import filesystem, athena
 from dlt.destinations.impl.athena.configuration import LakeformationConfig
 
-IAM_ARN = boto3.client("sts").get_caller_identity()["Arn"]
-ATHENA_WORKGROUP = dlt.config.get("tests.athena_workgroup", str)
-QUERY_RESULT_BUCKET = dlt.config.get("tests.query_results_bucket", str)
+from tests.load.utils import destinations_configs, DestinationTestConfiguration
+from tests.utils import preserve_module_environ, autouse_module_test_storage, patch_module_home_dir
 
 # These tests require Lakeformation permissions to be able to run.
 # The tests have only been run as lakeformation admin so far, since the fixtures require
 # permissions to create new tags, and grant lakeformation permissions to the current role
 
-@pytest.fixture(scope="session", autouse=True)
+
+@pytest.fixture(scope="module")
+def botocore_session(
+    autouse_module_test_storage, preserve_module_environ, patch_module_home_dir
+) -> Iterator[Session]:
+    # with dlt.pipeline("lf_client_pipeline", destination="athena").destination_client() as client:
+    credentials = dlt.config.get("destination.athena.credentials", AwsCredentials)
+    session: Session = credentials._to_botocore_session()
+    yield session
+
+
+@pytest.fixture(scope="module")
+def lf_client(botocore_session: Session) -> LakeFormationClient:
+    return botocore_session.create_client("lakeformation")  # type: ignore[return-value]
+
+
+@pytest.fixture(scope="module", autouse=True)
 def check_lakeformation_enabled(lf_client: LakeFormationClient) -> None:
     """Check if LakeFormation is enabled in the AWS account. Skip tests if it's not."""
     try:
@@ -34,27 +53,29 @@ def check_lakeformation_enabled(lf_client: LakeFormationClient) -> None:
 
     except lf_client.exceptions.AccessDeniedException:
         pytest.skip(
-            "No access to LakeFormation. Please ensure LakeFormation is enabled and you have proper permissions"
+            "No access to LakeFormation. Please ensure LakeFormation is enabled and you have proper"
+            " permissions"
         )
     except Exception as e:
         pytest.skip(f"Failed to verify LakeFormation status: {str(e)}")
 
 
-@pytest.fixture(scope="session")
-def lf_client() -> LakeFormationClient:
-    return boto3.client("lakeformation")
-
-
-@pytest.fixture(scope="session")
-def s3_bucket(lf_client) -> Generator[str, None, None]:
+@pytest.fixture(scope="module")
+def s3_bucket(
+    lf_client: LakeFormationClient, botocore_session: Session
+) -> Generator[str, None, None]:
     """Create a new S3 bucket and register it in lakeformation."""
-    s3_client = boto3.client("s3")
+    from mypy_boto3_s3.client import S3Client
+
+    s3_client: S3Client = botocore_session.create_client("s3")  # type: ignore[assignment]
     bucket_name = "dlt-lakeformation-test-bucket-" + uniq_id(8)
 
     try:
         # Create bucket in the same region as Athena
+        region = botocore_session.get_config_variable("region")
+        assert region, "Must have region in botocore session"
         s3_client.create_bucket(
-            Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": "eu-north-1"}
+            Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": region}
         )
 
         # Wait for bucket to be available
@@ -73,7 +94,8 @@ def s3_bucket(lf_client) -> Generator[str, None, None]:
 
     try:
         # Delete all objects in the bucket
-        s3_resource = boto3.resource("s3")
+        boto3_session = boto3.Session(botocore_session=botocore_session)
+        s3_resource = boto3_session.resource("s3")
         bucket = s3_resource.Bucket(bucket_name)
         bucket.objects.all().delete()
 
@@ -119,13 +141,17 @@ def _grant_lf_permissions(
         lf_client.grant_permissions(**grant)
 
 
-@pytest.fixture(scope="session")
-def lf_tags_config(lf_client: LakeFormationClient) -> Generator[LakeformationConfig, None, None]:
+@pytest.fixture(scope="module")
+def lf_tags_config(
+    lf_client: LakeFormationClient, botocore_session: Session
+) -> Iterator[LakeformationConfig]:
     """Setup lakeformation tags and permissions to the current user."""
+    user_arn = botocore_session.create_client("sts").get_caller_identity()["Arn"]  # type: ignore
+
     key = "test"
     values = ["true", "false"]
     lf_client.create_lf_tag(TagKey=key, TagValues=values)
-    _grant_lf_permissions(IAM_ARN, lf_client, key, values)
+    _grant_lf_permissions(user_arn, lf_client, key, values)
 
     yield LakeformationConfig(enabled=True, tags={"test": "true"})
 
@@ -134,44 +160,34 @@ def lf_tags_config(lf_client: LakeFormationClient) -> Generator[LakeformationCon
     )
 
 
-@pytest.fixture
-def pipelines(
-    lf_tags_config: LakeformationConfig, s3_bucket: str
-) -> Generator[Tuple[dlt.Pipeline, dlt.Pipeline], None, None]:
-    """Create two identical pipelines with and without lakeformation enabled."""
+def create_pipelines(
+    lf_tags_config: LakeformationConfig,
+    s3_bucket: str,
+    destination_config: DestinationTestConfiguration,
+) -> Tuple[dlt.Pipeline, dlt.Pipeline]:
+    """Create two identical pipelines with and without lakeformation enabled. Test fixtures will
+    drop pipelines at the end
+    """
     uid = uniq_id()
     dataset_name = "lakeformation_test" + uid
     pipeline_name = "lakeformation_test" + uid
 
     staging_destination = filesystem(s3_bucket)
 
-    lf_enabled_pipeline = dlt.pipeline(
+    lf_enabled_pipeline = destination_config.setup_pipeline(
         pipeline_name,
-        destination=athena(
-            lakeformation_config=lf_tags_config,
-            athena_work_group=ATHENA_WORKGROUP,
-            query_result_bucket=QUERY_RESULT_BUCKET,
-        ),
+        destination=destination_config.destination_factory(lakeformation_config=lf_tags_config),
         dataset_name=dataset_name,
         staging=staging_destination,
-        dev_mode=False,
     )
-
-    lf_disabled_pipeline = dlt.pipeline(
+    lf_disabled_pipeline = destination_config.setup_pipeline(
         pipeline_name,
-        destination=athena(
-            athena_work_group=ATHENA_WORKGROUP,
-            query_result_bucket=QUERY_RESULT_BUCKET,
-        ),
+        destination=destination_config.destination_factory(),
         dataset_name=dataset_name,
         staging=staging_destination,
-        dev_mode=False,
     )
 
-    yield lf_enabled_pipeline, lf_disabled_pipeline
-
-    lf_enabled_pipeline.drop()
-    lf_disabled_pipeline.drop()
+    return lf_enabled_pipeline, lf_disabled_pipeline
 
 
 def _verify_tags_on_database(
@@ -209,11 +225,22 @@ def _verify_tags_on_table(
         assert db_tags["LFTagsOnTable"][0]["TagValues"] == values
 
 
+@pytest.mark.parametrize(
+    "destination_config",
+    destinations_configs(
+        default_sql_configs=True,
+        subset=["athena"],
+    ),
+    ids=lambda x: x.name,
+)
 def test_new_pipeline_with_lakeformation_tags(
-    lf_client: LakeFormationClient, pipelines: Tuple[dlt.Pipeline, dlt.Pipeline]
+    lf_client: LakeFormationClient,
+    lf_tags_config: LakeformationConfig,
+    s3_bucket: str,
+    destination_config: DestinationTestConfiguration,
 ) -> None:
     """Test that a new pipeline with LakeFormation configuration has tags applied to database and tables."""
-    lf_enabled_pipeline, _ = pipelines
+    lf_enabled_pipeline, _ = create_pipelines(lf_tags_config, s3_bucket, destination_config)
 
     # Create a simple table
     table_name = "test_table"
@@ -227,19 +254,30 @@ def test_new_pipeline_with_lakeformation_tags(
 
     # Verify tags are applied to the database and tables
     _verify_tags_on_database(lf_client, lf_enabled_pipeline.dataset_name, "test", ["true"])
-    _verify_tags_on_table(
-        lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"]
-    )
+    _verify_tags_on_table(lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"])
 
     # Verify that we can run the pipeline again without errors
     lf_enabled_pipeline.run(test_data)
 
 
+@pytest.mark.parametrize(
+    "destination_config",
+    destinations_configs(
+        default_sql_configs=True,
+        subset=["athena"],
+    ),
+    ids=lambda x: x.name,
+)
 def test_apply_tags_to_existing_pipeline_resource(
-    lf_client: LakeFormationClient, pipelines: Tuple[dlt.Pipeline, dlt.Pipeline]
+    lf_client: LakeFormationClient,
+    lf_tags_config: LakeformationConfig,
+    s3_bucket: str,
+    destination_config: DestinationTestConfiguration,
 ) -> None:
     """Test applying LakeFormation tags schema/tables from a pipeline with lakeformation disabled."""
-    lf_enabled_pipeline, lf_disabled_pipeline = pipelines
+    lf_enabled_pipeline, lf_disabled_pipeline = create_pipelines(
+        lf_tags_config, s3_bucket, destination_config
+    )
 
     # Create a table without LakeFormation
     table_name = "test_table"
@@ -260,16 +298,27 @@ def test_apply_tags_to_existing_pipeline_resource(
 
     # Verify tags are applied to the database and tables
     _verify_tags_on_database(lf_client, lf_enabled_pipeline.dataset_name, "test", ["true"])
-    _verify_tags_on_table(
-        lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"]
-    )
+    _verify_tags_on_table(lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"])
 
 
+@pytest.mark.parametrize(
+    "destination_config",
+    destinations_configs(
+        default_sql_configs=True,
+        subset=["athena"],
+    ),
+    ids=lambda x: x.name,
+)
 def test_remove_lakeformation_tags_from_resource(
-    lf_client: LakeFormationClient, pipelines: Tuple[dlt.Pipeline, dlt.Pipeline]
+    lf_client: LakeFormationClient,
+    lf_tags_config: LakeformationConfig,
+    s3_bucket: str,
+    destination_config: DestinationTestConfiguration,
 ) -> None:
     """Test removing lakeformation tags from a resource"""
-    lf_enabled_pipeline, lf_disabled_pipeline = pipelines
+    lf_enabled_pipeline, lf_disabled_pipeline = create_pipelines(
+        lf_tags_config, s3_bucket, destination_config
+    )
 
     # Create a table with LakeFormation enabled
     table_name = "test_table"
@@ -283,9 +332,7 @@ def test_remove_lakeformation_tags_from_resource(
 
     # Verify tags are applied to the database and tables
     _verify_tags_on_database(lf_client, lf_enabled_pipeline.dataset_name, "test", ["true"])
-    _verify_tags_on_table(
-        lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"]
-    )
+    _verify_tags_on_table(lf_client, lf_enabled_pipeline.dataset_name, table_name, "test", ["true"])
 
     # Run the pipeline with LakeFormation disabled
     lf_disabled_pipeline.run(test_data)
