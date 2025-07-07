3dcada31705ccdd44257f36dfb59060dc96c3aad
anuunchin <88698977+anuunchin@users.noreply.github.com>
2025-07-07 09:25:29 +0200

    Drop column from child table test

--- dlt/destinations/impl/athena/sql_client.py
+++ dlt/destinations/impl/athena/sql_client.py
@@ -9,6 +9,9 @@ from typing import (
     Dict,
     Callable,
     Type,
+    List,
+    Union,
+    cast,
 )
 from copy import deepcopy
 import re
@@ -140,6 +143,19 @@ class AthenaSQLClient(SqlClientBase[Connection]):
         statements = [
             f"DROP TABLE IF EXISTS {self.make_qualified_ddl_table_name(table)};" for table in tables
         ]
+        self.execute_many(statements)
+
+    def drop_columns(self, from_tables_drop_cols: List[Dict[str, Union[str, List[str]]]]) -> None:
+        """Drops specified columns from specified tables if they exist"""
+        statements = []
+        for from_table_drop_cols in from_tables_drop_cols:
+            table = cast(str, from_table_drop_cols["from_table"])
+            for column in from_table_drop_cols["drop_columns"]:
+                statements.append(
+                    f"ALTER TABLE {self.make_qualified_ddl_table_name(table)} DROP COLUMN"
+                    f" {self.escape_ddl_identifier(column)};"
+                )
+
         self.execute_many(statements)
 
     @contextmanager
--- tests/load/pipeline/test_drop_column.py
+++ tests/load/pipeline/test_drop_column.py
@@ -1,4 +1,5 @@
 from typing import Any, List, Dict
+from contextlib import nullcontext as does_not_raise
 import pytest
 
 from dlt.pipeline import helpers, Pipeline
@@ -27,13 +28,19 @@ from tests.pipeline.utils import assert_load_info, load_table_counts
 )
 def test_drop_column_command_resources(destination_config: DestinationTestConfiguration) -> None:
     """Test the drop command with resource and verify correct data is deleted from destination"""
+    if (
+        destination_config.destination_type == "athena"
+        and destination_config.table_format != "iceberg"
+    ):
+        pytest.skip()
     source: Any = droppable_source()
 
-    pipeline = destination_config.setup_pipeline("droppbale", dev_mode=True)
+    pipeline = destination_config.setup_pipeline(
+        "test_drop_column_command_resources", dev_mode=True
+    )
     info = pipeline.run(source, **destination_config.run_kwargs)
     assert_load_info(info)
 
-    # TODO: remove this because repeated
     all_tables = list(pipeline.default_schema.tables.keys())
     assert load_table_counts(pipeline, *all_tables) == {
         "_dlt_version": 1,
@@ -57,6 +64,7 @@ def test_drop_column_command_resources(destination_config: DestinationTestConfig
         pipeline,
         from_resources=from_resources,
     )
+
     must_drop_infos = drop_cmd.from_tables_drop_cols
 
     assert (
@@ -81,3 +89,37 @@ def test_drop_column_command_resources(destination_config: DestinationTestConfig
         remaining_cols = pipeline.default_schema.get_table_columns(must_drop_info["from_table"])
         for dropped_col in must_drop_info["drop_columns"]:
             assert dropped_col not in remaining_cols
+
+
+@pytest.mark.parametrize(
+    "destination_config",
+    destinations_configs(
+        default_sql_configs=True,
+        local_filesystem_configs=True,
+        all_buckets_filesystem_configs=True,
+        table_format_local_configs=True,
+    ),
+    ids=lambda x: x.name,
+)
+def test_drop_column_from_child_table(destination_config: DestinationTestConfiguration) -> None:
+    """Test the drop command with column and child table and verify correct data is deleted from destination"""
+    source: Any = droppable_source()
+
+    pipeline = destination_config.setup_pipeline("test_drop_column_from_child_table", dev_mode=True)
+    info = pipeline.run(source, **destination_config.run_kwargs)
+    assert_load_info(info)
+
+    original_parent_tbl_cols = pipeline.default_schema.get_table_columns("droppable_b")
+    assert "m" in pipeline.default_schema.get_table_columns("droppable_b__items")
+
+    drop_cmd = helpers.DropCommand(
+        pipeline, from_resources=["droppable_b"], from_tables=["droppable_b__items"], columns=["m"]
+    )
+    must_drop_infos = drop_cmd.from_tables_drop_cols
+
+    assert [{"from_table": "droppable_b__items", "drop_columns": ["m"]}] == must_drop_infos
+
+    drop_cmd()
+
+    assert "m" not in pipeline.default_schema.get_table_columns("droppable_b__items")
+    assert original_parent_tbl_cols == pipeline.default_schema.get_table_columns("droppable_b")
