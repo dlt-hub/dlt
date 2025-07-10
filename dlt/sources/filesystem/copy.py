769314381ac0aa53da76e18796ce433f8f0b9cfa
dave <shrps@posteo.net>
2025-07-10 16:02:37 +0200

    clean up files a bit and change instantiation of import transformer

--- dlt/sources/filesystem/copy.py
+++ dlt/sources/filesystem/copy.py
@@ -3,38 +3,50 @@ import tempfile, os
 from urllib.parse import urlparse
 import dlt
 
-from typing import Union, List, Any, overload, Iterator
+from typing import Any, Iterator
 
 from dlt.common.storages.fsspec_filesystem import FileItemDict
 from dlt.common.pipeline import SupportsPipeline
 from dlt.common.utils import custom_environ
 
+from dlt.common.storages.configuration import FilesystemConfiguration
+
 from dlt.extract import DltResource
 
+__source_name__ = "filesystem"
 
-@dlt.transformer()
-def import_transformer(items: Iterator[FileItemDict], temp_dir: str) -> Any:
+
+def _import_files(items: Iterator[FileItemDict], temp_dir: str) -> Any:
     for item in items:
         if isinstance(item, FileItemDict):
-            url = urlparse(item["file_url"])
             # local files can be imported directly
-            if url.scheme == "file":
+            if FilesystemConfiguration.is_local_path(item["file_url"]):
                 local_file_path = item.local_file_path
             else:
-                # remote files need to be downloaded to a temp directory
-                local_file_path = os.path.join(temp_dir, os.path.basename(item["file_url"]))
+                url = urlparse(item["file_url"])
+                base_name = os.path.basename(url.path)
+                remote_dir_name = os.path.dirname(url.path)
+
+                # remote files need to be downloaded to a temp directory with the remote structure in tact
+                local_file_dir = os.path.join(temp_dir, remote_dir_name[1:])
+                base_name = os.path.basename(item["file_url"])
+                local_file_path = os.path.join(local_file_dir, base_name)
+
+                # copy file to the local filesystem
+                os.makedirs(local_file_dir, exist_ok=True)
                 with open(local_file_path, "wb") as f:
                     if file_content := item.get("file_content", None):
                         f.write(file_content)
                     else:
                         f.write(item.read_bytes())
+        # for string we assume it is a local file path
+        # TODO: does this make sense?
         elif isinstance(item, str):
             local_file_path = item
 
-        print("ITERATE")
-        print(local_file_path)
-
         ext = os.path.splitext(local_file_path)[1][1:]
+
+        # TODO: should we raise? Should it be configurable?
         if ext not in [
             "jsonl",
             "typed-jsonl",
@@ -46,40 +58,19 @@ def import_transformer(items: Iterator[FileItemDict], temp_dir: str) -> Any:
         ]:
             continue
 
-        print(local_file_path)
-
         yield dlt.mark.with_file_import(local_file_path, ext, 0)  # type: ignore[arg-type]
 
 
-@overload
 def copy_files(
     items: DltResource,
     pipeline: SupportsPipeline,
-    run_kwargs: Any = None,
-) -> None: ...
-
-
-@overload
-def copy_files(
-    items: List[str],
-    pipeline: SupportsPipeline,
-    table_name: str = None,
-    run_kwargs: Any = None,
-) -> None: ...
-
-
-def copy_files(  # type: ignore[misc]
-    items: Union[DltResource, List[str]],
-    pipeline: SupportsPipeline,
-    table_name: str = None,
     run_kwargs: Any = None,
 ) -> None:
     """Copy a file from the source to the destination.
 
     Args:
-        resource: The source to copy from. May also be an list of local file paths
+        resource: The source to copy from.
         pipeline: The pipeline to run. Must have a filesystem destination attached
-        table_name: The name of the table to copy the files to, only used if items is a list of local file paths.
         run_kwargs: The kwargs to pass to the pipeline.run method.
     """
 
@@ -89,20 +80,17 @@ def copy_files(  # type: ignore[misc]
             + pipeline.destination.destination_type
         )
 
-    temp_dir = os.path.join(tempfile.gettempdir(), "dlt")
-
-    # Wrap list of item in resource
-    if isinstance(items, list):
-        items = dlt.resource(items, table_name=table_name)
+    temp_dir = os.path.join(tempfile.gettempdir(), pipeline.dataset_name)
 
     # add import transformer to the resource
     if isinstance(items, DltResource):
         # pipe through transformer and forward table name
         table_name = items.table_name
-        items = items | import_transformer(temp_dir)
-        items = items.with_name(table_name)
+        items = items | dlt.transformer(table_name=table_name)(_import_files).bind(
+            temp_dir=temp_dir
+        )
     else:
         raise ValueError("Invalid resource parameter type: " + type(items))
 
-    with custom_environ({"_experimental_exclude_dlt_tables": "true"}):
+    with custom_environ({"DESTINATION__EXPERIMENTAL_EXCLUDE_DLT_TABLES": "True"}):
         pipeline.run(items, **(run_kwargs or {}))
--- tests/load/sources/filesystem/test_filesystem_copy.py
+++ tests/load/sources/filesystem/test_filesystem_copy.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 import pytest
+import dlt
 
 from tests.load.utils import DestinationTestConfiguration, destinations_configs
-from tests.load.sources.filesystem.cases import  TESTS_BUCKET_URLS
+from tests.load.sources.filesystem.cases import TESTS_BUCKET_URLS
 
 from dlt.sources.filesystem.copy import copy_files
 from dlt.sources.filesystem import filesystem
@@ -9,40 +12,68 @@ from dlt.sources.filesystem import filesystem
 from tests.load.sources.filesystem.utils import glob_test_setup
 from tests.load.sources.filesystem.cases import CSV_GLOB_RESULT
 
+from tests.pipeline.utils import load_table_counts
+from dlt.common.destination.exceptions import DestinationUndefinedEntity
+
+from dlt.common.utils import custom_environ
+
 
 @pytest.mark.parametrize("bucket_url", TESTS_BUCKET_URLS)
 @pytest.mark.parametrize(
     "destination_config",
-    # NOTE: this setup will only load to local filesystem 
+    # NOTE: this setup will only load to local filesystem
     # (jsonl file format is not used, but if we do not add it, every test will run twice with jsonl and parquet set)
     destinations_configs(local_filesystem_configs=True, with_file_format="jsonl"),
     ids=lambda x: x.name,
 )
 def test_copy_to_local(bucket_url: str, destination_config: DestinationTestConfiguration) -> None:
+    # TODO: add test for copy_files with incorrect source type
+    fs_creds: Any = dlt.config.get("destination.filesystem.credentials")
 
-    glob_result = CSV_GLOB_RESULT
     p = destination_config.setup_pipeline("test_copy_to_local", dev_mode=True)
-    copy_files(filesystem(bucket_url, file_glob=glob_result["glob"]).with_name("my_table"), p)
 
-    # check all expected csv files are there
-    all_files = p._fs_client().list_table_files("my_table")
+    # forward credentials defined for destinations to sources
+    with custom_environ(fs_creds):
+        copy_files(
+            filesystem(bucket_url, file_glob=CSV_GLOB_RESULT["glob"]).with_name("my_table"), p  # type: ignore[arg-type]
+        )
 
-    # we have the same amount of files
-    assert len(all_files) == 7
-    assert len(all_files) == len(glob_result["relative_paths"])
+        # check all expected csv files are there
+        all_files = p._fs_client().list_table_files("my_table")
 
-    # check that each local file is found unmodified in the destination
-    source_files = {
-        f["file_content"]
-        for f in list(filesystem(bucket_url, file_glob=glob_result["glob"], extract_content=True))
-    }
-    destination_files = {p._fs_client().read_bytes(f) for f in all_files}
-    assert source_files == destination_files
+        # we have the same amount of files
+        assert len(all_files) == 7
+        assert len(all_files) == len(CSV_GLOB_RESULT["relative_paths"])
+
+        # check that each local file is found unmodified in the destination (no dlt columns added or anything else)
+        source_files = {
+            f["file_content"]
+            for f in list(
+                filesystem(bucket_url, file_glob=CSV_GLOB_RESULT["glob"], extract_content=True)  # type: ignore[arg-type]
+            )
+        }
+        destination_files = {p._fs_client().read_bytes(f) for f in all_files}
+        assert source_files == destination_files
+
+        # check that dlt tables are not created in destination
+        # folders do not exist and raise if accessed
+        with pytest.raises(DestinationUndefinedEntity):
+            load_table_counts(p, "_dlt_loads")
+        with pytest.raises(DestinationUndefinedEntity):
+            load_table_counts(p, "_dlt_pipeline_state")
+        with pytest.raises(DestinationUndefinedEntity):
+            load_table_counts(p, "_dlt_version")
 
 
 # TODO
 def test_copy_with_incorrect_source_type() -> None:
     pass
+
+
+# TODO
+def test_copy_with_file_content_already_extracted() -> None:
+    pass
+
 
 # TODO
 def test_foward_run_kwargs() -> None:
@@ -53,11 +84,7 @@ def test_foward_run_kwargs() -> None:
 def test_copy_unknown_file_format() -> None:
     pass
 
+
 # TODO
 def test_prevent_non_filesystem_destination() -> None:
-    pass
-
-
-# TODO
-def test_copy_files_from_list() -> None:
     pass
