48b6d34ee5ccc4fd375c25eb0f09f09ba6d0f9ae
zilto <68975210+zilto@users.noreply.github.com>
2025-07-16 11:10:37 -0400

    another semver compliant strings

--- dlt/common/destination/dataset.py
+++ dlt/common/destination/dataset.py
@@ -5,8 +5,8 @@ if not TYPE_CHECKING:
 
     DltDeprecationWarning(
         "Content from this module was moved to `dlt._dataset.typing`, which is internal. ",
-        since="1.15",
-        expected_due="2.0",
+        since="1.15.0",
+        expected_due="2.0.0",
     )
 
 
