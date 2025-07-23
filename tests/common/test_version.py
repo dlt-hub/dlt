8911a36eb44ff915cd80550625a29159794c9a7c
David Scharf <shrps@posteo.net>
2025-07-23 15:47:43 +0200

    fix one common test

--- tests/common/test_version.py
+++ tests/common/test_version.py
@@ -8,7 +8,7 @@ from dlt.version import get_installed_requirement_string
 def test_installed_requirement_string() -> None:
     # we are running tests in editable mode so we should get path to here
     path = get_installed_requirement_string()
-    assert os.path.commonpath((__file__, path)) == path
+    assert os.path.join(os.path.commonpath((__file__, path)), "packages", "dlt") == path
     # requests should be properly installed
     requirement = get_installed_requirement_string("requests")
     assert requirement.startswith("requests==")
