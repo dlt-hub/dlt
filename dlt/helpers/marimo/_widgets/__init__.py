ef7312876f48711f43106ae42bec2c3b8a3b3331
zilto <68975210+zilto@users.noreply.github.com>
2025-08-20 16:42:44 -0400

    added schema inspector

--- dlt/helpers/marimo/_widgets/__init__.py
+++ dlt/helpers/marimo/_widgets/__init__.py
@@ -1,7 +1,8 @@
 import marimo as mo
 
 # when adding widget, import the `app` variable and rename it to the widget name
-from dlt.helpers.marimo._widgets._load_package_inspector import app as load_package_inspector
+from dlt.helpers.marimo._widgets._load_package_viewer import app as load_package_viewer
+from dlt.helpers.marimo._widgets._schema_viewer import app as schema_viewer
 
 
 async def render(widget: mo.App) -> mo.Html:
