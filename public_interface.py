ca77f31dce8d833dab0433d151ac4ba155325509
zilto <68975210+zilto@users.noreply.github.com>
2025-08-25 13:58:59 -0400

    updated demo

--- public_interface.py
+++ public_interface.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.14.17"
-app = marimo.App(width="medium")
+app = marimo.App(width="full")
 
 with app.setup:
     import semver
@@ -15,7 +15,7 @@ with app.setup:
     import duckdb
 
 
-@app.function
+@app.function(hide_code=True)
 def get_release_versions(repo: str, org: str) -> list[str]:
     """Returns versions from old to recent"""
     url = f"https://api.github.com/repos/{org}/{repo}/tags"
@@ -32,7 +32,7 @@ def get_release_versions(repo: str, org: str) -> list[str]:
     return release_versions[::-1]
 
 
-@app.function
+@app.function(hide_code=True)
 @dlt.resource(file_format="parquet")
 def breaking_changes_resource(repo: str = "dlt", org: str = "dlt-hub"):
     state_key = "processed_versions"
@@ -67,11 +67,10 @@ def _():
 def _(response):
     dlt_release_versions = []
     for tag in response.json():
-        try:
-            semver.parse(tag["name"])
-            dlt_release_versions.append(tag["name"])
-        except ValueError:
+        if tag["name"].startswith("archive"):
             continue
+
+        dlt_release_versions.append(tag["name"])
     return (dlt_release_versions,)
 
 
@@ -98,8 +97,7 @@ def _(dlt_release_versions, version_selector):
 
 @app.cell
 def _(previous_version_selector, version_selector):
-    versions_selector = mo.hstack([version_selector, previous_version_selector], justify="start")
-    versions_selector
+    mo.hstack([previous_version_selector, version_selector], justify="start")
     return
 
 
@@ -112,13 +110,11 @@ def _(previous_version_selector, version_selector):
 @app.cell
 def _(breaking_changes):
     for bc in breaking_changes:
-        print(bc._explain_oneline())
-
-    bc = breaking_changes[3]
-    return (bc,)
+        print(bc._explain_github())
+    return
 
 
-@app.function
+@app.function(hide_code=True)
 def normalize_breaking_change(bc) -> dict:
     qualified_name = bc.obj.name if bc.obj.parent.is_module else bc.obj.parent.name  + "." + bc.obj.name
     return dict(
@@ -133,36 +129,33 @@ def normalize_breaking_change(bc) -> dict:
     )
 
 
-@app.cell
-def _(bc):
-    bc._format_new_value()
-    return
-
-
-@app.cell
-def _(bc):
-    normalize_breaking_change(bc)
+@app.cell(hide_code=True)
+def _(df, previous_version_selector, version_selector):
+    table = (
+        gt.GT(df)
+        .cols_hide(["parent", "name"])
+        .tab_header("Breaking Changes", subtitle=f"{previous_version_selector.value} -> {version_selector.value}")
+        .tab_stub("qualified_name", groupname_col="module")
+        .tab_style(
+            style=[
+                gt.style.text(weight="bold"),
+                gt.style.fill(color="lightgray")
+            ],
+            locations=gt.loc.row_groups()
+        )
+        .data_color(columns=["kind"], palette="Set3")
+        # .tab_spanner("source", columns=["parent", "name", "lineno"])
+        # .fmt_markdown(["old_value", "new_value"])
+        .opt_stylize(2, color="blue")
+    )
+    table
     return
 
 
 @app.cell
 def _(breaking_changes):
     df = pl.DataFrame([normalize_breaking_change(bc) for bc in breaking_changes])
-    df
     return (df,)
-
-
-@app.cell
-def _(df, previous_version_selector, version_selector):
-    table = (
-        gt.GT(df)
-        .tab_header("Breaking Changes", subtitle=f"{previous_version_selector.value} -> {version_selector.value}")
-        .tab_stub("qualified_name", groupname_col="module")
-        .tab_spanner("source", columns=["parent", "name", "lineno"])
-        .fmt_markdown(["old_value", "new_value"])
-    )
-    table
-    return
 
 
 if __name__ == "__main__":
