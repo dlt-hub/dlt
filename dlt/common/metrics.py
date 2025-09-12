61ad58b331a6e24965ecc47d8966cadb12e10751
anuunchin <88698977+anuunchin@users.noreply.github.com>
2025-09-12 10:50:52 +0200

    custom metrics merged into DataWriterAndCustomMetrics

--- dlt/common/metrics.py
+++ dlt/common/metrics.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass, field, asdict
 import datetime  # noqa: I251
-from typing import Any, Dict, List, NamedTuple, Optional, Tuple  # noqa: 251
+from typing import Any, Dict, List, NamedTuple, Optional, Tuple, Mapping  # noqa: 251
 from dlt.common.typing import TypedDict
+from types import MappingProxyType
 
 
 class DataWriterMetrics(NamedTuple):
@@ -23,6 +25,55 @@ class DataWriterMetrics(NamedTuple):
         return NotImplemented
 
 
+@dataclass(frozen=True)
+class DataWriterAndCustomMetrics:
+    writer_metrics: DataWriterMetrics
+    _custom: Dict[str, Any] = field(default_factory=dict)
+
+    @classmethod
+    def from_writer_metrics(
+        cls,
+        writer_metrics: DataWriterMetrics,
+        resource_metrics: Dict[str, Any] = None,
+        step_metrics: Dict[str, Any] = None,
+    ) -> "DataWriterAndCustomMetrics":
+        custom = {}
+        if resource_metrics:
+            custom.update(resource_metrics)
+        if step_metrics:
+            custom.update(step_metrics)
+        return cls(writer_metrics, custom)
+
+    @property
+    def file_path(self) -> str:
+        return self.writer_metrics.file_path
+
+    @property
+    def items_count(self) -> int:
+        return self.writer_metrics.items_count
+
+    @property
+    def file_size(self) -> int:
+        return self.writer_metrics.file_size
+
+    @property
+    def created(self) -> float:
+        return self.writer_metrics.created
+
+    @property
+    def last_modified(self) -> float:
+        return self.writer_metrics.last_modified
+
+    @property
+    def custom_metrics(self) -> Mapping[str, Any]:
+        return MappingProxyType(self._custom)
+
+    def _asdict(self) -> Dict[str, Any]:
+        d = self.writer_metrics._asdict()
+        d.update(self._custom)
+        return d
+
+
 class StepMetrics(TypedDict):
     """Metrics for particular package processed in particular pipeline step"""
 
@@ -30,12 +81,6 @@ class StepMetrics(TypedDict):
     """Start of package processing"""
     finished_at: datetime.datetime
     """End of package processing"""
-
-
-class ResourceMetrics(TypedDict):
-    writer_metrics: DataWriterMetrics
-    custom_metrics: Dict[str, Any]
-    step_metrics: Dict[str, Dict[str, Any]]
 
 
 class ExtractDataInfo(TypedDict):
@@ -49,7 +94,7 @@ class ExtractMetrics(StepMetrics):
     """Metrics collected per job id during writing of job file"""
     table_metrics: Dict[str, DataWriterMetrics]
     """Job metrics aggregated by table"""
-    resource_metrics: Dict[str, ResourceMetrics]
+    resource_metrics: Dict[str, DataWriterAndCustomMetrics]
     """Job metrics aggregated by resource"""
     dag: List[Tuple[str, str]]
     """A resource dag where elements of the list are graph edges"""
