75477bb2423f7e10487db1804f6f8f3617eaff1b
Arne De Peuter <arne@depeuter.org>
2025-09-30 20:53:43 +0200

    FIX: fix linting error

--- dlt/helpers/dashboard/utils.py
+++ dlt/helpers/dashboard/utils.py
@@ -605,7 +605,7 @@ def open_local_folder(folder: str) -> None:
     """Open a folder in the file explorer"""
     system = platform.system()
     if system == "Windows":
-        os.startfile(folder)
+        os.startfile(folder)  # type: ignore[attr-defined]
     elif system == "Darwin":
         subprocess.run(["open", folder], check=True)
     elif shutil.which("wslview"):
