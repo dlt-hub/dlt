43c7fe936b85ede4da37349e51f9a9a45b874767
Marcin Rudolf <rudolfix@rudolfix.org>
2025-10-21 23:16:33 +0200

    implements signal handlers that allow graceful shutdown on a first signal. tests pipelines in forked tests

--- tests/common/runtime/test_signals.py
+++ tests/common/runtime/test_signals.py
@@ -15,7 +15,7 @@ from tests.utils import skipifwindows
 def clear_signal() -> Iterator[None]:
     yield
     signals.exit_event.clear()
-    signals._received_signal = 0
+    signals._clear_signals()
 
 
 def test_sleep() -> None:
@@ -25,19 +25,19 @@ def test_sleep() -> None:
     assert time.time() - start - 0.5 < 0.2
 
 
-def test_sleep_raises_if_signalled() -> None:
+def test_sleep_not_raises_if_signalled() -> None:
     signals.signal_receiver(4, None)
-    with pytest.raises(SignalReceivedException) as exc:
-        sleep(0.1)
-    assert exc.value.signal_code == 4
+    sleep(0.1)
 
 
 def test_signal_receiver() -> None:
     signals.signal_receiver(8, None)
     assert signals._received_signal == 8
-    # second signal gets ignored
+    assert signals._signal_counts[8] == 1
+
     signals.signal_receiver(4, None)
-    assert signals._received_signal == 8
+    assert signals._received_signal == 4
+    assert signals._signal_counts[4] == 1
 
 
 def test_raise_if_signalled() -> None:
@@ -64,7 +64,7 @@ def test_delayed_signals_context_manager() -> None:
         pytest.fail("Unexpected SignalReceivedException was raised")
 
 
-def test_sleep_signal() -> None:
+def test_raise_if_signalled_thread() -> None:
     thread_signal = 0
 
     def _thread() -> None:
@@ -72,7 +72,9 @@ def test_sleep_signal() -> None:
 
         try:
             # this will sleep on exit event forever
-            sleep(1000000)
+            sleep(100000)
+            assert signals.signal_received()
+            signals.raise_if_signalled()
         except SignalReceivedException as siex:
             thread_signal = siex.signal_code
 
@@ -96,17 +98,10 @@ def test_raise_signal_received_exception() -> None:
 
 @skipifwindows
 @pytest.mark.forked
-def test_signalling() -> None:
-    thread_signal = 0
-
+def test_signalling_graceful() -> None:
     def _thread() -> None:
-        nonlocal thread_signal
-
-        try:
-            # this will sleep on exit event forever
-            sleep(1000000)
-        except SignalReceivedException as siex:
-            thread_signal = siex.signal_code
+        # this will sleep on exit event forever
+        sleep(1000000)
 
     p = DummyProcess(target=_thread)
     p.start()
@@ -115,12 +110,40 @@ def test_signalling() -> None:
     with signals.delayed_signals():
         # now signal to itself
         os.kill(os.getpid(), signals.signal.SIGTERM)
+
         # handler is executed in the main thread (here)
         with pytest.raises(SignalReceivedException) as exc:
             signals.raise_if_signalled()
         assert exc.value.signal_code == 15
         p.join()
-        assert thread_signal == 15
+
+
+@skipifwindows
+@pytest.mark.forked
+def test_signalling_forced() -> None:
+    _done = False
+
+    def _thread() -> None:
+        from time import sleep
+
+        # this will block forever
+        while not _done:
+            sleep(1)
+
+    p = DummyProcess(target=_thread)
+    p.start()
+
+    # handle signals without killing the process
+    with signals.delayed_signals():
+        # now signal to itself
+        os.kill(os.getpid(), signals.signal.SIGINT)
+
+        # second signal kills immediately (SIGTERM would just exit process)
+        with pytest.raises(KeyboardInterrupt):
+            os.kill(os.getpid(), signals.signal.SIGINT)
+
+        _done = True
+        p.join()
 
 
 def test_cleanup() -> None:
