9619002c04f40aabb3f9c657824e1b6cf7402cc5
Jorrit Sandbrink <47451109+jorritsandbrink@users.noreply.github.com>
2025-11-25 20:39:13 +0400

    feat: `snowflake` clustering key modifications (#3365)
    * add support for snowflake clustering key modifications
    
    * add cluster column order test case
    
    * update snowflake cluster hint docs
    
    * switch to reading snowflake cluster hints from table schema

--- dlt/destinations/impl/databricks/databricks.py
+++ dlt/destinations/impl/databricks/databricks.py
@@ -393,11 +393,7 @@ class DatabricksClient(SqlJobClientWithStagingDataset, SupportsStagingDestinatio
         return ""
 
     def _get_table_update_sql(
-        self,
-        table_name: str,
-        new_columns: Sequence[TColumnSchema],
-        generate_alter: bool,
-        separate_alters: bool = False,
+        self, table_name: str, new_columns: Sequence[TColumnSchema], generate_alter: bool
     ) -> List[str]:
         table = self.prepare_load_table(table_name)
 
