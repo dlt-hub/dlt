317544143b2e4753750cd189124a3d3e32fcb069
Alberto Hernandez <22517436+tetelio@users.noreply.github.com>
2025-12-03 10:18:33 +0100

    Remove generator from sqlite file test to use normal json and use storage fixture

--- tests/load/sqlalchemy/test_sqlite_destination.py
+++ tests/load/sqlalchemy/test_sqlite_destination.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 
 import dlt
 import sqlalchemy as sa
@@ -8,7 +7,6 @@ from dlt.destinations import sqlalchemy as dlt_sqlalchemy
 from dlt.common.utils import uniq_id
 
 from tests.pipeline.utils import assert_load_info
-from tests.load.utils import sequence_generator
 
 
 def assert_engine_not_disposed(engine):
@@ -64,34 +62,36 @@ def test_inmemory_database_passing_engine() -> None:
 
 
 def test_file_based_database_with_engine_args() -> None:
-    generator_instance = sequence_generator()
+    output = [
+        {"content": 1},
+        {"content": 2},
+        {"content": 3},
+    ]
 
     @dlt.resource
     def some_data():
-        yield from next(generator_instance)
+        yield output
 
-    fd, db_path = tempfile.mkstemp(suffix=".db")
-    os.close(fd)
+    local_dir = dlt.current.run_context().local_dir
+    db_path = os.path.join(local_dir, "test.db")
+    credentials = f"sqlite:///{db_path}"
 
-    try:
-        credentials = f"sqlite:///{db_path}"
+    engine_args = {
+        "connect_args": {"check_same_thread": False},
+        "poolclass": sa.pool.StaticPool
+    }
 
-        engine_args = {
-            "connect_args": {"check_same_thread": False},
-            "poolclass": sa.pool.StaticPool
-        }
+    pipeline = dlt.pipeline(
+        pipeline_name="test_pipeline_sqlite_file_engine_args" + uniq_id(),
+        destination=dlt_sqlalchemy(credentials, engine_args=engine_args),
+        dataset_name="main"
+    )
 
-        pipeline = dlt.pipeline(
-            pipeline_name="test_pipeline_sqlite_file_engine_args" + uniq_id(),
-            destination=dlt_sqlalchemy(credentials, engine_args=engine_args),
-            dataset_name="main"
-        )
+    info = pipeline.run(
+        some_data(),
+        table_name="file_table"
+    )
 
-        info = pipeline.run(
-            some_data(),
-            table_name="file_table"
-        )
+    print(pipeline.dataset())
 
-        assert_load_info(info)
-    finally:
-        os.remove(db_path)
+    assert_load_info(info)
