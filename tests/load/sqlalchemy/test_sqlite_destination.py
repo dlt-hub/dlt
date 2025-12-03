fe4adb614e6ee171d5ea00fd5fd40a9d6352c6cb
Alberto Hernandez <22517436+tetelio@users.noreply.github.com>
2025-12-03 09:51:15 +0100

    Add uniq id for pipeline names

--- tests/load/sqlalchemy/test_sqlite_destination.py
+++ tests/load/sqlalchemy/test_sqlite_destination.py
@@ -5,6 +5,7 @@ import dlt
 import sqlalchemy as sa
 from sqlalchemy import text
 from dlt.destinations import sqlalchemy as dlt_sqlalchemy
+from dlt.common.utils import uniq_id
 
 from tests.pipeline.utils import assert_load_info
 from tests.load.utils import sequence_generator
@@ -37,7 +38,7 @@ def test_inmemory_database_passing_engine() -> None:
     )
 
     pipeline = dlt.pipeline(
-        pipeline_name="test_pipeline_sqlite_inmemory",
+        pipeline_name="test_pipeline_sqlite_inmemory" + uniq_id(),
         destination=dlt_sqlalchemy(engine),
         dataset_name='main'
     )
@@ -81,7 +82,7 @@ def test_file_based_database_with_engine_args() -> None:
         }
 
         pipeline = dlt.pipeline(
-            pipeline_name="test_pipeline_sqlite_file_engine_args",
+            pipeline_name="test_pipeline_sqlite_file_engine_args" + uniq_id(),
             destination=dlt_sqlalchemy(credentials, engine_args=engine_args),
             dataset_name="main"
         )
