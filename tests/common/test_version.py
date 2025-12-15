d34899fc94dd5a65cc736255ed4d885c7882e64b
Alberto Hernandez <22517436+tetelio@users.noreply.github.com>
2025-12-15 16:48:43 +0100

    Try with pytest xdist group for requests-X

--- tests/common/test_version.py
+++ tests/common/test_version.py
@@ -1,13 +1,12 @@
 import os
 import pytest
-from importlib.metadata import PackageNotFoundError, distributions
+from importlib.metadata import PackageNotFoundError
 from packaging.requirements import Requirement
 
 from dlt.version import get_installed_requirement_string, get_dependency_requirement
 
-
+@pytest.mark.xdist_group("metadata")
 def test_installed_requirement_string() -> None:
-    list(distributions())
     # we are running tests in editable mode so we should get path to here
     path = get_installed_requirement_string()
     assert os.path.commonpath((__file__, path)) == path
