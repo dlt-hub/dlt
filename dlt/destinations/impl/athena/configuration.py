a11e48dbbfadabf45073536ef38d44947eb02921
Jorrit Sandbrink <47451109+jorritsandbrink@users.noreply.github.com>
2025-12-18 12:01:55 +0400

    feat: S3 Tables support for `athena` destination (#3434)
    * fix typo
    
    * add support for s3 tables to athena
    
    * fix test util
    
    * improve naming convention test utils
    
    * improve athena catalog handling and add test
    
    * add docs for athena s3 tables
    
    * remove athena-specific import from test module
    
    * Revert "improve naming convention test utils"
    
    This reverts commit b5eb58f741940821a9d5d5038894b5db4f6a73eb.
    
    * switch to using explicit credentials in TableBucketTestClient
    
    * add athena s3 tables naming convention setting test
    
    * separate athena s3 tables ci job and add id based destination test configuration exclusion
    
    * fix destination test configuration ids to match github workflow
    
    ---------
    
    Co-authored-by: Marcin Rudolf <rudolfix@rudolfix.org>

--- dlt/destinations/impl/athena/configuration.py
+++ dlt/destinations/impl/athena/configuration.py
@@ -4,6 +4,11 @@ from typing import Any, ClassVar, Dict, Final, List, Optional
 from dlt.common.configuration import configspec
 from dlt.common.destination.client import DestinationClientDwhWithStagingConfiguration
 from dlt.common.configuration.specs import AwsCredentials
+from dlt.destinations.impl.athena.utils import is_s3_tables_catalog
+
+
+DEFAULT_AWS_DATA_CATALOG = "awsdatacatalog"
+DEFAULT_TABLE_LOCATION_LAYOUT = "{dataset_name}/{table_name}"
 
 
 @configspec
@@ -18,11 +23,11 @@ class AthenaClientConfiguration(DestinationClientDwhWithStagingConfiguration):
     query_result_bucket: str = None
     credentials: AwsCredentials = None
     athena_work_group: Optional[str] = None
-    aws_data_catalog: Optional[str] = "awsdatacatalog"
+    aws_data_catalog: str = DEFAULT_AWS_DATA_CATALOG
+    staging_aws_data_catalog: Optional[str] = None
     connection_params: Optional[Dict[str, Any]] = None
     force_iceberg: Optional[bool] = None
-    # possible placeholders: {dataset_name}, {table_name}, {location_tag}
-    table_location_layout: Optional[str] = "{dataset_name}/{table_name}"
+    table_location_layout: Optional[str] = DEFAULT_TABLE_LOCATION_LAYOUT
     table_properties: Optional[Dict[str, str]] = None
     lakeformation_config: Optional[LakeformationConfig] = None
     info_tables_query_threshold: int = 90
@@ -36,15 +41,18 @@ class AthenaClientConfiguration(DestinationClientDwhWithStagingConfiguration):
         "info_tables_query_threshold",
     ]
 
-    def to_connector_params(self) -> Dict[str, Any]:
+    def to_connector_params(self, use_catalog_name: bool = True) -> Dict[str, Any]:
         native_credentials = self.credentials.to_native_representation()
         return {
             "s3_staging_dir": self.query_result_bucket,
             "work_group": self.athena_work_group,
-            "catalog_name": self.aws_data_catalog,
+            "catalog_name": self.aws_data_catalog if use_catalog_name else None,
             **(self.connection_params or {}),
             **native_credentials,
         }
+
+    def _is_s3_tables_catalog(self) -> bool:
+        return is_s3_tables_catalog(self.aws_data_catalog)
 
     def __str__(self) -> str:
         """Return displayable destination location"""
