22553b3a2a0968bdb5159c6b1d1aaeb20ae7657a
Jorrit Sandbrink <jorritsandbrink@proton.me>
2026-01-08 18:02:33 +0400

    add test for replication + distribution use case with clickhouse_cluster destination

--- tests/load/clickhouse_cluster/test_cluster/README.md
+++ tests/load/clickhouse_cluster/test_cluster/README.md
@@ -1,13 +1,16 @@
 This folder contains the files to run a local ClickHouse cluster using Docker compose.
 
 ## Cluster characteristics
-The cluster has five nodes:
-- two data nodes
-- three keeper nodes
+The cluster has seven nodes:
+- four data nodes: `clickhouse-01` ... `clickhouse-04`
+- three keeper nodes: `clickhouse-keeper-01` ... `clickhouse-keeper-03`
 
-The cluster has two different configurations:
+The cluster has three different configurations:
 - `cluster_1S_2R`: one shard, two replications (based on https://clickhouse.com/docs/architecture/replication)
 - `cluster_2S_1R`: two shards, one replication (based on https://clickhouse.com/docs/architecture/horizontal-scaling)
+- `cluster_2S_2R`: two shards, two replications (based on https://clickhouse.com/docs/architecture/cluster-deployment)
+
+*The third and fourth data nodes (i.e. `clickhouse-03` and `clickhouse-04`) are only used in configuration `cluster_2S_2R`.*
 
 ## Notes
 
@@ -15,3 +18,4 @@ These adjustements are made to the cluster definition files (compared to ClickHo
 - ports changed, to not interfere with single-node ClickHouse container
 - environment variables added, to create `dlt_data` database and `loader` user
 - `users.xml` config files removed, because we create the user with environment variables
+- swapped data nodes two and three in configuration `cluster_2S_2R`, so it matches configuration `cluster_1S_2R`, so we can use the same `macros` definition for both
--- tests/load/pipeline/test_clickhouse_cluster.py
+++ tests/load/pipeline/test_clickhouse_cluster.py
@@ -15,6 +15,7 @@ from tests.load.clickhouse_cluster.utils import (
     CLICKHOUSE_CLUSTER_NODE_HTTP_PORTS,
     CLICKHOUSE_CLUSTER_NODE_PORTS,
     REPLICATED_CLUSTER_NAME,
+    REPLICATED_SHARDED_CLUSTER_NAME,
     SHARDED_CLUSTER_NAME,
     assert_clickhouse_cluster_conf,
     get_table_engine,
@@ -22,6 +23,11 @@ from tests.load.clickhouse_cluster.utils import (
 )
 from tests.load.utils import DestinationTestConfiguration, destinations_configs
 from tests.pipeline.utils import assert_load_info
+
+
+# NOTE: we can't use Dataset.row_counts, because distributed tables are not part of the schema
+def get_row_cnt(ds: dlt.Dataset, qualified_table_name: str) -> int:
+    return ds.query(f"SELECT COUNT(*) FROM {qualified_table_name}").fetchone()[0]
 
 
 @pytest.mark.parametrize(
@@ -34,11 +40,13 @@ def test_replication(destination_config: DestinationTestConfiguration) -> None:
 
     data = [{"foo": "bar"}]
     replicated = clickhouse_cluster_adapter(
-        data, table_engine_type="replicated_merge_tree"
+        data,
+        table_engine_type="replicated_merge_tree",  # use replicated engine
     ).apply_hints(table_name="replicated")
-    not_replicated = clickhouse_cluster_adapter(data, table_engine_type="merge_tree").apply_hints(
-        table_name="not_replicated"
-    )
+    not_replicated = clickhouse_cluster_adapter(
+        data,
+        table_engine_type="merge_tree",  # use non-replicated engine
+    ).apply_hints(table_name="not_replicated")
     pipe = destination_config.setup_pipeline("test_replication", dev_mode=True)
 
     # assert we are connecting to first node on replicated cluster
@@ -96,13 +104,10 @@ def test_distribution(destination_config: DestinationTestConfiguration) -> None:
     Use case:
     - cluster: 2 shards, 1 replica each
     - data is stored in sharded table
-    - data is not replicated (ENGINE = MergeTree)
+    - shards are not replicated (ENGINE = MergeTree)
     - distributed table is created on top of sharded table
     - distributed table is used to read from / write into all shards
     """
-
-    def get_row_cnt(ds: dlt.Dataset, qualified_table_name: str) -> int:
-        return ds.query(f"SELECT COUNT(*) FROM {qualified_table_name}").fetchone()[0]
 
     # switch to sharded cluster
     set_clickhouse_cluster_conf(cluster=SHARDED_CLUSTER_NAME)
@@ -112,7 +117,9 @@ def test_distribution(destination_config: DestinationTestConfiguration) -> None:
     data = [{"foo": "bar"} for _ in range(n_rows)]
     shard_table_name = "sharded_table"
     res = clickhouse_cluster_adapter(
-        data, table_engine_type="merge_tree", create_distributed_table=True
+        data,
+        table_engine_type="merge_tree",  # use non-replicated engine
+        create_distributed_table=True,
     ).apply_hints(table_name=shard_table_name)
     pipe = destination_config.setup_pipeline("test_distribution", dev_mode=True)
 
@@ -168,3 +175,90 @@ def test_distribution(destination_config: DestinationTestConfiguration) -> None:
 
     # assert total row count across both shards
     assert node_one_shard_row_cnt + node_two_shard_row_cnt == n_rows
+
+
+@pytest.mark.parametrize(
+    "destination_config",
+    destinations_configs(default_sql_configs=True, subset=["clickhouse_cluster"]),
+    ids=lambda x: x.name,
+)
+def test_replication_distribution(destination_config: DestinationTestConfiguration) -> None:
+    """Tests typical replication + distribution use case of ClickHouse cluster.
+
+    Based on https://clickhouse.com/docs/architecture/cluster-deployment.
+
+    Use case:
+    - cluster: 2 shards, 2 replicas each
+    - data is stored in sharded table
+    - shards are replicated (ENGINE = ReplicatedMergeTree)
+    - distributed table is created on top of sharded table
+    - distributed table is used to read from / write into all shards
+    """
+
+    # switch to replicated sharded cluster
+    set_clickhouse_cluster_conf(cluster=REPLICATED_SHARDED_CLUSTER_NAME)
+
+    # define resource and pipeline
+    n_rows = 100
+    data = [{"foo": "bar"} for _ in range(n_rows)]
+    shard_table_name = "replicated_sharded_table"
+    res = clickhouse_cluster_adapter(
+        data,
+        table_engine_type="replicated_merge_tree",  # use replicated engine
+        create_distributed_table=True,
+    ).apply_hints(table_name=shard_table_name)
+    pipe = destination_config.setup_pipeline("test_replication_distribution", dev_mode=True)
+
+    # assert we are connecting to replicated sharded cluster
+    assert_clickhouse_cluster_conf(
+        config=cast(ClickHouseClusterClientConfiguration, pipe.destination_client().config),
+        cluster=REPLICATED_SHARDED_CLUSTER_NAME,
+    )
+
+    # run pipeline
+    load_info = pipe.run(res, **destination_config.run_kwargs)
+    assert_load_info(load_info)
+
+    # assert table engines
+    sql_client = cast(ClickHouseSqlClient, pipe.sql_client())
+    dist_table_name = shard_table_name + DEFAULT_DISTRIBUTED_TABLE_SUFFIX
+    assert get_table_engine(sql_client, table_name=shard_table_name) == "ReplicatedMergeTree"
+    assert get_table_engine(sql_client, table_name=dist_table_name) == "Distributed"
+
+    # define qualified table names
+    shard_qual_table_name = sql_client.make_qualified_table_name(shard_table_name)
+    dist_qual_table_name = sql_client.make_qualified_table_name(dist_table_name)
+
+    # assert row counts on all cluster nodes
+    shard_row_cnts = []
+    for node in range(4):
+        # set ports to connect to current node
+        set_clickhouse_cluster_conf(
+            port=CLICKHOUSE_CLUSTER_NODE_PORTS[node],
+            http_port=CLICKHOUSE_CLUSTER_NODE_HTTP_PORTS[node],
+        )
+
+        # assert we are connecting to correct node
+        assert_clickhouse_cluster_conf(
+            config=cast(ClickHouseClusterClientConfiguration, pipe.destination_client().config),
+            port=CLICKHOUSE_CLUSTER_NODE_PORTS[node],
+            http_port=CLICKHOUSE_CLUSTER_NODE_HTTP_PORTS[node],
+        )
+
+        # assert row counts on current node
+        node_ds = pipe.dataset()
+        node_shard_row_cnt = get_row_cnt(node_ds, shard_qual_table_name)
+        node_dist_row_cnt = get_row_cnt(node_ds, dist_qual_table_name)
+        assert node_shard_row_cnt < n_rows
+        assert node_dist_row_cnt == n_rows
+
+        # save shard row count for cross-node assertions
+        shard_row_cnts.append(node_shard_row_cnt)
+
+    # replicated shards should have same row counts
+    assert shard_row_cnts[0] == shard_row_cnts[1]  # shard 1 replicas on nodes 1,2
+    assert shard_row_cnts[2] == shard_row_cnts[3]  # shard 2 replicas on nodes 3,4
+
+    # assert total row count across both shards
+    assert shard_row_cnts[0] + shard_row_cnts[2] == n_rows  # shard 1 on node 1, shard 2 on node 3
+    assert shard_row_cnts[1] + shard_row_cnts[3] == n_rows  # shard 1 on node 2, shard 2 on node 4
