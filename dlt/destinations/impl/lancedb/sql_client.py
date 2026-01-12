79897475f807f59fe00120598df631b59a91914d
zilto <68975210+zilto@users.noreply.github.com>
2026-01-12 17:19:04 -0500

    refactor to functional form

--- dlt/destinations/impl/lancedb/sql_client.py
+++ dlt/destinations/impl/lancedb/sql_client.py
@@ -35,27 +35,71 @@ def _get_lancedb_sql_capabilities() -> DestinationCapabilitiesContext:
     return caps
 
 
+def _install_and_load_lance_duckdb_extension(duckdb_con: DuckDBPyConnection) -> DuckDBPyConnection:
+    """Ensure the `lance-duckdb` extension is loaded.
+
+    DuckDB ensures installation is only done once per system.
+    Extension loading must be done on every connection
+    """
+    duckdb_con.execute("INSTALL lance FROM community;")
+    duckdb_con.execute("LOAD lance;")
+    return duckdb_con
+
+
+def _maybe_create_lance_duckdb_view(
+    duckdb_con: DuckDBPyConnection, lance_uri, dlt_schema: dlt.Schema, table_name: str
+):
+    """Add the required tables as views to the duckdb in memory instance"""
+    tables_with_data = dlt_schema.dlt_table_names() + dlt_schema.data_table_names(
+        seen_data_only=True
+    )
+    if table_name in tables_with_data:
+        # TODO raise for tables `dlt_schema["table_format"] != "lance"`
+        from_statement = f"{lance_uri}/{table_name}.lance"
+        create_view_sql = f'CREATE OR REPLACE VIEW {table_name} AS SELECT * FROM "{from_statement}"'
+        duckdb_con.execute(create_view_sql)
+
+
+def _create_duckdb_views_for_lance_tables(
+    duckdb_con: DuckDBPyConnection,
+    lance_uri: str,
+    dlt_schema: dlt.Schema,
+    query: AnyStr,
+) -> None:
+    """Create views in DuckDB for each table found in the LanceDB destination."""
+    expression = sqlglot.maybe_parse(query)
+    for table in expression.find_all(exp.Table):
+        if not table.this:
+            continue
+
+        _maybe_create_lance_duckdb_view(
+            duckdb_con=duckdb_con,
+            lance_uri=lance_uri,
+            dlt_schema=dlt_schema,
+            table_name=table.name,
+        )
+
+
 class LanceDBSQLClient(DuckDbSqlClient):
-    def __init__(
-        self,
-        lancedb_client: LanceDBClient,
-    ) -> None:
+    def __init__(self, lancedb_client: LanceDBClient) -> None:
         self.lancedb_client = lancedb_client
         self.lance_uri = lancedb_client.config.lance_uri
         super().__init__(
             dataset_name=self.lancedb_client.dataset_name,
             staging_dataset_name=None,
-            credentials=None,
+            credentials=None,  # duckdb doesn't need special credentials
             capabilities=_get_lancedb_sql_capabilities(),
         )
+        self._conn: DuckDBPyConnection | None = None
+        self._table_views = set()
 
     @raise_open_connection_error
     def open_connection(self) -> DuckDBPyConnection:
-        """Ensure the `lance-duckdb` extension is loaded. This needs to be done on every connection"""
+        if self._conn:
+            return self._conn
+
         self._conn = duckdb.connect(":memory:")
-        # only executes the first time on the system
-        self._conn.execute("INSTALL lance FROM community;")
-        self._conn.execute("LOAD lance;")
+        _install_and_load_lance_duckdb_extension(self._conn)
         return self._conn
 
     def close_connection(self) -> None:
@@ -65,49 +109,16 @@ class LanceDBSQLClient(DuckDbSqlClient):
     @contextmanager
     @raise_database_error
     def execute_query(self, query: AnyStr, *args: Any, **kwargs: Any) -> Iterator[DBApiCursor]:
+        # replace generic string placeholder by DuckDB placeholders
         if args or kwargs:
             query = query.replace("%s", "?")
 
-        expression = sqlglot.parse_one(query, read="duckdb")
-        load_tables: dict[str, str] = {}
-        for table in expression.find_all(exp.Table):
-            if not table.this:
-                continue
-
-            schema = table.db
-            if schema or schema.lower() != self.lancedb_client.dataset_name:
-                load_tables[table.name] = table.name
-
-        if load_tables:
-            self._create_views_for_tables(load_tables)
+        _create_duckdb_views_for_lance_tables(
+            duckdb_con=self.open_connection(),
+            lance_uri=self.lance_uri,
+            dlt_schema=self.lancedb_client.schema,
+            query=query,
+        )
 
         with super().execute_query(query, *args, **kwargs) as cursor:
             yield cursor
-
-    def _create_views_for_tables(self, tables: dict[str, str]) -> None:
-        """Add the required tables as views to the duckdb in memory instance"""
-        tables_with_data = (
-            self.lancedb_client.schema.dlt_table_names()
-            + self.lancedb_client.schema.data_table_names(seen_data_only=True)
-        )
-        for table_name in tables.keys():
-            view_name = tables[table_name]
-
-            if table_name not in tables_with_data:
-                continue
-
-            table_schema = self.lancedb_client.schema.get_table(table_name)
-            table_schema["table_format"] = "lance"
-            self._create_view(view_name, table_schema)
-
-    def _create_view(self, view_name: str, table_schema: PreparedTableSchema) -> None:
-        table_name = table_schema["name"]
-        table_format = table_schema.get("table_format")
-        if table_format != "lance":
-            raise NotImplementedError(
-                f"Can't create view for other formats than `.lance`. Received: {table_format}"
-            )
-
-        from_statement = f"{self.lance_uri}/{table_name}.lance"
-        create_view_sql = f'CREATE OR REPLACE VIEW {view_name} AS SELECT * FROM "{from_statement}"'
-        self._conn.execute(create_view_sql)
