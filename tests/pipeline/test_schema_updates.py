c8c1be6ef382547f053b38f0ee629bc8665db97a
anuunchin <88698977+anuunchin@users.noreply.github.com>
2026-01-12 13:33:20 +0100

    Rebase on devel

--- tests/pipeline/test_schema_updates.py
+++ tests/pipeline/test_schema_updates.py
@@ -6,6 +6,76 @@ import dlt
 from dlt.extract.resource import DltResource
 
 
+def test_schema_updates() -> None:
+    os.environ["COMPLETED_PROB"] = "1.0"  # make it complete immediately
+    p = dlt.pipeline(pipeline_name="test_schema_updates", dev_mode=True, destination="dummy")
+
+    @dlt.source()
+    def source():
+        @dlt.resource()
+        def resource():
+            yield [1, 2, 3]
+
+        return resource
+
+    # test without normalizer attributes
+    s = source()
+    p.run(s, table_name="items", write_disposition="append")
+    assert "config" not in p.default_schema._normalizers_config["json"]
+
+    # add table propagation
+    s = source()
+    p.run(s, table_name="items", write_disposition="merge")
+    assert p.default_schema._normalizers_config["json"]["config"] == {
+        "propagation": {"tables": {"items": {"_dlt_id": "_dlt_root_id"}}}
+    }
+
+    # set root key
+    s = source()
+    s.root_key = True
+    p.run(s, table_name="items", write_disposition="merge")
+    assert p.default_schema._normalizers_config["json"]["config"] == {
+        "root_key_propagation": True,
+        "propagation": {
+            "tables": {"items": {"_dlt_id": "_dlt_root_id"}},
+        },
+    }
+
+    # root key prevails even if not set
+    s = source()
+    s.root_key = False
+    p.run(s, table_name="items", write_disposition="merge")
+    # source schema overwrites normalizer settings so `root` propagation is gone
+    assert p.default_schema._normalizers_config["json"]["config"] == {
+        "root_key_propagation": False,
+    }
+
+    # set max nesting
+    s = source()
+    s.max_table_nesting = 5
+    p.run(s, table_name="items", write_disposition="merge")
+    assert p.default_schema._normalizers_config["json"]["config"] == {
+        "max_nesting": 5,
+        "propagation": {
+            "tables": {"items": {"_dlt_id": "_dlt_root_id"}},
+        },
+    }
+
+    # update max nesting and new table
+    s = source()
+    s.max_table_nesting = 50
+    p.run(s, table_name="items2", write_disposition="merge")
+    assert p.default_schema._normalizers_config["json"]["config"] == {
+        "propagation": {
+            "tables": {
+                "items": {"_dlt_id": "_dlt_root_id"},
+                "items2": {"_dlt_id": "_dlt_root_id"},
+            }
+        },
+        "max_nesting": 50,
+    }
+
+
 def _get_resource(with_apply_hints: bool, data: Dict[str, Any], **hints: Any) -> DltResource:
     if with_apply_hints:
 
