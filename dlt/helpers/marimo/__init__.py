b8cb07d01eee91c7fd8a5de694ad94bbe8d964e9
zilto <68975210+zilto@users.noreply.github.com>
2026-01-16 10:29:11 -0500

    widgets + dependencies

--- dlt/helpers/marimo/__init__.py
+++ dlt/helpers/marimo/__init__.py
@@ -1,8 +1,44 @@
-from dlt.helpers.marimo._widgets import render, load_package_viewer, schema_viewer
+import marimo
+import mowidgets
+
+from dlt.helpers.marimo._load_package_viewer import app as load_package_viewer
+from dlt.helpers.marimo._schema_viewer import app as schema_viewer
+from dlt.helpers.marimo._pipeline_selector import app as pipeline_selector
+
+
+def render(app: marimo.App, *args, **kwargs):
+    if not isinstance(app, marimo.App):
+        raise ValueError("app must be an instance of marimo.App")
+
+    if app is load_package_viewer:
+        return load_package_widget(*args, **kwargs)
+    elif app is schema_viewer:
+        return schema_viewer_widget(app, *args, **kwargs)
+    elif app is pipeline_selector:
+        return pipeline_selector_widget(app, *args, **kwargs)
+    else:
+        raise ValueError("app must be either load_package_viewer or schema_viewer")
+
+
+def pipeline_selector_widget(app, *args, **kwargs):
+    return mowidgets.widgetize(
+        app,
+        data_access=True,
+        public_variables=["pipeline_path", "pipeline_name", "pipeline_locations"],
+    )
+
+
+def load_package_widget(app, pipeline_path: str, *args, **kwargs):
+    return mowidgets.widgetize(app, data_access=True, inputs={"pipeline_path": pipeline_path})
+
+
+def schema_viewer_widget(app, pipeline_name: str, *args, **kwargs):
+    return mowidgets.widgetize(app, data_access=True, inputs={"pipeline_name": pipeline_name})
 
 
 __all__ = (
     "render",
     "load_package_viewer",
     "schema_viewer",
+    "pipeline_selector",
 )
---
+++ dlt/helpers/marimo/_pipeline_selector.py
@@ -0,0 +1,45 @@
+import marimo
+
+__generated_with = "0.19.2"
+app = marimo.App(width="medium", app_title="pipeline_selector")
+
+with app.setup:
+    import marimo as mo
+    from dlt.common.storages import FileStorage
+    from dlt.common.pipeline import get_dlt_pipelines_dir
+
+
+@app.cell
+def _():
+    _storage = FileStorage(get_dlt_pipelines_dir())
+
+    try:
+        _pipelines = _storage.list_folder_dirs(".", to_root=False)
+    except Exception:
+        _pipelines = []
+
+    pipelines_locations = {p: _storage.storage_path + "/" + p for p in sorted(_pipelines)}
+    return (pipelines_locations,)
+
+
+@app.cell
+def _(pipelines_locations):
+    _first_pipeline_name = next(iter(pipelines_locations.keys()))
+    pipeline_selector = mo.ui.dropdown(
+        pipelines_locations.keys(),
+        value=_first_pipeline_name,
+        label="Pipeline",
+    )
+    pipeline_selector
+    return (pipeline_selector,)
+
+
+@app.cell
+def _(pipeline_selector, pipelines_locations):
+    pipeline_name = pipeline_selector.value
+    pipeline_path = pipelines_locations[pipeline_name]
+    return
+
+
+if __name__ == "__main__":
+    app.run()
