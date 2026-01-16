0f2a3c761edb6aeb2f8354e6f1bf7742b7fb3a65
Jorrit Sandbrink <jorritsandbrink@proton.me>
2026-01-16 19:00:27 +0400

    override attribute in clickhouse cluster config to improve default value

--- dlt/destinations/impl/clickhouse_cluster/configuration.py
+++ dlt/destinations/impl/clickhouse_cluster/configuration.py
@@ -4,6 +4,7 @@ from dlt.destinations.impl.clickhouse.configuration import (
     ClickHouseClientConfiguration,
     ClickHouseCredentials,
 )
+from dlt.destinations.impl.clickhouse.typing import TTableEngineType
 
 
 DEFAULT_DISTRIBUTED_TABLE_SUFFIX = "_dist"
@@ -50,7 +51,13 @@ class ClickHouseClusterCredentials(ClickHouseCredentials):
 
 @configspec
 class ClickHouseClusterClientConfiguration(ClickHouseClientConfiguration):
+    # override `clickhouse` attributes
     credentials: ClickHouseClusterCredentials = None
+    # NOTE: `replicated_merge_tree` makes sense for dlt tables because they are small
+    dlt_tables_table_engine_type: TTableEngineType = "replicated_merge_tree"
+    """Default table engine to use for dlt tables. Also applies to dataset sentinel table. Falls back to `table_engine_type` if set to `None`."""
+
+    # add `clickhouse_cluster` specific attributes
     cluster: str = None
     create_distributed_tables: bool = False
     distributed_tables_database: Optional[str] = None
