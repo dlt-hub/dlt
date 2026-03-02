42f79e17f7892f812e0bfb0171ae752b01c9007d
Marcin Rudolf <rudolfix@rudolfix.org>
2026-03-02 19:06:52 +0100

    switches workbench tests to master

--- dlt/_workspace/cli/_urls.py
+++ dlt/_workspace/cli/_urls.py
@@ -8,4 +8,4 @@ DLT_TELEMETRY_DOCS_URL = "https://dlthub.com/docs/reference/telemetry"
 DLT_DEPLOY_DOCS_URL = "https://dlthub.com/docs/walkthroughs/deploy-a-pipeline"
 
 DEFAULT_AI_WORKBENCH_REPO = "https://github.com/dlt-hub/dlthub-ai-workbench.git"
-DEFAULT_AI_WORKBENCH_BRANCH: Optional[str] = "rfix/more-workbench"
+DEFAULT_AI_WORKBENCH_BRANCH: Optional[str] = None
