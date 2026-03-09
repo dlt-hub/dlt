88205b5ac9ffaa8775e02b474d315067188bf21c
anuunchin <88698977+anuunchin@users.noreply.github.com>
2026-03-09 13:19:52 +0100

    Unused type ignore

--- dlt/sources/filesystem/readers.py
+++ dlt/sources/filesystem/readers.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Iterable, Iterator, Optional
+from typing import TYPE_CHECKING, Any, Iterable, Iterator, Optional, cast
 
 from dlt.common import json
 from dlt.common.typing import copy_sig_any
@@ -119,7 +119,7 @@ def _read_csv_duckdb(
 
     for item in items:
         with item.open() as f:
-            file_data = duckdb.from_csv_auto(f, **duckdb_kwargs)  # type: ignore
+            file_data = duckdb.from_csv_auto(cast(Any, f), **duckdb_kwargs)
 
             for batch in helper(file_data, chunk_size):
                 if add_filename:
