7715b8d4520495c8b58b9b0f1c46fb149e97c38a
rudolfix <rudolfix@rudolfix.org>
2026-03-21 11:46:12 +0100

    adds 'jobs' top level section to compact layouts in resolve

--- dlt/_workspace/known_sections.py
+++ dlt/_workspace/known_sections.py
@@ -1,4 +1,8 @@
 """Config section constants for dlt workspace entities."""
 
+from dlt.common.configuration.resolve import COMPACT_LAYOUT_SECTIONS
+
 JOBS = "jobs"
 """Top section holding job configurations: jobs.<section>.<name>.<param>"""
+
+COMPACT_LAYOUT_SECTIONS.add(JOBS)
