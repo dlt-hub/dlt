a62a381b49318e938620172b3e2fd0c0ef69bc58
zilto <68975210+zilto@users.noreply.github.com>
2026-05-11 17:11:53 -0400

    narwhals typing fix

--- dlt/common/libs/narwhals.py
+++ dlt/common/libs/narwhals.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import narwhals
-from narwhals.typing import IntoDataFrame
+
 
 if TYPE_CHECKING:
+    from narwhals.typing import IntoFrame
+
     from dlt.common.libs.pyarrow import pyarrow
 
 
-def df_to_arrow(df: IntoDataFrame) -> pyarrow.Table:
+def df_to_arrow(df: IntoFrame) -> pyarrow.Table:
     """Converts any narwhals-compatible eager or lazy frame to a pyarrow table.
     lazy frames are eagerly collected.
     """
