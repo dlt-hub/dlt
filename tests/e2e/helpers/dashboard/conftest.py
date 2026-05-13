59d69f5e8ad38922bf087e365670b0177394d5d2
Anton Burnashev <anton.burnashev@gmail.com>
2026-05-13 23:36:59 +0200

    try to switch storage cleanup to module scope so fruit_pipeline persists

--- tests/e2e/helpers/dashboard/conftest.py
+++ tests/e2e/helpers/dashboard/conftest.py
@@ -41,12 +41,8 @@ def simple_incremental_pipeline() -> Any:
 
 
 @pytest.fixture(scope="module")
-def fruit_pipeline(pipelines_dir: Path) -> Any:
-    pf = dlt.pipeline(
-        pipeline_name="fruit_pipeline",
-        destination="duckdb",
-        pipelines_dir=str(pipelines_dir),
-    )
+def fruit_pipeline() -> Any:
+    pf = dlt.pipeline(pipeline_name="fruit_pipeline", destination="duckdb")
     pf.run(fruitshop_source())
     return pf
 
