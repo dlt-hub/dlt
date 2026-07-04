700ddf3c4284603e28e2da1cf9ea57d8f9fa43d6
annasclaw <annasclaw@users.noreply.github.com>
2026-07-04 09:34:51 +0100

    fix(bigquery): separate auth project_id from destination project_id in Client

--- dlt/destinations/impl/bigquery/sql_client.py
+++ dlt/destinations/impl/bigquery/sql_client.py
@@ -94,6 +94,7 @@ class BigQuerySqlClient(SqlClientBase[bigquery.Client], DBTransaction):
         ] = credentials
         self.location = location
         self.project_id = project_id or self.credentials.project_id
+        self._auth_project_id = self.credentials.project_id
         self.http_timeout = http_timeout
         super().__init__(self.project_id, dataset_name, staging_dataset_name, capabilities)
 
@@ -106,7 +107,7 @@ class BigQuerySqlClient(SqlClientBase[bigquery.Client], DBTransaction):
     @raise_open_connection_error
     def open_connection(self) -> bigquery.Client:
         self._client = bigquery.Client(
-            self.project_id,
+            self._auth_project_id,
             credentials=self.credentials.to_native_credentials(),
             location=self.location,
         )
